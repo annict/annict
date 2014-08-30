19b6d98c6e11197966ebe408480c80698b9826f8
Koji Shimba <bojovs@gmail.com>
2014-08-30 23:30:25 +0900

    Sidekiqの設定激しく間違えていた

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{REDIS_HOST}:6379/12", namespace: 'sidekiq' }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12", namespace: 'sidekiq' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12", namespace: 'sidekiq' }
 end
