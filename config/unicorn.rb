7b1d5ba52535bf1687e863a5611431579534fda3
Koji Shimba <bojovs@gmail.com>
2014-09-07 00:53:59 +0900

    Unicornの設定をHeroku向けに修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,39 +1,25 @@
-application  = 'annict'
-app_path     = "/home/#{application}"
-current_path = "#{app_path}/current"
-shared_path  = "#{app_path}/shared"
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 
-listen "#{shared_path}/sockets/unicorn.sock"
-pid    "#{shared_path}/pids/unicorn.pid"
+timeout 15
 
-worker_processes 5
 # ダウンタイムをなくす
 preload_app true
 
-# Capistrano 用に RAILS_ROOT を指定
-working_directory current_path
-
-if 'production' == ENV['RAILS_ENV']
-  stderr_path = "#{shared_path}/log/unicorn.stderr.log"
-  stdout_path = "#{shared_path}/log/unicorn.stdout.log"
-end
-
-# ログ
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
