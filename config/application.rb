0aca3604082e5f7f60111ea76b1a8068e83127a3
Koji Shimba <bojovs@gmail.com>
2014-09-07 06:30:33 +0900

    タイムゾーンをUTCに変更

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Annict
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Asia/Tokyo'
-    config.active_record.default_timezone = :local
+    # config.time_zone = 'Asia/Tokyo'
+    # config.active_record.default_timezone = :local
 
     config.i18n.enforce_available_locales = false
 
