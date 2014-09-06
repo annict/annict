515f0e67178da92718230aca1c0adca8bf9b78a4
Koji Shimba <bojovs@gmail.com>
2014-09-07 00:16:33 +0900

    | MySQL |  三 ┏( ^o^)┛ | PostgreSQL |

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140906095802) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: true do |t|
     t.integer  "user_id",        null: false
     t.integer  "recipient_id",   null: false
@@ -26,7 +29,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
 
   add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
   add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
-  add_index "activities", ["user_id"], name: "activities_user_id_fk", using: :btree
 
   create_table "channel_groups", force: true do |t|
     t.string   "sc_chgid",    null: false
@@ -46,21 +48,18 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.datetime "updated_at"
   end
 
-  add_index "channel_works", ["channel_id"], name: "channel_works_channel_id_fk", using: :btree
   add_index "channel_works", ["user_id", "work_id", "channel_id"], name: "index_channel_works_on_user_id_and_work_id_and_channel_id", unique: true, using: :btree
   add_index "channel_works", ["user_id", "work_id"], name: "index_channel_works_on_user_id_and_work_id", using: :btree
-  add_index "channel_works", ["work_id"], name: "channel_works_work_id_fk", using: :btree
 
   create_table "channels", force: true do |t|
     t.integer  "channel_group_id",                null: false
     t.integer  "sc_chid",                         null: false
     t.string   "name",                            null: false
-    t.boolean  "published",        default: true, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "published",        default: true, null: false
   end
 
-  add_index "channels", ["channel_group_id"], name: "channels_channel_group_id_fk", using: :btree
   add_index "channels", ["published"], name: "index_channels_on_published", using: :btree
   add_index "channels", ["sc_chid"], name: "index_channels_on_sc_chid", unique: true, using: :btree
 
@@ -68,50 +67,44 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.integer  "user_id",                              null: false
     t.integer  "episode_id",                           null: false
     t.text     "comment"
-    t.boolean  "spoil",                default: false, null: false
-    t.boolean  "modify_comment",       default: false, null: false
     t.string   "twitter_url_hash"
-    t.string   "facebook_url_hash"
     t.integer  "twitter_click_count",  default: 0,     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "spoil",                default: false, null: false
+    t.string   "facebook_url_hash"
     t.integer  "facebook_click_count", default: 0,     null: false
     t.integer  "comments_count",       default: 0,     null: false
     t.integer  "likes_count",          default: 0,     null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.boolean  "modify_comment",       default: false, null: false
   end
 
-  add_index "checkins", ["episode_id"], name: "checkins_episode_id_fk", using: :btree
   add_index "checkins", ["facebook_url_hash"], name: "index_checkins_on_facebook_url_hash", unique: true, using: :btree
   add_index "checkins", ["twitter_url_hash"], name: "index_checkins_on_twitter_url_hash", unique: true, using: :btree
-  add_index "checkins", ["user_id"], name: "checkins_user_id_fk", using: :btree
 
   create_table "comments", force: true do |t|
     t.integer  "user_id",                 null: false
     t.integer  "checkin_id",              null: false
     t.text     "body",                    null: false
-    t.integer  "likes_count", default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "likes_count", default: 0, null: false
   end
-
-  add_index "comments", ["checkin_id"], name: "comments_checkin_id_fk", using: :btree
-  add_index "comments", ["user_id"], name: "comments_user_id_fk", using: :btree
 
   create_table "episodes", force: true do |t|
     t.integer  "work_id",                        null: false
     t.string   "number"
     t.integer  "sort_number",    default: 0,     null: false
-    t.integer  "sc_count"
     t.string   "title"
-    t.boolean  "single",         default: false
-    t.integer  "checkins_count", default: 0,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "checkins_count", default: 0,     null: false
+    t.boolean  "single",         default: false
+    t.integer  "sc_count"
   end
 
   add_index "episodes", ["checkins_count"], name: "index_episodes_on_checkins_count", using: :btree
   add_index "episodes", ["work_id", "sc_count"], name: "index_episodes_on_work_id_and_sc_count", unique: true, using: :btree
-  add_index "episodes", ["work_id"], name: "episodes_work_id_fk", using: :btree
 
   create_table "follows", force: true do |t|
     t.integer  "user_id",      null: false
@@ -120,7 +113,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.datetime "updated_at"
   end
 
-  add_index "follows", ["following_id"], name: "follows_following_id_fk", using: :btree
   add_index "follows", ["user_id", "following_id"], name: "index_follows_on_user_id_and_following_id", unique: true, using: :btree
 
   create_table "items", force: true do |t|
@@ -133,8 +125,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.datetime "updated_at"
   end
 
-  add_index "items", ["work_id"], name: "items_work_id_fk", using: :btree
-
   create_table "likes", force: true do |t|
     t.integer  "user_id",        null: false
     t.integer  "recipient_id",   null: false
@@ -144,7 +134,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
   end
 
   add_index "likes", ["recipient_id", "recipient_type"], name: "index_likes_on_recipient_id_and_recipient_type", using: :btree
-  add_index "likes", ["user_id"], name: "likes_user_id_fk", using: :btree
 
   create_table "notifications", force: true do |t|
     t.integer  "user_id",                        null: false
@@ -157,19 +146,17 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.datetime "updated_at"
   end
 
-  add_index "notifications", ["action_user_id"], name: "notifications_action_user_id_fk", using: :btree
   add_index "notifications", ["read"], name: "index_notifications_on_read", using: :btree
   add_index "notifications", ["trackable_id", "trackable_type"], name: "index_notifications_on_trackable_id_and_trackable_type", using: :btree
-  add_index "notifications", ["user_id"], name: "notifications_user_id_fk", using: :btree
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id",                           null: false
     t.string   "name",                 default: "", null: false
     t.string   "description",          default: "", null: false
-    t.string   "avatar_uid"
-    t.string   "background_image_uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_uid"
+    t.string   "background_image_uid"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
@@ -179,14 +166,12 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.integer  "episode_id",     null: false
     t.integer  "work_id",        null: false
     t.datetime "started_at",     null: false
-    t.datetime "sc_last_update"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "sc_last_update"
   end
 
   add_index "programs", ["channel_id", "episode_id"], name: "index_programs_on_channel_id_and_episode_id", unique: true, using: :btree
-  add_index "programs", ["episode_id"], name: "programs_episode_id_fk", using: :btree
-  add_index "programs", ["work_id"], name: "programs_work_id_fk", using: :btree
 
   create_table "providers", force: true do |t|
     t.integer  "user_id",          null: false
@@ -200,7 +185,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
   end
 
   add_index "providers", ["name", "uid"], name: "index_providers_on_name_and_uid", unique: true, using: :btree
-  add_index "providers", ["user_id"], name: "providers_user_id_fk", using: :btree
 
   create_table "receptions", force: true do |t|
     t.integer  "user_id",    null: false
@@ -209,7 +193,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.datetime "updated_at"
   end
 
-  add_index "receptions", ["channel_id"], name: "receptions_channel_id_fk", using: :btree
   add_index "receptions", ["user_id", "channel_id"], name: "index_receptions_on_user_id_and_channel_id", unique: true, using: :btree
 
   create_table "seasons", force: true do |t|
@@ -249,19 +232,17 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.integer  "user_id",                     null: false
     t.integer  "work_id",                     null: false
     t.integer  "kind",                        null: false
-    t.boolean  "latest",      default: false, null: false
-    t.integer  "likes_count", default: 0,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "latest",      default: false, null: false
+    t.integer  "likes_count", default: 0,     null: false
   end
 
   add_index "statuses", ["user_id", "latest"], name: "index_statuses_on_user_id_and_latest", using: :btree
-  add_index "statuses", ["work_id"], name: "statuses_work_id_fk", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username",                          null: false
     t.string   "email",                             null: false
-    t.integer  "role",                              null: false
     t.string   "encrypted_password",   default: "", null: false
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",        default: 0,  null: false
@@ -272,11 +253,12 @@ ActiveRecord::Schema.define(version: 20140906095802) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "checkins_count",       default: 0,  null: false
-    t.integer  "notifications_count",  default: 0,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "unconfirmed_email"
+    t.integer  "role",                              null: false
+    t.integer  "checkins_count",       default: 0,  null: false
+    t.integer  "notifications_count",  default: 0,  null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
@@ -296,22 +278,22 @@ ActiveRecord::Schema.define(version: 20140906095802) do
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
   create_table "works", force: true do |t|
-    t.integer  "season_id"
-    t.integer  "sc_tid"
     t.string   "title",                             null: false
     t.integer  "media",                             null: false
     t.string   "official_site_url", default: "",    null: false
     t.string   "wikipedia_url",     default: "",    null: false
-    t.integer  "episodes_count",    default: 0,     null: false
-    t.integer  "watchers_count",    default: 0,     null: false
     t.date     "released_at"
-    t.datetime "nicoch_started_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "episodes_count",    default: 0,     null: false
+    t.integer  "season_id"
     t.boolean  "on_air",            default: false, null: false
-    t.boolean  "fetch_syobocal",    default: false, null: false
     t.string   "twitter_username"
     t.string   "twitter_hashtag"
+    t.integer  "watchers_count",    default: 0,     null: false
+    t.integer  "sc_tid"
+    t.boolean  "fetch_syobocal",    default: false, null: false
+    t.datetime "nicoch_started_at"
   end
 
   add_index "works", ["episodes_count"], name: "index_works_on_episodes_count", using: :btree
@@ -319,7 +301,6 @@ ActiveRecord::Schema.define(version: 20140906095802) do
   add_index "works", ["on_air"], name: "index_works_on_on_air", using: :btree
   add_index "works", ["released_at"], name: "index_works_on_released_at", using: :btree
   add_index "works", ["sc_tid"], name: "index_works_on_sc_tid", unique: true, using: :btree
-  add_index "works", ["season_id"], name: "works_season_id_fk", using: :btree
   add_index "works", ["watchers_count"], name: "index_works_on_watchers_count", using: :btree
 
   add_foreign_key "activities", "users", name: "activities_user_id_fk", dependent: :delete
