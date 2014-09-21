289cfddb547f1a51bee66765cdaa482370c4fd3c
Koji Shimba <bojovs@gmail.com>
2014-09-21 17:10:23 +0900

    marie.jsをプリコンパイル対象にする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Annict::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( application_pc.css application_mobile.css marie.css )
+  config.assets.precompile += %w( application_pc.css application_mobile.css marie.css marie.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
