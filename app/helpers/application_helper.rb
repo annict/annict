0260620a779bd9bcbda4f3f1a97ab895b356614e
Koji Shimba <bojovs@gmail.com>
2014-10-09 21:14:00 +0900

    ページタイトル、metaタグ周りの修正 #100

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,28 @@ module ApplicationHelper
   def custom_time_ago_in_words(date)
     "#{time_ago_in_words(date)}#{t('words.ago')}"
   end
+
+  def meta_description(text = '')
+    text + t('meta.description')
+  end
+
+  def meta_keywords(keywords = [])
+    default_keywords = [
+      'annict',
+      'アニクト',
+      'アニメ',
+      '深夜アニメ',
+      '視聴記録',
+      '視聴管理',
+      '見た',
+      '観た',
+      '記録',
+      '管理',
+      '感想',
+      'sns',
+      'ソーシャル'
+    ]
+
+    (keywords + default_keywords).join(',')
+  end
 end
