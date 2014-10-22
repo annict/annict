ce38ca80d948a6c1e56a50328f2f9e212ead2e9e
Koji Shimba <bojovs@gmail.com>
2014-10-23 01:20:33 +0900

    モデルファイル内のスキーマ情報を削除

--- app/models/finished_tip.rb
+++ app/models/finished_tip.rb
@@ -1,13 +1,2 @@
-# == Schema Information
-#
-# Table name: finished_tips
-#
-#  id         :integer          not null, primary key
-#  user_id    :integer          not null
-#  tip_id     :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class FinishedTip < ActiveRecord::Base
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: tips
-#
-#  id           :integer          not null, primary key
-#  title        :string(255)      not null
-#  partial_name :string(255)      not null
-#  target       :integer          not null
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#
-
 class Tip < ActiveRecord::Base
   extend Enumerize
 
