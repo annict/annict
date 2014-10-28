ec39227ccbedb16b5ab59c8f92d9d72e91bda221
Koji Shimba <bojovs@gmail.com>
2014-10-29 00:59:03 +0900

    tips, finished_tipsテーブルのスキーマファイルを削除

--- db/schemas/finished_tips.rb
+++
@@ -1,9 +0,0 @@
-create_table "finished_tips", force: true do |t|
-  t.integer  "user_id",    null: false
-  t.integer  "tip_id",     null: false
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-end
-
-add_foreign_key "finished_tips", "tips", name: "finished_tips_tip_id_fk", dependent: :delete
-add_foreign_key "finished_tips", "users", name: "finished_tips_user_id_fk", dependent: :delete
--- db/schemas/tips.rb
+++
@@ -1,8 +0,0 @@
-create_table :tips, force: true do |t|
-  t.integer  :target, null: false
-  t.string   :title, null: false
-  t.string   :partial_name, null: false
-  t.integer  :icon_name, null: false
-  t.datetime :created_at, null: false
-  t.datetime :updated_at, null: false
-end
