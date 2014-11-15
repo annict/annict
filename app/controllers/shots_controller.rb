5e4b41648f8aaa8c91d50026fd21022968fb93f6
Koji Shimba <bojovs@gmail.com>
2014-11-15 18:19:42 +0900

    スクショ用ページの見た目を調整

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -3,5 +3,9 @@ class ShotsController < ApplicationController
 
   def show(username)
     @user = User.find_by(username: username)
+    columns = ['works.id', 'works.title', 'seasons.name as season_name']
+    works1 = @user.watching_works.where.not(season_id: nil).order(released_at: :desc).joins(:season).select(columns)
+    works2 = @user.watching_works.where(season_id: nil).select(:id, :title)
+    @seasons = works1.group_by(&:season_name).merge('その他' => works2.to_a)
   end
 end
