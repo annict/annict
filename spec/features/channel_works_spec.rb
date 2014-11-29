e5d4bf68dcb560f6b46f8f1ce16cba2276b2b9d1
Koji Shimba <bojovs@gmail.com>
2014-11-29 12:16:05 +0900

    作品別チャンネル設定ページ復活

---
+++ spec/features/channel_works_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe '作品別チャンネル設定ページ' do
+  let(:user) { create(:registered_user) }
+  let(:episode) { create(:episode) }
+  let!(:program) { create(:program, episode: episode) }
+
+  before do
+    Status.skip_callback(:create, :after, :finish_tips)
+    user.statuses.create(work: episode.work, kind: :watching)
+    login_as(user, scope: :user)
+
+    visit '/channel/works'
+  end
+
+  it 'ページが表示されること' do
+    expect(page).to have_content(episode.work.title)
+  end
+end
