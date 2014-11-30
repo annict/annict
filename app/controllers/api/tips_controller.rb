cfb442feea1b0ad2727b3f9ce70a0461a8982025
Koji Shimba <bojovs@gmail.com>
2014-12-01 00:01:08 +0900

    Api::TipsController#finishが死亡していたので蘇生

--- app/controllers/api/tips_controller.rb
+++ app/controllers/api/tips_controller.rb
@@ -2,7 +2,7 @@ class Api::TipsController < ApplicationController
   before_filter :authenticate_user!, only: [:finish]
 
   def finish(partial_name)
-    UserTipsService.new(user).finish!(partial_name)
+    UserTipsService.new(current_user).finish!(partial_name)
 
     render status: 200, nothing: true
   end
