fbeec57405867ca94a7fb9585fbb2096012c4fde
Koji Shimba <bojovs@gmail.com>
2014-12-14 03:55:22 +0900

    Angular.js関係のファイルを削除

--- app/assets/javascripts/annict.js.coffee
+++ app/assets/javascripts/annict.js.coffee
@@ -1,22 +1,5 @@
 window.Annict = {}
 
-modules = [
-  'angulartics',
-  'angulartics.google.analytics',
-  'ngAnimate',
-  'ngSanitize',
-  'angularSpinner',
-  'pascalprecht.translate',
-  'infinite-scroll'
-]
-Annict.angular = angular.module('annict', modules)
-
-# Making it work with CSRF protection
-# https://shellycloud.com/blog/2013/10/how-to-integrate-angularjs-with-rails-4
-Annict.angular.config ($httpProvider) ->
-  authToken = $('meta[name="csrf-token"]').prop('content')
-  $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = authToken
-
 # Moment.jsを日本語で使う
 # http://momentjs.com/docs/#/i18n/changing-language/
 moment.lang('ja')
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require lodash.min
-//= require angular.min
-//= require angular-animate.min
-//= require angular-sanitize.min
-//= require angular-translate.min
-//= require angulartics.min
-//= require angulartics-ga.min
 //= require bootstrap-sprockets
 //= require spin.min
-//= require angular-spinner.min
 //= require Chart.min
 //= require jquery.easydropdown.min
 //= require moment-with-langs.min
-//= require ng-infinite-scroll.min
 //= require jquery.easing.1.3
 
 //= require_self
 
 //= require annict
-//= require_tree ./config
-//= require_tree ./directives
-//= require_tree ./filters
-//= require_tree ./services
-//= require_tree ./controllers
