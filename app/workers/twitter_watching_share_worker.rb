e3c83b8af7cff034a8ac560b2e7dabde43fc3772
Koji Shimba <bojovs@gmail.com>
2015-01-24 16:06:05 +0900

    見てるアニメ画像のURLを修正

--- app/workers/twitter_watching_share_worker.rb
+++ app/workers/twitter_watching_share_worker.rb
@@ -6,7 +6,7 @@ class TwitterWatchingShareWorker
   def perform(user_id, body)
     user = User.find(user_id)
     shot = user.shots.last
-    image_url = open(shot.image.url)
+    image_url = open("#{ENV['ANNICT_IMAGE2_URL']}/images/#{shot.image_uid}")
     body = body.present? ? "#{body.truncate(53)} " : ''
     tweet = "#{body}#{ENV['ANNICT_URL']}/users/#{user.username} "
 
