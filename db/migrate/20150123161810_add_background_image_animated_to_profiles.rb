6c39a84cd45faf562807931973ae8652a914c53c
Koji Shimba <bojovs@gmail.com>
2015-01-24 11:49:42 +0900

    Houndさんに怒られたところを修正

--- db/migrate/20150123161810_add_background_image_animated_to_profiles.rb
+++ db/migrate/20150123161810_add_background_image_animated_to_profiles.rb
@@ -1,5 +1,6 @@
 class AddBackgroundImageAnimatedToProfiles < ActiveRecord::Migration
   def change
-    add_column :profiles, :background_image_animated, :boolean, null: false, default: false
+    add_column :profiles, :background_image_animated, :boolean,
+      null: false, default: false
   end
 end
