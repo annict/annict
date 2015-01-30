a93e7fb6424ed104ce203d9f7845525b35c2bbd3
Koji Shimba <bojovs@gmail.com>
2015-01-31 01:35:25 +0900

    インデントの調整

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,10 +1,10 @@
 module ProfilesHelper
   def profile_background_image_url(profile, mini: false)
     size = if mini
-      browser.mobile? ? '640x320e' : '500x250e'
-    else
-      browser.mobile? ? '640x650e' : '500x325e'
-    end
+             browser.mobile? ? '640x320e' : '500x250e'
+           else
+             browser.mobile? ? '640x650e' : '500x325e'
+           end
     accessor_name = profile.background_image.present? ? :background_image : :avatar
 
     thumb_url(profile, accessor_name, size)
