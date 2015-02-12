9819f0fa8eb6d33040e98006ee8ed9ec66c8e651
Koji Shimba <bojovs@gmail.com>
2015-02-12 23:11:54 +0900

    エピソード詳細 自分のチェックインを表示する

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -11,15 +11,15 @@ class EpisodesController < ApplicationController
                         .where('checkins.user_id': @checkins.pluck(:user_id).uniq)
                         .order('checkins.id DESC')
     @checkin_user_ids = checkin_users.pluck(:id).uniq
-
-    if @checkin_user.present?
-      @user_checkins = @checkins.where(user: @checkin_user)
-    end
+    @user_checkins = get_user_checkins
+    @current_user_checkins = get_current_user_checkins
 
     if user_signed_in?
       @checkin = @episode.checkins.new
       @checkin.set_shared_sns(current_user)
     end
+
+    @checkins = @checkins.where.not(id: get_checkin_ids)
   end
 
   private
@@ -32,5 +32,25 @@ class EpisodesController < ApplicationController
     if params[:username].present?
       @checkin_user = User.find_by(username: params[:username])
     end
+  end
+
+  def get_user_checkins
+    if @checkin_user.present?
+      @checkins.where(user: @checkin_user)
+    else
+      @checkins.none
+    end
+  end
+
+  def get_current_user_checkins
+    if user_signed_in?
+      @checkins.where(user: current_user).order(created_at: :desc)
+    else
+      @checkins.none
+    end
+  end
+
+  def get_checkin_ids
+    @user_checkins.pluck(:id) | @current_user_checkins.pluck(:id)
   end
 end
