b144e31e7128e6352c8d49d1031fa6d1514df657
Koji Shimba <bojovs@gmail.com>
2015-03-30 23:59:12 +0900

    Unnecessary spacingを除去

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -13,8 +13,8 @@ else
     storage: :s3,
     path: ENV["PAPERCLIP_PATH"],
     s3_credentials: {
-      bucket:            ENV["S3_BUCKET_NAME_PRODUCTION"],
-      access_key_id:     ENV["S3_ACCESS_KEY_ID"],
+      bucket: ENV["S3_BUCKET_NAME_PRODUCTION"],
+      access_key_id: ENV["S3_ACCESS_KEY_ID"],
       secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
     }
   )
