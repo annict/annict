6a680e2820b7f611631ba8f26cb2dd94aed79d6b
Koji Shimba <me@shimba.co>
2015-05-24 13:51:34 +0900

    テストが死亡してるのを直す

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :item do
     name "プリキュアのDVD"
-    url "http://example.com"
+    url "http://amazon.co.jp"
     tombo_image File.open("#{Rails.root}/db/data/image/work_1.png")
     main true
   end
