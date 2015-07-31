a0f082edb9febb2955568e78d915df29852506f2
Koji Shimba <me@shimba.co>
2015-08-01 00:16:14 +0900

    Annotate

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,27 +3,30 @@
 # Table name: works
 #
 #  id                :integer          not null, primary key
-#  season_id         :integer
-#  sc_tid            :integer
-#  title             :string(510)      not null
+#  title             :string           not null
 #  media             :integer          not null
-#  official_site_url :string(510)      default(""), not null
-#  wikipedia_url     :string(510)      default(""), not null
-#  episodes_count    :integer          default(0), not null
-#  watchers_count    :integer          default(0), not null
+#  official_site_url :string           default(""), not null
+#  wikipedia_url     :string           default(""), not null
 #  released_at       :date
-#  nicoch_started_at :datetime
 #  created_at        :datetime
 #  updated_at        :datetime
+#  episodes_count    :integer          default(0), not null
+#  season_id         :integer
+#  twitter_username  :string
+#  twitter_hashtag   :string
+#  watchers_count    :integer          default(0), not null
+#  sc_tid            :integer
 #  fetch_syobocal    :boolean          default(FALSE), not null
-#  twitter_username  :string(510)
-#  twitter_hashtag   :string(510)
+#  nicoch_started_at :datetime
 #  released_at_about :string
 #
 # Indexes
 #
-#  works_sc_tid_key     (sc_tid) UNIQUE
-#  works_season_id_idx  (season_id)
+#  index_works_on_episodes_count  (episodes_count)
+#  index_works_on_media           (media)
+#  index_works_on_released_at     (released_at)
+#  index_works_on_sc_tid          (sc_tid) UNIQUE
+#  index_works_on_watchers_count  (watchers_count)
 #
 
 class Work < ActiveRecord::Base
