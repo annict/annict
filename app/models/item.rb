0db0cc772bf4fb10af0eca6c92042cb1b81d1d2a
Koji Shimba <me@shimba.co>
2015-08-02 01:37:43 +0900

    Annotate

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,19 @@
 #
 #  id                       :integer          not null, primary key
 #  work_id                  :integer
-#  name                     :string           not null
-#  url                      :string           not null
-#  main                     :boolean          default(FALSE), not null
+#  name                     :string(510)      not null
+#  url                      :string(510)      not null
+#  main                     :boolean          not null
 #  created_at               :datetime
 #  updated_at               :datetime
 #  tombo_image_file_name    :string
 #  tombo_image_content_type :string
 #  tombo_image_file_size    :integer
 #  tombo_image_updated_at   :datetime
+#
+# Indexes
+#
+#  items_work_id_idx  (work_id)
 #
 
 class Item < ActiveRecord::Base
--- app/models/season.rb
+++ app/models/season.rb
@@ -3,14 +3,14 @@
 # Table name: seasons
 #
 #  id         :integer          not null, primary key
-#  name       :string           not null
-#  slug       :string           not null
+#  name       :string(510)      not null
+#  slug       :string(510)      not null
 #  created_at :datetime
 #  updated_at :datetime
 #
 # Indexes
 #
-#  index_seasons_on_slug  (slug) UNIQUE
+#  seasons_slug_key  (slug) UNIQUE
 #
 
 class Season < ActiveRecord::Base
