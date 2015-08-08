9108c137d8ed618c237728ebd0e4ba91241725ee
Koji Shimba <me@shimba.co>
2015-08-08 21:13:54 +0900

    rake db:seedしたときに作成される作品の数を50件に絞る

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,7 +78,11 @@ end
 # Work
 # ==============================================================================
 
-CSV.foreach("#{Dir.pwd}/db/data/csv/works.csv", headers: true) do |row|
+limit = ENV.fetch('limit', 50).to_i
+works = CSV.read("#{Dir.pwd}/db/data/csv/works.csv", headers: true)
+works = limit == 0 ? works.to_a : works.to_a.sample(limit)
+
+works.each do |row|
   work = Work.create do |w|
     w.id                = row[0]
     w.season_id         = row[1].presence || nil
@@ -97,19 +101,25 @@ CSV.foreach("#{Dir.pwd}/db/data/csv/works.csv", headers: true) do |row|
   puts "work: #{work.id}"
 end
 
+work_ids = Work.pluck(:id)
+
 
 # ==============================================================================
 # CoverImage
 # ==============================================================================
 
 CSV.foreach("#{Dir.pwd}/db/data/csv/cover_images.csv", headers: true) do |row|
-  cover_image = CoverImage.create do |ci|
-    ci.id        = row[0]
-    ci.work_id   = row[1]
-    ci.file_name = row[2]
-    ci.location  = row[3]
+  work_id = row[1].to_i
+
+  if work_ids.include?(work_id)
+    cover_image = CoverImage.create do |ci|
+      ci.id        = row[0]
+      ci.work_id   = work_id
+      ci.file_name = row[2]
+      ci.location  = row[3]
+    end
+    puts "cover_image: #{cover_image.id}"
   end
-  puts "cover_image: #{cover_image.id}"
 end
 
 
@@ -118,31 +128,31 @@ end
 # ==============================================================================
 
 CSV.foreach("#{Dir.pwd}/db/data/csv/episodes.csv", headers: true) do |row|
-  episode = Episode.create do |e|
-    e.id              = row[0]
-    e.work_id         = row[1]
-    e.number          = row[2]
-    e.sort_number     = row[3]
-    e.sc_count        = row[4]
-    e.title           = row[5]
+  work_id = row[1].to_i
+
+  if work_ids.include?(work_id)
+    episode = Episode.create do |e|
+      e.id              = row[0]
+      e.work_id         = work_id
+      e.number          = row[2]
+      e.sort_number     = row[3]
+      e.sc_count        = row[4]
+      e.title           = row[5]
+    end
+    puts "episode: #{episode.id}"
   end
-  puts "episode: #{episode.id}"
 end
 
 Work.find_each do |work|
   prev_episode = nil
 
   work.episodes.order(:sort_number).find_each do |episode|
-    begin
-      if prev_episode.present?
-        prev_episode.update_column(:next_episode_id, episode.id)
-        puts "episode: #{episode.id}のnext_episode_idを更新..."
-      end
-
-      prev_episode = episode
-    rescue
-      binding.pry
+    if prev_episode.present?
+      prev_episode.update_column(:next_episode_id, episode.id)
+      puts "episode: #{episode.id}のnext_episode_idを更新..."
     end
+
+    prev_episode = episode
   end
 end
 
@@ -152,18 +162,18 @@ end
 # ==============================================================================
 
 CSV.foreach("#{Dir.pwd}/db/data/csv/programs.csv", headers: true) do |row|
-  begin
-  program = Program.create do |p|
-    p.id             = row[0]
-    p.channel_id     = row[1]
-    p.episode_id     = row[2]
-    p.work_id        = row[3]
-    p.started_at     = row[4]
-    p.sc_last_update = row[5]
-  end
-  puts "program: #{program.id}"
-  rescue
-    binding.pry
+  work_id = row[3].to_i
+
+  if work_ids.include?(work_id)
+    program = Program.create do |p|
+      p.id             = row[0]
+      p.channel_id     = row[1]
+      p.episode_id     = row[2]
+      p.work_id        = work_id
+      p.started_at     = row[4]
+      p.sc_last_update = row[5]
+    end
+    puts "program: #{program.id}"
   end
 end
 
@@ -182,11 +192,13 @@ Item.create do |i|
 end
 
 CSV.foreach("#{Dir.pwd}/db/data/csv/items.csv", headers: true) do |row|
-  if row[0] != "1"
+  work_id = row[1].to_i
+
+  if row[0] != "1" && work_ids.include?(work_id)
     identicon = RubyIdenticon.create(row[0], background_color: "ffffff")
     item = Item.create do |i|
       i.id          = row[0]
-      i.work_id     = row[1]
+      i.work_id     = work_id
       i.name        = row[2]
       i.url         = row[3]
       i.main        = (row[4] == "true")
