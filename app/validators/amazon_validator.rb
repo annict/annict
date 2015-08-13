732239be6a25729592fed1e6bfdd69470f85b4d1
Koji Shimba <me@shimba.co>
2015-08-13 11:26:44 +0900

    oops

--- app/validators/amazon_validator.rb
+++ app/validators/amazon_validator.rb
@@ -1,5 +1,5 @@
 class AmazonValidator < ActiveModel::EachValidator
-  def validate_each(record, _attribute, _value)
+  def validate_each(record, _attribute, value)
     if value.present?
       unless /amazon\.co\.jp\z/ === URI.parse(value).host
         record.errors.add(:url, "にはAmazon.co.jpの商品URLを入力してください。")
