9a0db81634742c579e0ccde9a0c69bfb160a869e
Koji Shimba <me@shimba.co>
2015-08-14 04:27:18 +0900

    公開、またはクローズした編集リクエストの変更点は表示しないことにした
    表示するには公開やクローズをした時点でのオリジナルリソース(作品とかエピソードとか)の情報を保持したりする必要があるけど、
    そこまでして変更点を表示する必要性を感じなかったので表示しないことにした。

--- db/migrate/20150620115430_add_diff_and_value_fields_to_edit_requests.rb
+++
@@ -1,7 +0,0 @@
-class AddDiffAndValueFieldsToEditRequests < ActiveRecord::Migration
-  def change
-    add_column :edit_requests, :diffs, :json
-    add_column :edit_requests, :origin_values, :json
-    add_column :edit_requests, :draft_values, :json
-  end
-end
