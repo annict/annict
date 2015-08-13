9628dea3b2adcfb0b4f648f3dff7e0e7c435e01b
Koji Shimba <me@shimba.co>
2015-08-13 11:13:27 +0900

    AmazonのURLかどうかをチェックするバリデータ周りのバグ修正

--- app/validators/amazon_validator.rb
+++ app/validators/amazon_validator.rb
@@ -2,7 +2,7 @@ class AmazonValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
       unless /amazon\.co\.jp\z/ === URI.parse(value).host
-        errors.add(:url, "にはAmazon.co.jpの商品URLを入力してください。")
+        record.errors.add(:url, "にはAmazon.co.jpの商品URLを入力してください。")
       end
     end
   end
