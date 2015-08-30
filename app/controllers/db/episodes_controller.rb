decc6acc516124ffae313d61d0aaf432da738792
Koji Shimba <me@shimba.co>
2015-08-30 16:06:54 +0900

    エピソードのしょぼかる番号を管理者が編集できるように修正

--- app/controllers/db/episodes_controller.rb
+++ app/controllers/db/episodes_controller.rb
@@ -1,5 +1,5 @@
 class Db::EpisodesController < Db::ApplicationController
-  permits :number, :sort_number, :title, :prev_episode_id
+  permits :number, :sort_number, :sc_count, :title, :prev_episode_id
 
   before_action :authenticate_user!
   before_action :load_work, only: [:index, :new, :create, :edit, :update, :destroy]
