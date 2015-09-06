5a9dc07412c60e612401136b46b41cd3eaa978f2
Koji Shimba <me@shimba.co>
2015-09-06 19:39:34 +0900

    編集者の名前の横に「編集者」ラベルを付ける

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,4 +2,10 @@ class UserDecorator < ApplicationDecorator
   def name_link(options = {})
     h.link_to(profile.name, h.user_path(username), options)
   end
+
+  def role_label
+    h.content_tag(:span, class: "role-label label label-default") do
+      role_text
+    end
+  end
 end
