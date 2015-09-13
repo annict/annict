7cc5c7e581978950e4d1a106d53b9089a7092767
Koji Shimba <me@shimba.co>
2015-09-13 18:34:28 +0900

    SNS連携を解除したときシェアするかどうかのフラグをfalseにする

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,7 +2,19 @@ class ProvidersController < ApplicationController
   before_filter :authenticate_user!
 
   def destroy(id)
-    current_user.providers.destroy(id)
+    provider = current_user.providers.find(id)
+
+    ActiveRecord::Base.transaction do
+      case provider.name
+      when "twitter"
+        provider.user.setting.update_column(:share_record_to_twitter, false)
+      when "facebook"
+        provider.user.setting.update_column(:share_record_to_facebook, false)
+      end
+
+      provider.destroy
+    end
+
     redirect_to :back, notice: "連携を解除しました"
   end
 end
