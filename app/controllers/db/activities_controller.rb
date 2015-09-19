5f691669a25719b90f31cce214818572cfd508e7
Koji Shimba <me@shimba.co>
2015-09-19 17:03:37 +0900

    編集リクエストへのコメントはアクティビティに表示しない
    いつか表示したい。

--- app/controllers/db/activities_controller.rb
+++ app/controllers/db/activities_controller.rb
@@ -1,5 +1,9 @@
 class Db::ActivitiesController < Db::ApplicationController
   def index(page = nil)
-    @activities = DbActivity.includes(:trackable, user: :profile).order(id: :desc).page(page)
+    @activities = DbActivity.
+                    includes(:trackable, user: :profile).
+                    where.not(action: "edit_request_comments.create").
+                    order(id: :desc).
+                    page(page)
   end
 end
