fd43b2c59c85128e24d329115574208866c42646
Koji Shimba <me@shimba.co>
2015-11-03 03:48:07 +0900

    sprintf -> format

--- spec/factories/season.rb
+++ spec/factories/season.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :season do
-    sequence(:year) { |n| "20#{sprintf('%02d', n)}" }
+    sequence(:year) { |n| "20%02d-spring" % n }
     name "winter"
     sequence(:sort_number) { |n| n }
-    sequence(:slug) { |n| "20#{sprintf('%02d', n)}-spring" }
+    sequence(:slug) { |n| "20%02d-spring" % n }
   end
 end
