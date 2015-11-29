7cd9dfe30188fc5285a6fd73dd58aeee1d73deda
Koji Shimba <me@shimba.co>
2015-11-29 21:17:06 +0900

    アクセスされたページの種類をGAに送信する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,32 +23,64 @@ module ApplicationHelper
     "#{time_ago_in_words(date)}#{t('words.ago')}"
   end
 
-  def meta_description(text = '')
-    text + t('meta.description')
+  def meta_description(text = "")
+    text + t("meta.description")
   end
 
   def meta_keywords(*keywords)
-    default_keywords = t('meta.keywords').split(',')
-    (keywords + default_keywords).join(',')
+    default_keywords = t("meta.keywords").split(",")
+    (keywords + default_keywords).join(",")
+  end
+
+  # Google Analyticsのカスタムディメンション「ページカテゴリ」に送信する文字列
+  def ga_page_category
+    if top_page?
+      "top"
+    elsif programs_page?
+      "programs"
+    elsif user_profile_page?
+      "user_profile"
+    elsif user_works_page?
+      "user_works"
+    elsif works_page?
+      "works"
+    elsif work_detail_page?
+      "work_detail"
+    elsif episode_detail_page?
+      "episode_detail"
+    else
+      "other"
+    end
   end
 
   def programs_page?
-    params[:controller] == 'programs' && params[:action] == 'index'
+    params[:controller] == "programs" && params[:action] == "index"
   end
 
   def user_works_page?
-    params[:controller] == 'users' && params[:action] == 'works'
+    params[:controller] == "users" && params[:action] == "works"
   end
 
   def works_page?
-    params[:controller] == 'works' &&
-    (params[:action] == 'season' ||
-     params[:action] == 'popular' ||
-     params[:action] == 'recommend' ||
-     params[:action] == 'search')
+    params[:controller] == "works" &&
+      (params[:action] == "season" ||
+       params[:action] == "popular" ||
+       params[:action] == "search")
+  end
+
+  def work_detail_page?
+    params[:controller] == "works" && params[:action] == "show"
+  end
+
+  def episode_detail_page?
+    params[:controller] == "episodes" && params[:action] == "show"
   end
 
   def user_profile_page?
-    params[:controller] == 'users' && params[:action] == 'show'
+    params[:controller] == "users" && params[:action] == "show"
+  end
+
+  def top_page?
+    params[:controller] == "home" && params[:action] == "index"
   end
 end
