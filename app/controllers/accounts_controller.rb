bc926bed469258f53bbce71afc3424cde3911489
Koji Shimba <me@shimba.co>
2015-12-18 21:26:57 +0900

    usernameが変更できるように修正

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,15 +1,30 @@
 class AccountsController < ApplicationController
-  permits :email
+  permits :username, :email, model_name: "User"
 
   before_action :authenticate_user!
 
-  def update(user)
-    current_user.email = user[:email]
+  def show
+    @user = current_user
+  end
 
-    if current_user.valid?
-      current_user.update_column(:unconfirmed_email, user[:email])
-      current_user.resend_confirmation_instructions
-      redirect_to account_path, notice: "確認メールを送信しました"
+  def update(user)
+    @user = User.find(current_user)
+    @user.attributes = user
+
+    if @user.valid?
+      message = nil
+
+      User.transaction do
+        if @user.email_changed?
+          @user.update_column(:unconfirmed_email, user[:email])
+          @user.resend_confirmation_instructions
+          message = "確認メールを送信しました"
+        end
+
+        @user.save(validate: false)
+      end
+
+      redirect_to account_path, notice: (message.presence || "保存しました")
     else
       render "/accounts/show"
     end
