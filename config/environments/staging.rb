0a918b8a43fdca43d8c08c06fd0d06ff6db98485
Koji Shimba <me@shimba.co>
2015-12-19 18:08:01 +0900

    config.assets.precompile を config/initializers/assets.rb に移動

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -57,16 +57,6 @@ Annict::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://d32jo2u8unqym6.cloudfront.net'
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(
-    application_pc.scss
-    application_mobile.scss
-    db.scss
-    base.js
-    db.js
-  )
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
---
+++ config/initializers/assets.rb
@@ -0,0 +1,6 @@
+Rails.application.config.assets.precompile += %w(
+  application_mobile.scss
+  application_common.js.coffee
+  application_mobile.js.coffee
+  db.scss
+)
