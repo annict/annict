d150f2a5f2fcbc899cc6f23ddf8f7b0e772f0d61
Koji Shimba <me@shimba.co>
2015-12-20 17:02:49 +0900

    スタッフ / キャストが保存できるようにした

--- app/controllers/db/people_controller.rb
+++ app/controllers/db/people_controller.rb
@@ -1,7 +1,29 @@
-class Db::PeopleController < Db::ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+module Db
+  class PeopleController < Db::ApplicationController
+    permits :name, :name_kana, :nickname, :gender, :blood_type, :prefecture_id,
+            :birthday, :height, :url, :wikipedia_url, :twitter_username
 
-  def index(page: nil)
-    @people = Person.order(id: :desc).page(page)
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+    def index(page: nil)
+      @people = Person.order(id: :desc).page(page)
+    end
+
+    def edit(id)
+      @person = Person.find(id)
+      authorize @person, :edit?
+    end
+
+    def update(id, person)
+      @person = Person.find(id)
+      authorize @person, :update?
+
+      @person.attributes = person
+      if @person.save_and_create_db_activity(current_user, "people.update")
+        redirect_to edit_db_person_path(@person), notice: "更新しました"
+      else
+        render :edit
+      end
+    end
   end
 end
