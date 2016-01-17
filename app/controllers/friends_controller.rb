2a63bd388baa17891f4c60ab2040a2662b34e197
Koji Shimba <me@shimba.co>
2016-01-17 14:06:50 +0900

    Facebookとのセッションが切れたときはソーシャル連携ページにリダイレクトする

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,9 +4,14 @@ class FriendsController < ApplicationController
   def index
     me_and_following_ids = current_user.followings.pluck(:id) << current_user.id
 
-    friend_ids = current_user.social_friends.all.pluck(:id)
-    not_following_friend_ids = friend_ids - me_and_following_ids
-    @friends = User.where(id: not_following_friend_ids).sample(20)
+    begin
+      friend_ids = current_user.social_friends.all.pluck(:id)
+      not_following_friend_ids = friend_ids - me_and_following_ids
+      @friends = User.where(id: not_following_friend_ids).sample(20)
+    rescue Koala::Facebook::AuthenticationError
+      message = "Facebookとのセッションが切れました。再連携をしてください。"
+      return redirect_to providers_path, alert: message
+    end
 
     user_ids = (User.pluck(:id) - (me_and_following_ids + @friends.map(&:id)))
     @users = User.where(id: user_ids).past_month(field: :current_sign_in_at).sample(20)
