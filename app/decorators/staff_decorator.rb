c41afed545ed822dfd7eca7c845f83019f7657cd
Koji Shimba <me@shimba.co>
2016-03-01 21:03:45 +0900

    スタッフの編集リクエストが見られない状態だったので修正

--- app/decorators/staff_decorator.rb
+++ app/decorators/staff_decorator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class StaffDecorator < ApplicationDecorator
   include StaffDecoratorCommon
   include PersonableDecorator
@@ -5,9 +7,9 @@ class StaffDecorator < ApplicationDecorator
   def db_detail_link(options = {})
     name = options.delete(:name).presence || name
     path = if h.user_signed_in? && h.current_user.committer?
-      h.edit_db_person_staff_path(person, self)
+      h.edit_db_work_staff_path(person, self)
     else
-      h.new_db_person_draft_staff_path(person, staff_id: id)
+      h.new_db_work_draft_staff_path(person, staff_id: id)
     end
 
     h.link_to name, path, options
