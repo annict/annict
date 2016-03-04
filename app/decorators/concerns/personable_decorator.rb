7e9f70ac20f540cde885a43d47a1047b5ad91f2f
Koji Shimba <me@shimba.co>
2016-03-05 00:16:31 +0900

    person -> resource

--- app/decorators/concerns/personable_decorator.rb
+++ app/decorators/concerns/personable_decorator.rb
@@ -3,12 +3,12 @@ module PersonableDecorator
 
   included do
     def name_with_old
-      return name if name == person.name
-      "#{name} (#{person.name})"
+      return name if name == resource.name
+      "#{name} (#{resource.name})"
     end
 
     def name_with_old_link
-      h.link_to name_with_old, h.person_path(person)
+      h.link_to name_with_old, h.person_path(resource)
     end
   end
 end
