ee93fcb55b5451069b71eab82611fd4ebec939e5
Koji Shimba <me@shimba.co>
2016-03-06 11:55:26 +0900

    トップページに「人気アニメ (全体)」を表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,26 +5,12 @@ class HomeController < ApplicationController
     if user_signed_in?
       render :index, layout: "v1/application"
     else
-      @season_top_work = Work.
-        published.
-        by_season(ENV.fetch("ANNICT_CURRENT_SEASON")).
-        order(watchers_count: :desc).
-        first
-
-      @season_works = Work.
-        published.
-        by_season(ENV.fetch("ANNICT_CURRENT_SEASON")).
-        where.not(id: @season_top_work.id).
-        order(watchers_count: :desc).
-        limit(8)
-
-      cover_image_work_id = [@season_top_work.id, @season_works.pluck(:id)].flatten.sample
-      @cover_image_work = Work.find(cover_image_work_id)
-
-      @activities = Activity.where(action: ["checkins.create", "statuses.create"]).
-        order(id: :desc).
-        includes(:recipient, trackable: :user, user: :profile).
-        limit(30)
+      @season_top_work = GuestTopPageService.season_top_work
+      @season_works = GuestTopPageService.season_works
+      @top_work = GuestTopPageService.top_work
+      @works = GuestTopPageService.works
+      @cover_image_work = GuestTopPageService.cover_image_work
+      @activities = GuestTopPageService.activities
 
       render :index_guest, layout: "v2/application"
     end
