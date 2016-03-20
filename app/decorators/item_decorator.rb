8546e986735200fef6ea9044e7e6bc4c84093b6f
Koji Shimba <me@shimba.co>
2016-03-20 11:22:48 +0900

    ItemDecorator#db_detail_link を追加

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
 class ItemDecorator < ApplicationDecorator
   include ItemDecoratorCommon
+
+  def db_detail_link(options = {})
+    name = options.delete(:name).presence || self.name
+    path = if h.user_signed_in? && h.current_user.committer?
+      h.edit_db_work_item_path(work)
+    else
+      h.new_db_work_draft_item_path(work, item_id: id)
+    end
+
+    h.link_to name, path, options
+  end
 end
