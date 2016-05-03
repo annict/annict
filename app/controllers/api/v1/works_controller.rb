2185ee61ef4e9da27b544e5c175c73daac914df6
Koji Shimba <me@shimba.co>
2016-05-03 23:11:01 +0900

    /v1/worksのコントローラの中身をサービス層に移す

--- app/controllers/api/v1/works_controller.rb
+++ app/controllers/api/v1/works_controller.rb
@@ -7,19 +7,7 @@ module Api
 
       def index
         @works = Work.published
-        @works = @works.where(id: @params.filter_ids) if @params.filter_ids.present?
-        @works = @works.by_season(@params.filter_season) if @params.filter_season.present?
-        if @params.filter_title.present?
-          @works = @works.search(title_or_title_kana_cont: @params.filter_title).result
-        end
-        @works = @works.order(id: @params.sort_id) if @params.sort_id.present?
-        if @params.sort_season.present?
-          @works = @works.order_by_season(@params.sort_season)
-        end
-        if @params.sort_watchers_count.present?
-          @works = @works.order(watchers_count: @params.sort_watchers_count)
-        end
-        @works = @works.page(@params.page).per(@params.per_page)
+        @works = Api::V1::WorkIndexService.new(@works, @params).result
       end
     end
   end
