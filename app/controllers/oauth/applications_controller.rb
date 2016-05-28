e2e51e58df58f27d20f35adfa5a8fbf5596f3a47
Koji Shimba <me@shimba.co>
2016-05-28 15:54:22 +0900

    [Annict API] アプリケーションが作られたらアナリティクスにイベントを送る

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -1,40 +1,44 @@
 # frozen_string_literal: true
 
-class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
-  include ViewSelector
+module Oauth
+  class ApplicationsController < Doorkeeper::ApplicationsController
+    include AnalyticsFilter
+    include ViewSelector
 
-  layout "v3/application"
+    layout "v3/application"
 
-  before_action :set_search_params
+    before_action :set_search_params
 
-  def index
-    @applications = current_user.oauth_applications.available
-  end
-
-  def create
-    @application = Doorkeeper::Application.new(application_params)
-    @application.owner = current_user
-
-    if @application.save
-      flash[:notice] = "登録しました"
-      redirect_to oauth_application_url(@application)
-    else
-      render :new
+    def index
+      @applications = current_user.oauth_applications.available
     end
-  end
 
-  def destroy
-    @application.hide!
-    redirect_to oauth_applications_url
-  end
+    def create
+      @application = Doorkeeper::Application.new(application_params)
+      @application.owner = current_user
 
-  private
+      if @application.save
+        ga_client.events.create("oauth_applications", "create", ds: "web")
+        flash[:notice] = "登録しました"
+        redirect_to oauth_application_url(@application)
+      else
+        render :new
+      end
+    end
 
-  def set_application
-    @application = current_user.oauth_applications.available.find(params[:id])
-  end
+    def destroy
+      @application.hide!
+      redirect_to oauth_applications_url
+    end
 
-  def set_search_params
-    @search = SearchService.new(params[:q])
+    private
+
+    def set_application
+      @application = current_user.oauth_applications.available.find(params[:id])
+    end
+
+    def set_search_params
+      @search = SearchService.new(params[:q])
+    end
   end
 end
