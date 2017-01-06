7d24ebfaba54165704acb79b893d8f58a05ef834
Koji Shimba <me@shimba.co>
2017-01-07 06:43:28 +0900

    Rolling restart every 4 hours

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ end
 before_fork do
   require "puma_worker_killer"
 
-  PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
+  PumaWorkerKiller.enable_rolling_restart(4 * 3600) # every 4 hours
 end
 
 # Allow puma to be restarted by `rails restart` command.
