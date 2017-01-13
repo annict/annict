fce2a9b02a882790d8b4d2dc056fa2f61b5c64ea
Koji Shimba <me@shimba.co>
2017-01-14 07:14:29 +0900

    Terms and Privacy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def terms
-    render layout: "v1/application"
-  end
-
-  def privacy
-    render layout: "v1/application"
-  end
 end
