3d1eb55001aedc9d6a20323b4c33e84365ee9d32
Koji Shimba <me@shimba.co>
2017-02-11 15:28:59 +0900

    Encode URL

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,7 +2,7 @@
 
 module UrlHelper
   def link_with_domain(url)
-    link_to URI.parse(url).host.downcase, url, target: "_blank"
+    link_to URI.parse(URI.encode(url)).host.downcase, url, target: "_blank"
   end
 
   def annict_url(method, *args, **options)
