1fdf2d19b851fe1aa7b0e8357c978374fb04c8a4
Koji Shimba <me@shimba.co>
2017-03-11 15:22:18 +0900

    Add config.action_mailer.asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
     end
   end
 
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery
   # to raise delivery errors.
