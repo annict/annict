57bc760d09ac63e11b1fc67d5eee34612a5afc8f
Koji Shimba <me@shimba.co>
2017-03-11 14:49:07 +0900

    Use ANNICT_FORCE_SSL ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = ENV["ANNICT_FORCE_SSL"] == "true"
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
