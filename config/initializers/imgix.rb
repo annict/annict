ad86a7efd9dc6e21f91a650ad4d4f6d3785d3b23
Koji Shimba <me@shimba.co>
2017-03-12 22:13:26 +0900

    oops

--- config/initializers/imgix.rb
+++ config/initializers/imgix.rb
@@ -7,8 +7,7 @@ if Rails.env.development?
         # https://github.com/imgix/imgix-rails が提供する `ix_image_url` メソッドを上書きする
         # 開発環境ではDmmyixを利用して画像のリサイズを行う
         def ix_image_url(source, options = {})
-          "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150"
-          # "/dmmyix#{source}?#{options.to_query}"
+          "/dmmyix#{source}?#{options.to_query}"
         end
       end
     end
