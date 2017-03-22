bd2b8a19f853d6a23539fd51825130d8cfbc6212
Koji Shimba <me@shimba.co>
2017-03-23 01:01:58 +0900

    Merge config file with default files in Rails 5.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
 %w(fonts).each do |dir_name|
   Rails.application.config.assets.paths << "#{Rails.root}/app/assets/#{dir_name}"
 end
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 Rails.application.config.assets.precompile += %w(
   db.scss
