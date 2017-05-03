05653cb35f8bebce9b28ce64db09d2953fbc413a
Koji Shimba <me@shimba.co>
2017-05-04 00:26:20 +0900

    Fix API

--- spec/factories/work.rb
+++ spec/factories/work.rb
@@ -19,27 +19,21 @@ FactoryGirl.define do
     end
 
     trait :with_current_season do
-      after :create do |work|
-        year, name = ENV["ANNICT_CURRENT_SEASON"].split("-")
-        season = create(:season, year: year, name: name)
-        work.update(season_id: season.id)
-      end
+      year, name = ENV["ANNICT_CURRENT_SEASON"].split("-")
+      season_year year
+      season_name name
     end
 
     trait :with_next_season do
-      after :create do |work|
-        year, name = ENV["ANNICT_NEXT_SEASON"].split("-")
-        season = create(:season, year: year, name: name)
-        work.update(season_id: season.id)
-      end
+      year, name = ENV["ANNICT_NEXT_SEASON"].split("-")
+      season_year year
+      season_name name
     end
 
     trait :with_prev_season do
-      after :create do |work|
-        year, name = ENV["ANNICT_PREVIOUS_SEASON"].split("-")
-        season = create(:season, year: year, name: name)
-        work.update(season_id: season.id)
-      end
+      year, name = ENV["ANNICT_PREVIOUS_SEASON"].split("-")
+      season_year year
+      season_name name
     end
 
     trait :with_episode do
