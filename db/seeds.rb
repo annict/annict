bec576175a341ca1fc860eacc0a1efc580439d00
Kohei Suzuki <eagletmt@gmail.com>
2017-05-06 03:36:51 +0900

    Reduce the number of commits in db:seed for better performance
    Before
    
    ```
    % bin/rails db:seed
    % psql -c 'delete from episodes' annict_development
    DELETE 1565
    % time bin/rails db:seed
    bin/rails db:seed  26.08s user 1.50s system 36% cpu 1:16.16 total
    ```
    
    After
    
    ```
    % bin/rails db:seed
    % psql -c 'delete from episodes' annict_development
    DELETE 1565
    % time bin/rails db:seed
    bin/rails db:seed  13.77s user 0.90s system 87% cpu 16.805 total
    ````

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,10 @@
 ].each do |model_class|
   file_path = "#{Dir.pwd}/db/data/csv/#{model_class.table_name}.csv"
 
-  CSV.foreach(file_path, headers: true) do |row|
-    record = model_class.where(id: row["id"]).first_or_create!(row.to_h.except("id"))
-    puts "#{model_class}: #{record.id}"
+  model_class.transaction do
+    CSV.foreach(file_path, headers: true) do |row|
+      record = model_class.where(id: row["id"]).first_or_create!(row.to_h.except("id"))
+      puts "#{model_class}: #{record.id}"
+    end
   end
 end
