73d5bf6769aaa7e77ca66a4185e4ee2e8dd80c7d
Koji Shimba <me@shimba.co>
2017-05-23 22:59:42 +0900

    Add user and viewer queries

--- app/graphql/object_types/query.rb
+++ app/graphql/object_types/query.rb
@@ -6,6 +6,20 @@ ObjectTypes::Query = GraphQL::ObjectType.define do
   field :node, GraphQL::Relay::Node.field
   field :nodes, GraphQL::Relay::Node.plural_field
 
+  field :user, ObjectTypes::User do
+    argument :username, !types.String
+
+    resolve ->(_obj, args, _ctx) {
+      User.find_by(username: args[:username])
+    }
+  end
+
+  field :viewer, ObjectTypes::User do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:viewer]
+    }
+  end
+
   connection :searchWorks, ObjectTypes::Work.connection_type do
     argument :annictIds, types[!types.Int]
     argument :seasons, types[!types.String]
