3f5ba5c140f1e12400a5c67294f149e0e7ab1bc5
Koji Shimba <me@shimba.co>
2017-06-17 19:52:08 +0900

    Add program info each episodes to track page

--- app/queries/user_episodes_query.rb
+++ app/queries/user_episodes_query.rb
@@ -13,4 +13,14 @@ class UserEpisodesQuery
     episode_ids = work.episodes.published.pluck(:id)
     work.episodes.where(id: (episode_ids - latest_status.watched_episode_ids))
   end
+
+  def program(episode)
+    channel_work = @user.channel_works.find_by(work: episode.work)
+    return if channel_work.blank?
+    Program.
+      where(channel: channel_work.channel, episode: episode).
+      published.
+      order(started_at: :desc).
+      first
+  end
 end
