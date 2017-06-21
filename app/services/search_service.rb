f90f4caed7e421d9abec7e1f763654ff557d5cbd
Koji Shimba <me@shimba.co>
2017-06-21 21:55:32 +0900

    Fix a bug which can’t search by Katakana & Kanji keywords
    fixes #803

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -9,28 +9,32 @@ class SearchService
   end
 
   def series_list
-    collection(Series).search(name_or_name_ro_or_name_en_cont: keyword).result
+    collection(Series).search(name_or_name_ro_or_name_en_cont_any: keywords).result
   end
 
   def works
-    collection(Work).search(title_or_title_ro_or_title_en_or_title_kana_cont: keyword).result
+    collection(Work).search(title_or_title_ro_or_title_en_or_title_kana_cont_any: keywords).result
   end
 
   def people
-    collection(Person).search(name_or_name_en_or_name_kana_cont: keyword).result
+    collection(Person).search(name_or_name_en_or_name_kana_cont_any: keywords).result
   end
 
   def organizations
-    collection(Organization).search(name_or_name_en_or_name_kana_cont: keyword).result
+    collection(Organization).search(name_or_name_en_or_name_kana_cont_any: keywords).result
   end
 
   def characters
-    collection(Character).search(name_or_name_en_or_name_kana_cont: keyword).result
+    collection(Character).search(name_or_name_en_or_name_kana_cont_any: keywords).result
   end
 
   private
 
-  def keyword
+  def keywords
+    [@q, keyword_hira]
+  end
+
+  def keyword_hira
     Moji.kata_to_hira(@q)
   end
 
