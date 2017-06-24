d3ce0b65a307fff659df5cc8bb016edf590bd87c
Koji Shimba <me@shimba.co>
2017-06-24 23:11:45 +0900

    Update sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -23,9 +23,6 @@
 
     User.find_each do |u|
       add user_path(u.username), priority: 0.9, lastmod: u.updated_at
-      add favorite_characters_path(u.username), priority: 0.8, lastmod: u.updated_at
-      add favorite_people_path(u.username), priority: 0.8, lastmod: u.updated_at
-      add favorite_organizations_path(u.username), priority: 0.8, lastmod: u.updated_at
 
       Status.kind.values.each do |k|
         add user_works_path(u.username, k), priority: 0.8
@@ -34,10 +31,20 @@
       u.records.find_each do |r|
         add record_path(u.username, r), priority: 0.8
       end
+
+      u.reviews.published.find_each do |r|
+        add review_path(u.username, r), priority: 0.8
+      end
     end
 
     Season.list do |s|
       add season_works_path(s.slug), priority: 0.9
+    end
+
+    Character.published.find_each do |c|
+      if c.casts.published.present?
+        add character_path(c.id), priority: 0.9, lastmod: c.updated_at
+      end
     end
 
     Person.published.find_each do |p|
@@ -58,21 +65,6 @@
       w.episodes.published.find_each do |e|
         add work_episode_path(w.id, e.id), priority: 1.0 if e.records.present?
       end
-    end
-
-    Character.published.find_each do |c|
-      add character_path(c.id), priority: 0.9, lastmod: c.updated_at
-      add character_fans_path(c.id), priority: 0.7, lastmod: c.updated_at
-    end
-
-    Person.published.find_each do |p|
-      add person_path(p.id), priority: 0.9, lastmod: p.updated_at
-      add person_fans_path(p.id), priority: 0.7, lastmod: p.updated_at
-    end
-
-    Organization.published.find_each do |o|
-      add organization_path(o.id), priority: 0.9, lastmod: o.updated_at
-      add organization_fans_path(o.id), priority: 0.7, lastmod: o.updated_at
     end
   end
 end
