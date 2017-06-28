aae65abee153e229ee73752280aca4a66a53998c
Koji Shimba <me@shimba.co>
2017-06-29 00:22:35 +0900

    Add some methods to Annict::Amazon::Item

--- lib/annict/amazon/item.rb
+++ lib/annict/amazon/item.rb
@@ -11,14 +11,45 @@ module Annict
           options[:AWS_secret_key] = ENV.fetch("AWS_PAA_SECRET_KEY")
           options[:associate_tag] = ENV.fetch("AMAZON_ASSOCIATE_TAG")
         end
+        @res ||= ::Amazon::Ecs.item_lookup(@asin,
+          country: @country,
+          response_group: "Medium")
       end
 
-      def detail
-        res = ::Amazon::Ecs.item_lookup(@asin,
-          country: @country,
-          response_group: "Small,Images")
-        binding.pry
-        image_set_list = res.doc.css("Items Item ImageSets ImageSet")
+      def manufacturer
+        @res.doc.css("Items Item ItemAttributes Manufacturer").text
+      end
+
+      def title
+        @res.doc.css("Items Item ItemAttributes Title").text
+      end
+
+      def detail_page_url
+        @res.doc.css("Items Item DetailPageURL").text
+      end
+
+      def amount
+        @res.doc.css("Items Item ItemAttributes ListPrice Amount").text.to_i
+      end
+
+      def currency_code
+        @res.doc.css("Items Item ItemAttributes ListPrice CurrencyCode").text
+      end
+
+      def offer_amount
+        @res.doc.css("Items Item OfferSummary LowestNewPrice Amount").text.to_i
+      end
+
+      def offer_currency_code
+        @res.doc.css("Items Item OfferSummary LowestNewPrice Amount").text
+      end
+
+      def release_date
+        @res.doc.css("Items Item ItemAttributes ReleaseDate").text
+      end
+
+      def images
+        image_set_list = @res.doc.css("Items Item ImageSets ImageSet")
         image_set_list.map do |image_set|
           image = image_set.css("HiResImage").presence ||
             image_set.css("LargeImage").presence ||
