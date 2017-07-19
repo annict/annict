1d310d05d0fa7b7feaeb05aa47b5605ac48c3cfe
Koji Shimba <me@shimba.co>
2017-07-19 23:36:13 +0900

    Fix a missing email confirmation message
    fixes #816

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class ConfirmationsController < ApplicationController
   def show
     user = User.confirm_by_token(params[:confirmation_token])
 
     if user.errors.empty?
-      redirect_to root_path, notice: t('confirmations.confirmed')
+      redirect_to root_path, notice: t("messages.confirmations.confirmed")
     else
-      redirect_to root_path, danger: t('confirmations.failure')
+      redirect_to root_path, danger: t("messages.confirmations.failure")
     end
   end
 end
