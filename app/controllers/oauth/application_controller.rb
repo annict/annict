8934d10bbdb93c4927e8b843a7d5c031d09d4653
Koji Shimba <me@shimba.co>
2017-08-04 22:24:35 +0900

    Fix redirect bug on OAuth process

--- app/controllers/oauth/application_controller.rb
+++ app/controllers/oauth/application_controller.rb
@@ -15,6 +15,8 @@ module Oauth
 
     layout "application"
 
+    before_action :redirect_if_unexpected_subdomain
+    before_action :switch_languages
     before_action :set_search_params
     before_action :load_data_into_gon
 
