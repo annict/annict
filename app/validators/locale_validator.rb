3b6ada4bd746184806494ad3b3ede38446ddb7d0
Koji Shimba <me@shimba.co>
2017-08-16 01:19:53 +0900

    Remove unused validation

--- app/validators/locale_validator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class LocaleValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    return if value.match?(/\A(ja|en)\z/)
-    record.errors.add(attribute, "に不正な値が入っています")
-  end
-end
