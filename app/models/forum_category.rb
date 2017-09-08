6f42a553fefc41be76be85db96ebc78291dab4cf
Koji Shimba <me@shimba.co>
2017-09-08 23:22:54 +0900

    Add site news section to home page

--- app/models/forum_category.rb
+++ app/models/forum_category.rb
@@ -21,5 +21,9 @@
 #
 
 class ForumCategory < ApplicationRecord
+  extend Enumerize
+
+  enumerize :slug, in: %i(site_news general feedback db_request), scope: true
+
   has_many :forum_posts, inverse_of: :forum_category, dependent: :destroy
 end
