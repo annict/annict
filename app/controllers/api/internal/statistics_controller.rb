da9fbb249176639da30b41e62d6025c6667eeb50
Koji Shimba <me@shimba.co>
2017-11-05 11:43:03 +0900

    Fix user heat map to count multiple records and use local time zone

--- app/controllers/api/internal/statistics_controller.rb
+++ app/controllers/api/internal/statistics_controller.rb
@@ -3,14 +3,25 @@
 module Api
   module Internal
     class StatisticsController < Api::Internal::ApplicationController
+      include TimeZoneHelper
+
       def user_heatmap(username, start_date, end_date)
         start_date = Time.parse(start_date)
         end_date = Time.parse(end_date)
         user = User.find_by!(username: username)
-        @days = user.activities.records_and_reviews.between_times(start_date, end_date).
-          group_by_day(:created_at).count.
+        time_zone = local_time_zone.presence || user.time_zone
+
+        record_days = user.records.between_times(start_date, end_date).
+          group_by_day(:created_at, time_zone: time_zone).count.
           map { |date, val| [date.to_time.to_i, val] }.
           to_h
+
+        review_days = user.reviews.between_times(start_date, end_date).
+          group_by_day(:created_at, time_zone: time_zone).count.
+          map { |date, val| [date.to_time.to_i, val] }.
+          to_h
+
+        @days = record_days.merge(review_days) { |_, v1, v2| v1 + v2 }
       end
     end
   end
