d18b69e49a6cdd85492091ba4ad2ee1b648005e0
Koji Shimba <me@shimba.co>
2018-02-04 20:49:24 +0900

    Remove alias of d3-selection

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -3,7 +3,6 @@ const webpack = require('webpack')
 module.exports = {
   resolve: {
     alias: {
-      'd3-selection': 'd3-selection/build/d3-selection.js',
       vue: 'vue/dist/vue.esm.js',
     },
   },
