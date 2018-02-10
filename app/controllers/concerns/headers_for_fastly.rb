1359332bcfd8c09f8fc9d984974420227bcdc832
Koji Shimba <me@shimba.co>
2018-02-10 23:31:31 +0900

    Add X-Visitor-Type to Vary header

--- app/controllers/concerns/headers_for_fastly.rb
+++ app/controllers/concerns/headers_for_fastly.rb
@@ -9,7 +9,9 @@ module HeadersForFastly
 
   private
 
+  # X-UA-Device: pc | mobile
+  # X-Visitor-Type: user | guest
   def set_vary_header
-    response.set_header("Vary", "Accept-Encoding, Origin, X-UA-Device")
+    response.set_header("Vary", "Accept-Encoding, X-UA-Device, X-Visitor-Type")
   end
 end
