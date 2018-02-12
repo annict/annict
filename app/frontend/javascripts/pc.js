2d0e28e6c3fd39ab21b1d715168687290795fd69
Koji Shimba <me@shimba.co>
2018-02-12 18:21:24 +0900

    Stop to use async/await
    Related:
    10b4ae36648fe3a6dddc0276f464a9b1f8084abb
    1e95ccacb5654c36086dfa00815bdd57055061d8

--- app/frontend/javascripts/pc.js
+++ app/frontend/javascripts/pc.js
@@ -143,17 +143,27 @@ document.addEventListener('turbolinks:load', event => {
 
   new Vue({
     el: '.p-application',
+
     data: {
       appData: {},
     },
-    created: async function() {
-      this.appData = await app.loadAppData()
 
-      if (this.appData.isUserSignedIn) {
-        this.pageData = await app.loadPageData()
-      }
+    created() {
+      const self = this
 
-      eventHub.$emit('app:loaded')
+      app.loadAppData().done(appData => {
+        self.appData = appData
+
+        if (self.appData.isUserSignedIn && app.loadPageData()) {
+          app.loadPageData().done(pageData => {
+            this.pageData = pageData
+            eventHub.$emit('app:loaded')
+            return
+          })
+        }
+
+        eventHub.$emit('app:loaded')
+      })
     },
   })
 })
