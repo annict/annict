cb12c995a37317f9baf8d531d1edc22e82a3e857
Koji Shimba <me@shimba.co>
2018-02-14 23:48:17 +0900

    Log template_path

--- app/controllers/concerns/page_params_methods.rb
+++ app/controllers/concerns/page_params_methods.rb
@@ -8,6 +8,10 @@ module PageParamsMethods
   def store_page_params(assigns)
     template_path = "application/page_params/#{page_category}"
 
+    if Rails.env.development?
+      logger.info("store_page_params - template_path: #{template_path}")
+    end
+
     return unless File.exist?(Rails.root.join("app/views/#{template_path}.jb"))
 
     page = gon.page.presence || {}
