314731c153579fd5787a2b48cabdbf282bc0018f
Koji Shimba <me@shimba.co>
2018-02-17 20:58:08 +0900

    Add controller to change display options

---
+++ app/controllers/work_display_options_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class WorkDisplayOptionsController < ApplicationController
+  def show
+    redirect_path = params[:to].presence || root_path
+
+    display_options = Setting.display_option_work_list.values
+    return redirect_back(fallback_location: send(redirect_path)) unless params[:display].in?(display_options)
+
+    if user_signed_in? && current_user.setting.display_option_work_list != params[:display]
+      current_user.setting.update_column(:display_option_work_list, params[:display])
+    end
+
+    redirect_to "#{redirect_path}?display=#{params[:display]}"
+  end
+end
