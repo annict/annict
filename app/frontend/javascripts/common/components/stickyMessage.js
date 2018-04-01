4b3da7f34b596ea1823abcf06c7f45c9e6224aae
Koji Shimba <me@shimba.co>
2018-04-01 14:39:12 +0900

    Add sticky message component

---
+++ app/frontend/javascripts/common/components/stickyMessage.js
@@ -0,0 +1,39 @@
+import eventHub from "../eventHub";
+
+export default {
+  template: '#t-sticky-message',
+
+  data() {
+    return {
+      appData: {},
+      appLoaded: false,
+      pageCategory: gon.page.category,
+    }
+  },
+
+  props: {
+    messageBody: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    isDisplayable: function() {
+      return !this.appData.isUserSignedIn
+    },
+  },
+
+  mounted() {
+    eventHub.$on('app:loaded', () => {
+      this.appData = this.$root.appData
+      this.appLoaded = true
+
+      if (this.isDisplayable() && typeof ga === 'function') {
+        return ga('send', 'event', 'Components', 'load', `stickyMessage-${this.pageCategory}`, {
+          nonInteraction: true
+        })
+      }
+    })
+  },
+}
