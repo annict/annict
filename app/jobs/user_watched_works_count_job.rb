680c6a320c563f691d9f370fc5c1e1c055c10a8c
Koji Shimba <me@shimba.co>
2018-05-06 02:17:06 +0900

    Change priority of UserWatchedWorksCountJob to low

--- app/jobs/user_watched_works_count_job.rb
+++ app/jobs/user_watched_works_count_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserWatchedWorksCountJob < ApplicationJob
-  queue_as :default
+  queue_as :low_priority
 
   def perform(user)
     user.update_watched_works_count
