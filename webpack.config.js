5b98f368a20c8068548390f10a9ad514feed9b11
Koji Shimba <me@shimba.co>
2018-08-26 04:47:12 +0900

    Setup Webpack + Reason

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const glob = require('glob')
+const path = require('path')
+
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const packs = path.join(__dirname, 'app', 'frontend', 'packs')
+const targets = glob.sync(path.join(packs, '**/*.js'))
+const entry = targets.reduce((entry, target) => {
+  const bundle = path.relative(packs, target)
+  const ext = path.extname(bundle)
+
+  return Object.assign({}, entry, {
+    // Input: "application.js"
+    // Output: { "application": "./application.js" }
+    [bundle.replace(ext, '')]: `./${path.relative(__dirname, packs)}/${bundle}`,
+  })
+}, {})
+
+module.exports = {
+  entry,
+  mode: isProd ? 'production' : 'development',
+  output: {
+    filename: '[name]-[hash].js',
+    chunkFilename: '[name].bundle-[hash].js',
+    path: path.resolve(__dirname, 'public', 'packs'),
+    publicPath: '/packs/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name]-[hash].[ext]',
+              context: 'app/frontend',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'app', 'frontend')],
+    extensions: ['.css', '.gif', '.jpeg', '.jpg', '.js', '.jsx', '.png', '.scss', '.svg'],
+  },
+  plugins: [
+    new ManifestPlugin({
+      fileName: 'manifest.json',
+      publicPath: '/packs/',
+      writeToFileEmit: true,
+    }),
+  ],
+}
