86022130a55730d13e29e181e86dd432bd22c2ca
Koji Shimba <me@shimba.co>
2018-08-25 14:24:10 +0900

    Use LocalHelper#local_domain

--- app/controllers/api/internal/v3/base_data_controller.rb
+++ app/controllers/api/internal/v3/base_data_controller.rb
@@ -4,11 +4,13 @@ module Api
   module Internal
     module V3
       class BaseDataController < Api::Internal::V3::ApplicationController
+        include LocalHelper
+
         def show
           data = {
             csrfParam: request_forgery_protection_token,
             csrfToken: form_authenticity_token,
-            domain: locale == :ja ? ENV.fetch("ANNICT_JP_DOMAIN") : ENV.fetch("ANNICT_DOMAIN"),
+            domain: local_domain,
             env: Rails.env,
             locale: locale,
             isSignedIn: user_signed_in?
