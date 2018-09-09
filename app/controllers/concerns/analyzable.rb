732a71c708d1bc1c8f53d5ff327d128f47d799c5
Koji Shimba <me@shimba.co>
2018-09-09 11:53:52 +0900

    Remove GaHelper

--- app/controllers/concerns/analyzable.rb
+++ app/controllers/concerns/analyzable.rb
@@ -3,9 +3,18 @@
 module Analyzable
   extend ActiveSupport::Concern
 
-  included do
-    def ga_client
-      @ga_client ||= Annict::Analytics::Client.new(request, current_user)
+  def ga_client
+    @ga_client ||= Annict::Analytics::Client.new(request, current_user)
+  end
+
+  def ga_tracking_id(request)
+    return ENV.fetch("GA_TRACKING_ID_API") if request.subdomain == "api"
+
+    case [request.subdomain, request.domain].select(&:present?).join(".")
+    when ENV.fetch("ANNICT_JP_DOMAIN")
+      ENV.fetch("GA_TRACKING_ID_JP")
+    else
+      ENV.fetch("GA_TRACKING_ID")
     end
   end
 end
