a048ee065d3bdccd2e41c0f9b8fd11653c8d3873
Koji Shimba <me@shimba.co>
2018-09-09 20:52:23 +0900

    Remove call of gon

--- app/controllers/concerns/page_params_methods.rb
+++ app/controllers/concerns/page_params_methods.rb
@@ -9,9 +9,5 @@ module PageParamsMethods
     template_path = "application/page_params/#{page_category}"
     logger.info("store_page_params - template_path: #{template_path}") if Rails.env.development?
     return unless File.exist?(Rails.root.join("app/views/#{template_path}.jb"))
-
-    page = gon.page.presence || {}
-    page[:params] = render_jb(template_path, assigns)
-    gon.push(page: page)
   end
 end
