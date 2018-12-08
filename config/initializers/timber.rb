52459f7d2d16b906bb1e31d5d16a595854f94ce4
Koji Shimba <me@shimba.co>
2018-12-08 15:37:59 +0900

    Update Timber config

--- config/initializers/timber.rb
+++ config/initializers/timber.rb
@@ -1,23 +1,9 @@
-# Timber.io Ruby Configuration - Simple Structured Logging
-#
-#  ^  ^  ^   ^      ___I_      ^  ^   ^  ^  ^   ^  ^
-# /|\/|\/|\ /|\    /\-_--\    /|\/|\ /|\/|\/|\ /|\/|\
-# /|\/|\/|\ /|\   /  \_-__\   /|\/|\ /|\/|\/|\ /|\/|\
-# /|\/|\/|\ /|\   |[]| [] |   /|\/|\ /|\/|\/|\ /|\/|\
-# -------------------------------------------------------------------
-# Website:       https://timber.io
-# Documentation: https://timber.io/docs
-# Support:       support@timber.io
-# -------------------------------------------------------------------
+# frozen_string_literal: true
 
-config = Timber::Config.instance
-
-config.logrageify!
-
-# Add additional configuration here.
-# For common configuration options see:
-# https://timber.io/docs/languages/ruby/configuration
-#
-# For a full list of configuration options see:
-# http://www.rubydoc.info/github/timberio/timber-ruby/Timber/Config
-
+if Rails.env.production?
+  config = Timber::Config.instance
+  config.logrageify!
+  config.integrations.action_view.silence = true
+  config.integrations.active_record.silence = true
+  config.integrations.rack.http_events.collapse_into_single_event = true
+end
