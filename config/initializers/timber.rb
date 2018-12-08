4a97af231351edb5a07fce5abbcd219b18cfd154
Koji Shimba <me@shimba.co>
2018-12-08 17:24:48 +0900

    Override user context data to not send user email to Timber

--- config/initializers/timber.rb
+++ config/initializers/timber.rb
@@ -6,4 +6,17 @@ if Rails.env.production?
   config.integrations.action_view.silence = true
   config.integrations.active_record.silence = true
   config.integrations.rack.http_events.collapse_into_single_event = true
+  # Override user context data to not send user email to Timber.
+  # https://docs.timber.io/concepts/log-event-json-schema/context/user/
+  # https://github.com/timberio/timber-ruby/tree/e9d4582c20e974ff00a039e6ec697784fa1b6ecd#configuration
+  config.integrations.rack.user_context.custom_user_hash = lambda do |rack_env|
+    user = rack_env["warden"]&.user
+    return nil unless user
+    {
+      id: user.id,
+      meta: {
+        username: user.username
+      }
+    }
+  end
 end
