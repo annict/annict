13ff4b205c1d37863badde863f4f7aa6e83ba20a
Koji Shimba <me@shimba.co>
2019-01-05 02:39:24 +0900

    REST API: Add /v1/characters endpoint

---
+++ app/controllers/api/v1/characters_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CharactersController < Api::V1::ApplicationController
+      before_action :prepare_params!, only: %i(index)
+
+      def index
+        @characters = Character.published
+        @characters = Api::V1::CharacterIndexService.new(@characters, @params).result
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/characters_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+describe "Api::V1::Characters" do
+  let(:access_token) { create(:oauth_access_token) }
+  let!(:character) { create(:character) }
+
+  describe "GET /v1/characters" do
+    before do
+      get api("/v1/characters", access_token: access_token.token)
+    end
+
+    context "when added no parameters" do
+      it "responses 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "gets character info" do
+        expected_hash = {
+          "id" => character.id,
+          "name" => character.name,
+          "name_kana" => character.name_kana,
+          "name_en" => character.name_en,
+          "nickname" => character.nickname,
+          "nickname_en" => character.nickname_en,
+          "birthday" => character.birthday,
+          "birthday_en" => character.birthday_en,
+          "age" => character.age,
+          "age_en" => character.age_en,
+          "blood_type" => character.blood_type,
+          "blood_type_en" => character.blood_type_en,
+          "height" => character.height,
+          "height_en" => character.height_en,
+          "weight" => character.weight,
+          "weight_en" => character.weight_en,
+          "nationality" => character.nationality,
+          "nationality_en" => character.nationality_en,
+          "occupation" => character.occupation,
+          "occupation_en" => character.occupation_en,
+          "description" => character.description,
+          "description_en" => character.description_en,
+          "description_source" => character.description_source,
+          "description_source_en" => character.description_source_en,
+          "favorite_characters_count" => character.favorite_characters_count,
+          "series" => nil
+        }
+        expect(json["characters"][0]).to include(expected_hash)
+        expect(json["total_count"]).to eq(1)
+        expect(json["next_page"]).to eq(nil)
+        expect(json["prev_page"]).to eq(nil)
+      end
+    end
+  end
+end
