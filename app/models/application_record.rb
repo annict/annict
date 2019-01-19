8419c69e553e59dc49e9523becd05bab950d575d
Koji Shimba <me@shimba.co>
2019-01-20 00:10:03 +0900

    Move method_missing to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -14,4 +14,26 @@ class ApplicationRecord < ActiveRecord::Base
   def root_resource?
     false
   end
+
+  def method_missing(method_name, *arguments, &block)
+    return super if method_name.blank?
+    return super unless method_name.to_s.start_with?("local_")
+    _local_property(method_name.to_s.sub("local_", ""), *arguments)
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?("local_") || super
+  end
+
+  private
+
+  def _local_property(property_name, fallback: true)
+    property_ja = send(property_name.to_sym)
+    property_en = send("#{property_name}_en".to_sym)
+
+    return property_ja if I18n.locale == :ja
+    return property_en if property_en.present?
+
+    property_ja if fallback
+  end
 end
