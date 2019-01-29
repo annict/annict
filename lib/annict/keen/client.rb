067d6f91f394cad08aff06caa20b0e5607524580
Koji Shimba <me@shimba.co>
2019-01-30 00:21:44 +0900

    Send more data to Keen

--- lib/annict/keen/client.rb
+++ lib/annict/keen/client.rb
@@ -17,20 +17,29 @@ module Annict
       end
 
       def publish(action, data = {})
-        SendKeenEventJob.perform_later(ACTIONS.fetch(action), build_data(data))
+        SendKeenEventJob.perform_later(ACTIONS.fetch(action), base_data.merge(data))
       end
 
       private
 
-      def build_data(data)
-        data = data.merge(
-          page_category: @page_category.presence || "unknown",
-          viewer: {
-            type: @user.present? ? "user" : "guest"
-          }
-        )
-        data[:user] = { id: @user.id, username: @user.username } if @user
-        data
+      def base_data
+        {
+          page_category: @page_category,
+          viewer_type: @user ? "user" : "guest",
+          user_id: @user&.encoded_id,
+          user_weeks: @user&.weeks,
+          is_supporter: @user&.supporter? == true,
+          time_zone: @user&.time_zone,
+          locale: @user&.locale,
+          device: browser.device.mobile? ? "mobile" : "pc"
+        }
+      end
+
+      def browser
+        options = {
+          accept_language: @request.accept_language
+        }
+        @browser ||= Browser.new(@request.user_agent, options)
       end
     end
   end
