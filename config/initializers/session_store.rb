90155f2e65a18edcb5ad49eb7ad6ce5867ea0ffd
Koji Shimba <me@shimba.co>
2019-02-02 14:53:22 +0900

    Use new session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 # Be sure to restart your server when you modify this file.
 
 Annict::Application.config.session_store :redis_session_store,
-  key: "_ann_session",
+  key: "_annict_session",
   redis: {
     expire_after: 30.days,
     key_prefix: "annict:session:",
