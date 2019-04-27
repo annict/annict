90c5b58ba18077c19c3a34fcc7cd712f4c754f7d
Koji Shimba <me@shimba.co>
2019-04-27 18:24:40 +0900

    Set asset_host in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.action_controller.asset_host = ENV.fetch("ANNICT_ASSET_URL")
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
