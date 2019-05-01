5dff2901ae334a297b9a7cff71d51b88ee7f795c
Koji Shimba <me@shimba.co>
2019-05-02 02:20:48 +0900

    Remove Action Cable

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require_relative "boot"
   action_controller/railtie
   action_mailer/railtie
   action_view/railtie
-  action_cable/engine
   sprockets/railtie
 ).each do |railtie|
   require railtie
