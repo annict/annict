ec2f33a38694ed17aca5a511f47e89fa614e3f6d
chuross <azarasi.mo.mo@gmail.com>
2019-05-15 17:40:37 +0900

    Fix CreateReview mutation

--- app/graphql/mutations/create_review.rb
+++ app/graphql/mutations/create_review.rb
@@ -34,9 +34,11 @@ module Mutations
         r.work = work
         r.title = title
         r.body = body
-        WorkRecord::STATES.each do |state|
-          r.send("#{state}=".to_sym, inputs[state.to_s.camelcase(:lower).to_sym]&.downcase)
-        end
+        r.rating_overall_state = rating_overall_state
+        r.rating_animation_state = rating_animation_state
+        r.rating_music_state = rating_music_state
+        r.rating_story_state = rating_story_state
+        r.rating_character_state = rating_character_state
         r.oauth_application = context[:doorkeeper_token].application
       end
       context[:viewer].setting.attributes = {
