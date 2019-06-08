57b001e8c1630b8da8836a46b5168e5df34d4046
Koji Shimba <me@shimba.co>
2019-06-09 03:22:02 +0900

    Add MemberConstraint and GuestConstraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
+module ActionDispatch
+  module Routing
+    class Mapper
+      def draw(routes_name)
+        instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+      end
+    end
+  end
+end
 
 Rails.application.routes.draw do
-  root "welcome#show"
+  draw :web
 end
---
+++ config/routes/web.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class MemberConstraint
+  def matches?(request)
+    # !User.without_deleted.find_by_session(request.session).nil?
+    false
+  end
+end
+
+class GuestConstraint
+  def matches?(request)
+    !MemberConstraint.new.matches?(request)
+  end
+end
+
+root "home#show", constraints: MemberConstraint.new
+root "welcome#show", constraints: GuestConstraint.new, as: nil # Set :as option to avoid two routes with the same name
