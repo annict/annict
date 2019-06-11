f47214eaab8dad0368756246b0c91e5ecb276c33
Koji Shimba <me@shimba.co>
2019-06-12 01:44:38 +0900

    Set ANNICT_ASSET_URL

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  config.action_controller.asset_host = ENV.fetch("ANNICT_ASSET_URL")
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
