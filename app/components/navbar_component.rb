8e38b50b208e4c8c13a8d17a5c0e5559dfc997be
Koji Shimba <me@shimba.co>
2019-06-22 23:13:06 +0900

    Update navbar style

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -3,15 +3,25 @@
 class NavbarComponent < ApplicationComponent
   def self.template
     <<~ERB
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow-sm">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-toggler">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Hidden brand</a>
+        <div class="collapse navbar-collapse" id="navbar-toggler">
+          <%= link_to root_path, class: "navbar-brand" do %>
+            <%= image_tag asset_bundle_path("images/logo-color-mizuho.png"), size: "25x30", alt: "Annict" %>
+          <% end %>
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item dropdown active">
+              <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown">
+                <%= t("verb.explore") %>
+              </a>
+              <div class="dropdown-menu">
+                <%= link_to t("noun.current_season"), "", class: "dropdown-item" %>
+                <%= link_to t("noun.next_season"), "", class: "dropdown-item" %>
+                <%= link_to t("noun.prev_season"), "", class: "dropdown-item" %>
+                <%= link_to t("verb.search"), "", class: "dropdown-item" %>
+              </div>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
