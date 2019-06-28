5f7a5437bd6dcf24774970c580ac02b4c8805dd3
Koji Shimba <me@shimba.co>
2019-06-28 12:10:54 +0900

    Remove console.log

--- app/frontend/javascripts/v3/controllers/user_menu_controller.js
+++ app/frontend/javascripts/v3/controllers/user_menu_controller.js
@@ -3,12 +3,10 @@ import $ from 'jquery'
 
 export default class extends Controller {
   connect() {
-    console.log('stimulus:connected', this.element)
     $.ajax({
       method: 'GET',
       url: '/api/internal/v3/me/user_menu',
     }).done(data => {
-      console.log('data: ', data)
       this.element.innerHTML = data
     })
   }
