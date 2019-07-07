3630b84fd67926e976f52d682a312f1447348587
Koji Shimba <me@shimba.co>
2019-07-08 01:31:10 +0900

    Display synopsis

---
+++ app/decorators/work_object_decorator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module WorkObjectDecorator
+  def local_synopsis(raw: false)
+    text = case I18n.locale
+    when :en then synopsis_en
+    else synopsis
+    end
+
+    return if text.blank?
+
+    raw ? text : simple_format(text)
+  end
+end
