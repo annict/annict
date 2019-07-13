c661aa2a50d533405f0b7a603521f57126608882
Koji Shimba <me@shimba.co>
2019-07-13 17:51:31 +0900

    Make struct keys optional

--- app/structs/application_struct.rb
+++ app/structs/application_struct.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationStruct < Dry::Struct
+  transform_types do |type|
+    type.required(false)
+  end
+
   def decorate
     ActiveDecorator::Decorator.instance.decorate(self)
   end
--- app/structs/staff_struct.rb
+++ app/structs/staff_struct.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class StaffStruct < ApplicationStruct
-  attribute :person, PersonStruct.optional
-  attribute :organization, OrganizationStruct.optional
+  attribute :person, PersonStruct
+  attribute :organization, OrganizationStruct
   attribute :role_text, StructTypes::Strict::String
 end
