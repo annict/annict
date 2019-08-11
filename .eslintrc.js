17819b983cc292c42ce121014d51b2d0a64bd60a
Koji Shimba <me@shimba.co>
2019-08-11 17:19:01 +0900

    Set `'@typescript-eslint/indent': ['error', 2]`

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/indent': ['error', 2],
     'import/no-default-export': 'error',
   },
 }
