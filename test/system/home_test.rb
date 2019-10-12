d48185a733c9ab6f98ef9747dc139d13fe2dd727
Koji Shimba <me@shimba.co>
2019-10-12 22:33:20 +0900

    Update home test

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -1,48 +1,61 @@
 # frozen_string_literal: true
 
-describe "Top page" do
-  context "user does not sign in" do
-    let!(:work) { create(:work, :with_current_season) }
+require "application_system_test_case"
 
-    before do
-      visit "/"
-    end
-
-    it "displays the hero words" do
-      expect(page).to have_content("The platform for anime addicts.")
-    end
+class HomeTest < ApplicationSystemTestCase
+  setup do
+    @work = create(:work, :with_current_season)
   end
 
-  context "user signs in" do
-    let(:user) { create(:registered_user) }
-
-    before do
-      login_as(user, scope: :user)
-    end
-
-    describe "activity" do
-      context "no activities" do
-        before do
-          visit "/"
-        end
-
-        it "displays no activities message" do
-          expect(page).to have_content("アクティビティはありません")
-        end
-      end
-
-      context "has activities" do
-        let!(:activity) { create(:create_episode_record_activity, user: user) }
-
-        before do
-          visit "/"
-        end
-
-        it "displays activities" do
-          content = "#{user.profile.name}が#{activity.work.title}#{activity.episode.number}を見ました"
-          expect(page).to have_content(content)
-        end
-      end
-    end
+  test "should display the hero words" do
+    visit "/"
+    assert_text "The platform for anime addicts."
   end
 end
+
+# describe "Top page" do
+#   context "user does not sign in" do
+#     let!(:work) { create(:work, :with_current_season) }
+#
+#     before do
+#       visit "/"
+#     end
+#
+#     it "displays the hero words" do
+#       expect(page).to have_content("The platform for anime addicts.")
+#     end
+#   end
+#
+#   context "user signs in" do
+#     let(:user) { create(:registered_user) }
+#
+#     before do
+#       login_as(user, scope: :user)
+#     end
+#
+#     describe "activity" do
+#       context "no activities" do
+#         before do
+#           visit "/"
+#         end
+#
+#         it "displays no activities message" do
+#           expect(page).to have_content("アクティビティはありません")
+#         end
+#       end
+#
+#       context "has activities" do
+#         let!(:activity) { create(:create_episode_record_activity, user: user) }
+#
+#         before do
+#           visit "/"
+#         end
+#
+#         it "displays activities" do
+#           content = "#{user.profile.name}が#{activity.work.title}#{activity.episode.number}を見ました"
+#           expect(page).to have_content(content)
+#         end
+#       end
+#     end
+#   end
+# end
