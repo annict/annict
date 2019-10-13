d4e803885cbedcd54ae1313084902a1c0edf5098
Koji Shimba <me@shimba.co>
2019-10-13 21:38:36 +0900

    Remove callback from factory

--- spec/factories/episode.rb
+++ spec/factories/episode.rb
@@ -5,7 +5,5 @@ FactoryBot.define do
     work
     sequence(:number) { |n| "第#{n}話" }
     sequence(:title)  { |n| "Yes! プリキュア#{n}" }
-
-    before(:create) { |e| e.work = e.work.presence || create(:work) }
   end
 end
