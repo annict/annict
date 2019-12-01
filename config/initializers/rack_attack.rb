c72824060aa12376dd6b083ec377f4db1ce31c6d
Koji Shimba <me@shimba.co>
2019-12-01 16:33:37 +0900

    Add rack-attack gem

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def remote_ip(req)
+  (req.env["action_dispatch.remote_ip"] || req.ip).to_s
+end
+
+Rack::Attack.throttle("requests by ip", limit: 5, period: 2.seconds) do |req|
+  remote_ip(req)
+end
+
+ActiveSupport::Notifications.subscribe(/rack_attack/) do |_name, _start, _finish, _request_id, payload|
+  req = payload[:request]
+
+  next unless %i(throttle blacklist).include? req.env["rack.attack.match_type"]
+
+  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}): #{remote_ip(req)} #{req.request_method} #{req.fullpath}")
+end
