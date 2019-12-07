d351234373bddf185739e6bc7d08779485ec4172
Koji Shimba <me@shimba.co>
2019-12-07 23:44:13 +0900

    Remove StatusCommon module

--- app/models/status.rb
+++ app/models/status.rb
@@ -27,10 +27,30 @@
 #
 
 class Status < ApplicationRecord
-  include StatusCommon
+  extend Enumerize
+
   include Shareable
 
+  KIND_MAPPING = {
+    wanna_watch: :plan_to_watch,
+    watching: :watching,
+    watched: :completed,
+    on_hold: :on_hold,
+    stop_watching: :dropped,
+    no_select: :no_status
+  }.freeze
+
+  enumerize :kind, scope: true, in: {
+    wanna_watch: 1,
+    watching: 2,
+    watched: 3,
+    on_hold: 5,
+    stop_watching: 4
+  }
+
   belongs_to :oauth_application, class_name: "Doorkeeper::Application", optional: true
+  belongs_to :user
+  belongs_to :work
   has_many :activities,
     dependent: :destroy,
     as: :trackable
@@ -43,6 +63,21 @@ class Status < ApplicationRecord
   after_create :save_activity
   after_create :save_library_entry
   after_create :update_channel_work
+
+  scope :positive, -> { with_kind(:wanna_watch, :watching, :watched) }
+  scope :with_not_deleted_work, -> { joins(:work).merge(Work.without_deleted) }
+
+  def self.kind_v2_to_v3(kind_v2)
+    return if kind_v2.blank?
+
+    KIND_MAPPING[kind_v2.to_sym]
+  end
+
+  def self.kind_v3_to_v2(kind_v3)
+    return if kind_v3.blank?
+
+    KIND_MAPPING.invert[kind_v3.to_sym]
+  end
 
   def self.initial
     order(:id).first
