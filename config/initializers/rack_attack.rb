5eab4cd019bbad9dee21a4b6691dcab1398c808b
Koji Shimba <me@shimba.co>
2019-12-07 16:15:45 +0900

    Update rack attack config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
+BLOCK_USER_AGENTS = %w(
+  BLEXBot
+).freeze
+
+BLOCK_IPS = %w(
+  13.230.0.0/15
+  144.76.0.0/16
+  216.244.66.0/24
+  3.224.0.0/12
+  52.194.0.0/15
+  94.130.0.0/16
+).freeze
+
 def remote_ip(req)
-  (req.env["action_dispatch.remote_ip"] || req.ip).to_s
+  @remote_ip ||= (req.env["action_dispatch.remote_ip"] || req.ip).to_s
+end
+
+def include_ua?(req)
+  BLOCK_USER_AGENTS.any? { |ua| req.user_agent.include?(ua) }
+end
+
+def include_ip?(req)
+  BLOCK_IPS.any? { |ip| IPAddr.new(ip).include?(remote_ip(req)) }
+end
+
+Rack::Attack.blocklist("block all access from specific user agents and ips") do |req|
+  include_ua?(req) || include_ip?(req)
 end
 
 Rack::Attack.throttle("requests by ip", limit: 5, period: 2.seconds) do |req|
