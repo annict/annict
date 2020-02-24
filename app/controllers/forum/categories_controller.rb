9d039db6b7be6f8e4acc5cbff5b6027ac0439eb6
Koji Shimba <me@shimba.co>
2020-02-24 23:48:25 +0900

    Fix forum category page

--- app/controllers/forum/categories_controller.rb
+++ app/controllers/forum/categories_controller.rb
@@ -4,7 +4,8 @@ module Forum
   class CategoriesController < Forum::ApplicationController
     def show
       @category = ForumCategory.find_by!(slug: params[:id])
-      @posts = localable_resources(@category.forum_posts).order(last_commented_at: :desc).page(params[:page])
+      @posts = @category.forum_posts.joins(:user).merge(User.without_deleted)
+      @posts = localable_resources(@posts).order(last_commented_at: :desc).page(params[:page])
     end
   end
 end
