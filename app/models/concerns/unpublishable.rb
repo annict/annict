963c13855709bd79d65acd3cc86e3666f3fc288f
Koji Shimba <me@shimba.co>
2020-03-22 03:05:13 +0900

    Undef only_kept

--- app/models/concerns/unpublishable.rb
+++ app/models/concerns/unpublishable.rb
@@ -6,6 +6,9 @@ module Unpublishable
   include SoftDeletable
 
   included do
+    # Unpublishable.only_kept を定義するため、SoftDeletable.only_kept の定義を取り消す
+    class << self; undef :only_kept; end
+
     scope :published, -> { where(unpublished_at: nil) }
     scope :unpublished, -> { where.not(unpublished_at: nil) }
     scope :only_kept, -> { without_deleted.published }
