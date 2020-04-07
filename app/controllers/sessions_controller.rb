2d4bec213b7fb711a1500f66299a36d26911a983
Koji Shimba <me@shimba.co>
2020-04-08 01:46:44 +0900

    Require email confirmation after sign up for recent signed up users
    ref: https://github.com/annict/annict/commit/2175ed9b68902c2c9df6fe7e215ffd279efc187c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,13 @@ class SessionsController < Devise::SessionsController
     store_location_for(:user, params[:back]) if params[:back].present?
     super
   end
+
+  def create
+    super do |user|
+      if !user.confirmed? && user.registered_after_email_confirmation_required?
+        sign_out
+        return redirect_to root_path, alert: t("devise.failure.user.unconfirmed")
+      end
+    end
+  end
 end
