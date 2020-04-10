be55dea92d6cb2dcf5bf3baeaacc3b620b414471
Koji Shimba <me@shimba.co>
2020-04-11 08:12:09 +0900

    Continue to use Vue

--- app/frontend/javascripts/db.js
+++ app/frontend/javascripts/db.js
@@ -1,12 +1,18 @@
 import 'bootstrap';
+
 import ujs from '@rails/ujs';
 import Turbolinks from 'turbolinks';
+import Vue from 'vue';
 
 document.addEventListener('turbolinks:load', _event => {
   window.WebFont.load({
     google: {
       families: ['Raleway'],
     },
+  });
+
+  new Vue({
+    el: '.f-app',
   });
 });
 
