0b44750aa6a6dbad3e1cc772cc76f7e97b3fb136
Koji Shimba <me@shimba.co>
2020-04-11 08:14:54 +0900

    Add resource selector component

--- app/frontend/javascripts/db.js
+++ app/frontend/javascripts/db.js
@@ -4,12 +4,16 @@ import ujs from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import Vue from 'vue';
 
+import ResourceSelector from './db/components/ResourceSelector';
+
 document.addEventListener('turbolinks:load', _event => {
   window.WebFont.load({
     google: {
       families: ['Raleway'],
     },
   });
+
+  Vue.component('c-resource-selector', ResourceSelector);
 
   new Vue({
     el: '.f-app',
