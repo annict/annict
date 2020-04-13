0099faa03f2f2126740a20eedb8d70d49736fe69
Koji Shimba <me@shimba.co>
2020-04-13 16:10:24 +0900

    Rename block variable

--- app/services/episode_generator_service.rb
+++ app/services/episode_generator_service.rb
@@ -14,17 +14,17 @@ class EpisodeGeneratorService
       before(now + 7.days, field: :started_at).
       includes(:work)
 
-    episodeless_slots.order(:started_at).each do |p|
-      work = p.work
+    episodeless_slots.order(:started_at).each do |slot|
+      work = slot.work
 
-      next if work.manual_episodes_count && work.manual_episodes_count < p.number
+      next if work.manual_episodes_count && work.manual_episodes_count < slot.number
 
       irregular_slots_count = Slot.
         without_deleted.
-        where(program_id: p.program_id, irregular: true).
-        where("number >= ?", p.program.minimum_episode_generatable_number).
+        where(program_id: slot.program_id, irregular: true).
+        where("number >= ?", slot.program.minimum_episode_generatable_number).
         count
-      raw_number = p.number - irregular_slots_count
+      raw_number = slot.number - irregular_slots_count
       raw_number = raw_number + work.start_episode_raw_number - 1
       episode = work.episodes.without_deleted.find_by(raw_number: raw_number)
       episode_not_exists = episode.nil?
@@ -33,10 +33,10 @@ class EpisodeGeneratorService
         episode ||= work.episodes.create!(
           raw_number: raw_number,
           number: work.formatted_number(raw_number).presence || "第#{raw_number.to_i}話",
-          sort_number: p.number * 100
+          sort_number: slot.number * 100
         )
 
-        p.update_column(:episode_id, episode.id)
+        slot.update_column(:episode_id, episode.id)
       end
 
       AdminMailer.episode_created_notification(episode.id).deliver_later if episode_not_exists
