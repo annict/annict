f70c9c5e4137119ef04ad325df23ad9174f2f8af
Koji Shimba <me@shimba.co>
2020-04-24 16:18:09 +0900

    Replace mobile navbar with tab bar

---
+++ app/components/tab_bar_component.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+class TabBarComponent < ApplicationComponent
+  def initialize(user:)
+    @user = user
+  end
+
+  def call
+    Htmlrb.build do |el|
+      el.div class: "c-tab-bar" do
+        el.div class: "bg-white d-block d-sm-none h-100 navbar navbar-expand navbar-white px-0" do
+          el.ul class: "align-items-center h-100 justify-content-around navbar-nav" do
+            el.li class: "col nav-item px-0 text-center" do
+              el.a class: "text-dark", href: "/" do
+                el.i(class: "fal fa-home") {}
+                el.div class: "mt-1 small" do
+                  t "noun.home"
+                end
+              end
+            end
+
+            if user
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/programs" do
+                  el.i(class: "fal fa-calendar") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.slots"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/@#{user.username}/watching" do
+                  el.i(class: "fal fa-play") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.watching_shorten"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/works/#{ENV.fetch('ANNICT_CURRENT_SEASON')}" do
+                  el.i(class: "fal fa-tv") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.airing"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/menu" do
+                  el.i(class: "fal fa-th") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.menu"
+                  end
+                end
+              end
+            else
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/works/#{ENV.fetch('ANNICT_CURRENT_SEASON')}" do
+                  el.i(class: "fal fa-tv") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.airing"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/sign_up" do
+                  el.i(class: "fal fa-rocket") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.sign_up_shorten"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/about" do
+                  el.i(class: "fal fa-lightbulb") {}
+                  el.div class: "mt-1 small" do
+                    t "noun.about"
+                  end
+                end
+              end
+
+              el.li class: "col nav-item px-0 text-center" do
+                el.a class: "text-dark", href: "/menu" do
+                  el.i(class: "fal fa-th") {}
+                  el.div class: "small mt-1" do
+                    t "noun.menu"
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end.html_safe
+  end
+
+  private
+
+  attr_reader :user
+end
