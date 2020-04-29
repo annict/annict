23521047a38af69f3a1984248428306f9731e098
Koji Shimba <me@shimba.co>
2020-04-30 06:18:43 +0900

    Make layout keep even if JavaScript is disabled

--- app/frontend/javascript/web/components/sidebar.js
+++ app/frontend/javascript/web/components/sidebar.js
@@ -3,12 +3,10 @@ import eventHub from '../../common/eventHub';
 export default {
   template: `
     <div class="c-sidebar">
-      <div class="c-sidebar__background" v-on:click="hideSidebar" v-show="isSidebarOpen"></div>
-      <transition name="slide">
-        <div class="c-sidebar__content" v-show="isSidebarOpen">
-          <slot></slot>
-        </div>
-      </transition>
+      <slot
+        v-bind:hideSidebar="hideSidebar"
+        v-bind:isSidebarOpen="isSidebarOpen"
+      ></slot>
     </div>
   `,
 
