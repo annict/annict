84503f85246a39a6bd9e7ae8dd1b651cd46fed03
Koji Shimba <me@shimba.co>
2020-05-01 02:00:34 +0900

    Cache work and channels data

--- app/controllers/v4/works_controller.rb
+++ app/controllers/v4/works_controller.rb
@@ -3,9 +3,36 @@
 module V4
   class WorksController < V4::ApplicationController
     def show
-      @work = WorkDetail::WorkRepository.new(graphql_client: graphql_client).fetch(work_id: params[:id])
-      @vod_channels = WorkDetail::VodChannelsRepository.new(graphql_client: graphql_client).fetch(work: @work)
+      work = Work.only_kept.find(params[:id])
+
+      @work = Rails.cache.fetch(work_detail_work_cache_key(work), expires_in: 3.hours) do
+        WorkDetail::WorkRepository.new(graphql_client: graphql_client).fetch(work_id: work.id)
+      end
+
+      @vod_channels = Rails.cache.fetch(work_detail_vod_channels_cache_key(work), expires_in: 3.hours) do
+        WorkDetail::VodChannelsRepository.new(graphql_client: graphql_client).fetch(work: @work)
+      end
       @existing_vod_channels = @vod_channels.select { |vod_channel| vod_channel.programs.first.present? }
+    end
+
+    private
+
+    def work_detail_work_cache_key(work)
+      [
+        "work-detail",
+        "work",
+        work.id,
+        work.updated_at.rfc3339
+      ].freeze
+    end
+
+    def work_detail_vod_channels_cache_key(work)
+      [
+        "work-detail",
+        "vod-channels",
+        work.id,
+        work.updated_at.rfc3339
+      ].freeze
     end
   end
 end
