eafb3c9a9e7ebaa05fcce8ac822eac1bb9bb4cd1
Koji Shimba <me@shimba.co>
2020-05-01 12:49:22 +0900

    New "No Image" image

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:name) { |n| "人造人間#{n}号" }
     description { "悟空を倒すために生まれました。よろしくお願いします。" }
     url { "http://example.com" }
-    image { File.open("#{Rails.root}/public/images/no_image.png") }
-    background_image { File.open("#{Rails.root}/public/images/no_image.png") }
+    image { File.open("#{Rails.root}/public/images/no-image.jpg") }
+    background_image { File.open("#{Rails.root}/public/images/no-image.jpg") }
   end
 end
