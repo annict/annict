9dff6d2d865b11629b982972c0c6f9993817f2cf
Koji Shimba <me@shimba.co>
2020-05-01 23:40:11 +0900

    Make number_text optional

--- app/entities/episode_entity.rb
+++ app/entities/episode_entity.rb
@@ -4,7 +4,7 @@ class EpisodeEntity < ApplicationEntity
   local_attributes :title
 
   attribute? :id, Types::Integer
-  attribute? :number_text, Types::String
+  attribute? :number_text, Types::String.optional
   attribute? :title, Types::String.optional
   attribute? :title_en, Types::String.optional
 end
