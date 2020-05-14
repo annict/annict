416025d5c86ffd9f5da5989b61edb43a861e3ce4
Koji Shimba <me@shimba.co>
2020-05-14 19:09:11 +0900

    Use counter_culture on notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -24,25 +24,11 @@
 #
 
 class Notification < ApplicationRecord
+  counter_culture :user
+
   belongs_to :action_user, class_name: 'User'
   belongs_to :trackable,   polymorphic: true
   belongs_to :user
 
   scope :unread, -> { where(read: false) }
-
-  after_create :increment_notifications_count
-  after_destroy :decrement_notifications_count
-
-
-  private
-
-  # 通知ページにアクセスしたとき、通知数をリセットするので、
-  # read onlyになるカウンターキャッシュを使用せず、自前でインクリメントする
-  def increment_notifications_count
-    user.increment!(:notifications_count)
-  end
-
-  def decrement_notifications_count
-    user.decrement!(:notifications_count) if user.notifications_count > 0
-  end
 end
