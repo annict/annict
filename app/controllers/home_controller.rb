59c0381535305f96f9cf0c47e8e68fc2adb6485a
Koji Shimba <me@shimba.co>
2020-05-18 20:28:17 +0900

    Display Userland projects on user home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,8 @@ class HomeController < ApplicationController
       merge(ForumCategory.with_slug(:site_news))
     @forum_posts = localable_resources(@forum_posts).order(created_at: :desc).limit(5)
 
+    @userland_projects = UserlandProject.where(id: UserlandProject.pluck(:id).sample(3))
+
     @activity_group_result = if current_user.timeline_mode.following?
       UserHome::FetchFollowingActivityGroupsRepository.new(
         graphql_client: graphql_client(viewer: current_user)
