7458b38fbf39f5f8a98541e90d2a2692ac85ad07
Koji Shimba <me@shimba.co>
2020-05-18 20:59:36 +0900

    Fix spoiler guard when visitors do not sign in

--- app/controllers/api/internal/tracked_resources_controller.rb
+++ app/controllers/api/internal/tracked_resources_controller.rb
@@ -3,9 +3,9 @@
 module Api
   module Internal
     class TrackedResourcesController < Api::Internal::ApplicationController
-      before_action :authenticate_user!
-
       def index
+        return render(json: []) unless user_signed_in?
+
         render json: {
           episode_ids: current_user.episode_records.pluck(:episode_id),
           work_ids: current_user.work_records.pluck(:work_id)
