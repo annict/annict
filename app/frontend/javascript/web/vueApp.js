f6ee8b6eb1c7fdebf35e41e928bf2c6cff7be1f0
Koji Shimba <me@shimba.co>
2020-05-24 07:43:34 +0900

    Make components global
    アクティビティの「もっと見る」をクリックして動的にコンポーネントを読み込んだときグローバルになっていないとエラーになってしまうため。

--- app/frontend/javascript/web/vueApp.js
+++ app/frontend/javascript/web/vueApp.js
@@ -24,32 +24,30 @@ import workWatchersChart from './components/workWatchersChart';
 
 export default {
   start() {
+    Vue.component('c-activity-group-more-button', activityGroupMoreButton);
+    Vue.component('c-activity-group-more-content', activityGroupMoreContent);
+    Vue.component('c-activity-more-button', activityMoreButton);
+    Vue.component('c-activity-more-content', activityMoreContent);
+    Vue.component('c-flash', flash);
+    Vue.component('c-follow-button', followButton);
+    Vue.component('c-like-button', likeButton);
+    Vue.component('c-rating-label', ratingLabel);
+    Vue.component('c-relative-time', relativeTime);
+    Vue.component('c-share-to-facebook-button', shareToFacebookButton);
+    Vue.component('c-share-to-twitter-button', shareToTwitterButton);
+    Vue.component('c-sidebar', sidebar);
+    Vue.component('c-sign-up-modal', signUpModal);
+    Vue.component('c-spoiler-guard', spoilerGuard);
+    Vue.component('c-status-selector', statusSelector);
+    Vue.component('c-tab-bar', tabBar);
+    Vue.component('c-user-actions-dropdown', userActionsDropdown);
+    Vue.component('c-user-data-fetcher', userDataFetcher);
+    Vue.component('c-user-heatmap', userHeatmap);
+    Vue.component('c-work-status-chart', workStatusChart);
+    Vue.component('c-work-watchers-chart', workWatchersChart);
+
     new Vue({
       el: '.ann-application',
-
-      components: {
-        'c-activity-group-more-button': activityGroupMoreButton,
-        'c-activity-group-more-content': activityGroupMoreContent,
-        'c-activity-more-button': activityMoreButton,
-        'c-activity-more-content': activityMoreContent,
-        'c-flash': flash,
-        'c-follow-button': followButton,
-        'c-like-button': likeButton,
-        'c-rating-label': ratingLabel,
-        'c-relative-time': relativeTime,
-        'c-share-to-facebook-button': shareToFacebookButton,
-        'c-share-to-twitter-button': shareToTwitterButton,
-        'c-sidebar': sidebar,
-        'c-sign-up-modal': signUpModal,
-        'c-spoiler-guard': spoilerGuard,
-        'c-status-selector': statusSelector,
-        'c-tab-bar': tabBar,
-        'c-user-actions-dropdown': userActionsDropdown,
-        'c-user-data-fetcher': userDataFetcher,
-        'c-user-heatmap': userHeatmap,
-        'c-work-status-chart': workStatusChart,
-        'c-work-watchers-chart': workWatchersChart,
-      },
     });
   },
 };
