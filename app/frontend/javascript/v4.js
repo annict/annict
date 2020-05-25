9bb9214715bdd46bbbd25e87c89609223194de32
Koji Shimba <me@shimba.co>
2020-05-26 05:12:45 +0900

    tweak

--- app/frontend/javascript/v4.js
+++ app/frontend/javascript/v4.js
@@ -2,13 +2,13 @@ import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
 
 import ujs from '@rails/ujs';
+import $ from "jquery";
+import Cookies from "js-cookie";
+import moment from "moment-timezone";
 import Turbolinks from 'turbolinks';
 
 import vueApp from './web/vueApp';
 import lazyLoad from './web/utils/lazyLoad';
-import moment from "moment-timezone";
-import Cookies from "js-cookie";
-import $ from "jquery";
 
 document.addEventListener('turbolinks:load', (_event) => {
   moment.locale(AnnConfig.viewer.locale);
