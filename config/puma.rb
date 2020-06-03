e07f1ba7146a98b6d6c8da35e983a7f3fa6cd8ca
Koji Shimba <me@shimba.co>
2020-06-03 12:15:29 +0900

    Remove on_worker_boot config
    https://github.com/puma/puma-heroku/blob/63a49eabf9288031439490bc168baa3e3c7c93d5/lib/puma/plugin/heroku.rb

--- config/puma.rb
+++ config/puma.rb
@@ -28,12 +28,6 @@ preload_app!
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
-
 before_fork do
   require "puma_worker_killer"
 
