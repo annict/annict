935bad47d5d3e4e794bea3e50059b2bcef3fdba9
Koji Shimba <me@shimba.co>
2020-07-18 23:47:30 +0900

    Make password reset V4

---
+++ app/controllers/v4/passwords_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module V4
+  class PasswordsController < Devise::PasswordsController
+    layout "simple"
+  end
+end
--- config/routes/web.rb
+++ config/routes/web.rb
@@ -9,7 +9,6 @@ devise_for :users,
   skip: %i(passwords registrations sessions)
 
 devise_scope :user do
-  resource :password, only: %i(new create edit update)
   resources :oauth_users, only: %i(new create)
 end
 
@@ -156,11 +155,16 @@ root "welcome#show",
 scope module: :v4 do
   constraints format: "html" do
     devise_scope :user do
-      match "/sign_in",  via: :get,    as: :sign_in,           to: "sessions#new"
-      match "/sign_in",  via: :post,   as: :user_session,      to: "sessions#create"
-      match "/sign_out", via: :delete, as: :sign_out,          to: "sessions#destroy"
-      match "/sign_up",  via: :get,    as: :sign_up,           to: "registrations#new"
-      match "/sign_up",  via: :post,   as: :user_registration, to: "registrations#create"
+      match "/password",      via: :patch,  as: :password,          to: "passwords#update"
+      match "/password",      via: :post,                           to: "passwords#create"
+      match "/password/edit", via: :get,    as: :edit_password,     to: "passwords#edit"
+      match "/password/new",  via: :get,    as: :new_password,      to: "passwords#new"
+      match "/sign_in",       via: :get,    as: :new_user_session,  to: "sessions#new"
+      match "/sign_in",       via: :get,    as: :sign_in,           to: "sessions#new"
+      match "/sign_in",       via: :post,   as: :user_session,      to: "sessions#create"
+      match "/sign_out",      via: :delete, as: :sign_out,          to: "sessions#destroy"
+      match "/sign_up",       via: :get,    as: :sign_up,           to: "registrations#new"
+      match "/sign_up",       via: :post,   as: :user_registration, to: "registrations#create"
     end
 
     match "/@:username",         via: :get,   as: :profile_detail, to: "users#show",    username: USERNAME_FORMAT
