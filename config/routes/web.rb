e7d9973cf7c3f59d2d9cc37bea67325f23b33a7e
Koji Shimba <me@shimba.co>
2020-07-18 23:59:31 +0900

    Make sign up via oauth V4

--- config/routes/web.rb
+++ config/routes/web.rb
@@ -8,10 +8,6 @@ devise_for :users,
   controllers: { omniauth_callbacks: :callbacks },
   skip: %i(passwords registrations sessions)
 
-devise_scope :user do
-  resources :oauth_users, only: %i(new create)
-end
-
 use_doorkeeper do
   controllers applications: "oauth/applications"
   skip_controllers :authorized_applications
@@ -155,16 +151,18 @@ root "welcome#show",
 scope module: :v4 do
   constraints format: "html" do
     devise_scope :user do
-      match "/password",      via: :patch,  as: :password,          to: "passwords#update"
-      match "/password",      via: :post,                           to: "passwords#create"
-      match "/password/edit", via: :get,    as: :edit_password,     to: "passwords#edit"
-      match "/password/new",  via: :get,    as: :new_password,      to: "passwords#new"
-      match "/sign_in",       via: :get,    as: :new_user_session,  to: "sessions#new"
-      match "/sign_in",       via: :get,    as: :sign_in,           to: "sessions#new"
-      match "/sign_in",       via: :post,   as: :user_session,      to: "sessions#create"
-      match "/sign_out",      via: :delete, as: :sign_out,          to: "sessions#destroy"
-      match "/sign_up",       via: :get,    as: :sign_up,           to: "registrations#new"
-      match "/sign_up",       via: :post,   as: :user_registration, to: "registrations#create"
+      match "/oauth_users",     via: :post,   as: :oauth_users,       to: "oauth_users#create"
+      match "/oauth_users/new", via: :get,    as: :new_oauth_user,    to: "oauth_users#new"
+      match "/password",        via: :patch,  as: :password,          to: "passwords#update"
+      match "/password",        via: :post,                           to: "passwords#create"
+      match "/password/edit",   via: :get,    as: :edit_password,     to: "passwords#edit"
+      match "/password/new",    via: :get,    as: :new_password,      to: "passwords#new"
+      match "/sign_in",         via: :get,    as: :new_user_session,  to: "sessions#new"
+      match "/sign_in",         via: :get,    as: :sign_in,           to: "sessions#new"
+      match "/sign_in",         via: :post,   as: :user_session,      to: "sessions#create"
+      match "/sign_out",        via: :delete, as: :sign_out,          to: "sessions#destroy"
+      match "/sign_up",         via: :get,    as: :sign_up,           to: "registrations#new"
+      match "/sign_up",         via: :post,   as: :user_registration, to: "registrations#create"
     end
 
     match "/@:username",         via: :get,   as: :profile_detail, to: "users#show",    username: USERNAME_FORMAT
