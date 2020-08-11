3202408e3acaae99a66516de632988034402643c
Koji Shimba <me@shimba.co>
2020-08-11 20:44:21 +0900

    Update expose-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -42,16 +42,10 @@ module.exports = {
       },
       {
         test: require.resolve('jquery'),
-        use: [
-          {
-            loader: 'expose-loader',
-            options: 'jQuery',
-          },
-          {
-            loader: 'expose-loader',
-            options: '$',
-          },
-        ],
+        loader: 'expose-loader',
+        options: {
+          exposes: ['$', 'jQuery'],
+        },
       },
       {
         test: /\.scss$/,
