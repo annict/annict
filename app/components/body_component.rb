fdf199515a8432a680eeef9a27c59a40e8cc4217
Koji Shimba <me@shimba.co>
2020-10-03 23:05:21 +0900

    Support HTML format

--- app/components/body_component.rb
+++ app/components/body_component.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 class BodyComponent < ApplicationComponent
-  def initialize(height: nil, markdown: false, class_name: "")
+  def initialize(height: nil, format: :simple, class_name: "")
     @height = height
-    @markdown = markdown
+    @format = format
     @class_name = class_name
   end
 
   private
 
-  attr_reader :class_name, :height, :markdown
+  attr_reader :class_name, :height
 
   def render_content
-    return simple_format(content) unless markdown
-
-    helpers.render_markdown(content)
+    case @format
+    when :simple
+      simple_format(content)
+    when :markdown
+      helpers.render_markdown(content)
+    when :html
+      content
+    end
   end
 end
