d5a075cc4e5d581812413901262d333fea504731
Koji Shimba <me@shimba.co>
2020-10-07 08:15:50 +0900

    Show other comments

--- app/controllers/v4/episodes_controller.rb
+++ app/controllers/v4/episodes_controller.rb
@@ -13,6 +13,7 @@ module V4
       @episode_entity = Rails.cache.fetch(episode_cache_key(episode), expires_in: 3.days) do
         EpisodePage::EpisodeRepository.new(graphql_client: graphql_client).execute(database_id: episode.id)
       end
+      @other_record_entities = @episode_entity.records
 
       load_vod_channel_entities(anime: anime, anime_entity: @episode_entity.anime)
 
@@ -26,6 +27,8 @@ module V4
         @following_record_entities = Rails.cache.fetch(following_records_cache_key(current_user, episode), expires_in: 3.hours) do
           EpisodePage::FollowingRecordsRepository.new(graphql_client: graphql_client(viewer: current_user)).execute(episode_id: @episode_entity.id)
         end
+
+        @other_record_entities = filter_records(current_user, @other_record_entities, @my_record_entities + @following_record_entities)
       end
     end
 
@@ -58,6 +61,18 @@ module V4
         episode.id,
         user.id
       ].freeze
+    end
+
+    def filter_records(viewer, base_record_entities, record_entities)
+      muted_user_ids = viewer.mute_users.pluck(:muted_user_id)
+      record_ids = record_entities.pluck(:database_id)
+
+      base_record_entities.filter do |record_entity|
+        user_id = record_entity.user.database_id
+        record_id = record_entity.database_id
+
+        !user_id.in?(muted_user_ids) && !record_id.in?(record_ids)
+      end
     end
   end
 end
