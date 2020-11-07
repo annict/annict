f705cf071ce64ecc2d0924815aa42f76a79bd7d2
Koji Shimba <me@shimba.co>
2020-11-07 23:31:45 +0900

    Strip email

--- app/forms/sign_up_form.rb
+++ app/forms/sign_up_form.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class SignUpForm < ApplicationForm
-  attr_accessor :email, :back
+  attr_accessor :back
+  attr_reader :email
 
   validates :email, presence: true, email: true
   validate :back_must_be_a_path
+
+  def email=(email)
+    @email = email&.strip
+  end
 
   def back_must_be_a_path
     if back && !%r{\A/}.match?(back)
