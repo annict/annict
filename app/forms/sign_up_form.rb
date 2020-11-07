b208b5302b12cc94f327dfd62018d2be470443df
Koji Shimba <me@shimba.co>
2020-11-07 23:47:30 +0900

    Remove custom validator

--- app/forms/sign_up_form.rb
+++ app/forms/sign_up_form.rb
@@ -4,16 +4,10 @@ class SignUpForm < ApplicationForm
   attr_accessor :back
   attr_reader :email
 
+  validates :back, format: { with: %r{\A/} }
   validates :email, presence: true, email: true
-  validate :back_must_be_a_path
 
   def email=(email)
     @email = email&.strip
-  end
-
-  def back_must_be_a_path
-    if back && !%r{\A/}.match?(back)
-      errors.add(:back, :invalid_back_format)
-    end
   end
 end
