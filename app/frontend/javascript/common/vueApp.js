199c1d0e60a76b37e391b690a5c2880a272e8dce
Koji Shimba <me@shimba.co>
2020-11-16 02:39:15 +0900

    Remove old files

--- app/frontend/javascript/common/vueApp.js
+++ app/frontend/javascript/common/vueApp.js
@@ -1,21 +1,12 @@
 import Vue from 'vue';
 
-import autosizeTextarea from './components/autosizeTextarea';
 import channelReceiveButton from './components/channelReceiveButton';
 import episodeList from './components/episodeList';
-import episodeRatingStateChart from './components/episodeRatingStateChart';
-import episodeRecordsChart from './components/episodeRecordsChart';
 import favoriteButton from './components/favoriteButton';
 import followButton from './components/followButton';
 import impressionButton from './components/impressionButton';
 import impressionButtonModal from './components/impressionButtonModal';
 import inputWordsCount from './components/inputWordsCount';
-import muteUserButton from './components/muteUserButton';
-import record from './components/record';
-import recordRating from './components/recordRating';
-import recordSorter from './components/recordSorter';
-import recordTextarea from './components/recordTextarea';
-import recordWordCount from './components/recordWordCount';
 import userActionsDropdown from './components/userActionsDropdown';
 import userHeatmap from './components/userHeatmap';
 import workComment from './components/workComment';
@@ -27,22 +18,13 @@ import youtubeModalPlayer from './components/youtubeModalPlayer';
 
 export default {
   start() {
-    Vue.component('c-autosize-textarea', autosizeTextarea);
     Vue.component('c-channel-receive-button', channelReceiveButton);
     Vue.component('c-episode-list', episodeList);
-    Vue.component('c-episode-rating-state-chart', episodeRatingStateChart);
-    Vue.component('c-episode-records-chart', episodeRecordsChart);
     Vue.component('c-favorite-button', favoriteButton);
     Vue.component('c-follow-button', followButton);
     Vue.component('c-impression-button', impressionButton);
     Vue.component('c-impression-button-modal', impressionButtonModal);
     Vue.component('c-input-words-count', inputWordsCount);
-    Vue.component('c-mute-user-button', muteUserButton);
-    Vue.component('c-record', record);
-    Vue.component('c-record-rating', recordRating);
-    Vue.component('c-record-sorter', recordSorter);
-    Vue.component('c-record-textarea', recordTextarea);
-    Vue.component('c-record-word-count', recordWordCount);
     Vue.component('c-user-actions-dropdown', userActionsDropdown);
     Vue.component('c-user-heatmap', userHeatmap);
     Vue.component('c-work-comment', workComment);
