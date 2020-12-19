b7ff6f0a42b7f03005335f931b4e5ecfc1cf8f7b
Koji Shimba <me@shimba.co>
2020-12-19 20:30:51 +0900

    Use CreateEpisodeRecordService

--- app/graphql/canary/mutations/create_episode_record.rb
+++ app/graphql/canary/mutations/create_episode_record.rb
@@ -16,47 +16,25 @@ module Canary
         description: "エピソードへの記録をTwitterでシェアするかどうか"
 
       field :record, Canary::Types::Objects::RecordType, null: true
+      field :errors, [Canary::Types::Objects::ClientErrorType], null: false
 
       def resolve(episode_id:, comment: nil, rating: nil, share_to_twitter: nil)
         raise Annict::Errors::InvalidAPITokenScopeError unless context[:writable]
 
         viewer = context[:viewer]
         episode = Episode.only_kept.find_by_graphql_id(episode_id)
-        work = episode.work
 
-        episode_record = viewer.episode_records.new(
-          rating_state: rating&.downcase,
-          body: comment,
+        result = CreateEpisodeRecordService.new(
+          user: viewer,
+          episode: episode,
+          rating: rating,
+          comment: comment,
           share_to_twitter: share_to_twitter
-        )
-        episode_record.episode = episode
-        episode_record.work = work
-        episode_record.detect_locale!(:body)
-
-        library_entry = viewer.library_entries.find_by(work: work)
-
-        ActiveRecord::Base.transaction do
-          episode_record.record = viewer.records.create!(work: work)
-
-          unless episode_record.valid?
-            raise GraphQL::ExecutionError, episode_record.errors.full_messages.join(", ")
-          end
-
-          episode_record.save
-
-          activity_group = viewer.create_or_last_activity_group!(episode_record)
-          viewer.activities.create!(itemable: episode_record, activity_group: activity_group)
-
-          viewer.update_share_record_setting(share_to_twitter)
-          library_entry&.append_episode!(episode)
-
-          if viewer.share_record_to_twitter?
-            viewer.share_episode_record_to_twitter(episode_record)
-          end
-        end
+        ).call
 
         {
-          record: episode_record.record
+          record: result.record,
+          errors: result.errors
         }
       end
     end
