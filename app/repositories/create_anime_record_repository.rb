ccbd75c1c356fda36589c025b8dd392af1b1d3a9
Koji Shimba <me@shimba.co>
2020-12-19 21:31:16 +0900

    tweak

--- app/repositories/create_anime_record_repository.rb
+++ app/repositories/create_anime_record_repository.rb
@@ -2,16 +2,18 @@
 
 class CreateAnimeRecordRepository < ApplicationRepository
   def execute(form:)
-    result = mutate(variables: {
-      animeId: form.anime_id,
-      comment: form.comment,
-      ratingOverall: form.rating_overall&.upcase.presence || nil,
-      ratingAnimation: form.rating_animation&.upcase.presence || nil,
-      ratingMusic: form.rating_music&.upcase.presence || nil,
-      ratingStory: form.rating_story&.upcase.presence || nil,
-      ratingCharacter: form.rating_character&.upcase.presence || nil,
-      shareToTwitter: form.share_to_twitter
-    })
+    result = mutate(
+      variables: {
+        animeId: form.anime_id,
+        comment: form.comment,
+        ratingOverall: form.rating_overall&.upcase.presence,
+        ratingAnimation: form.rating_animation&.upcase.presence,
+        ratingMusic: form.rating_music&.upcase.presence,
+        ratingStory: form.rating_story&.upcase.presence,
+        ratingCharacter: form.rating_character&.upcase.presence,
+        shareToTwitter: form.share_to_twitter
+      }
+    )
 
     if result.to_h["errors"]
       return [nil, MutationError.new(message: result.to_h["errors"][0]["message"])]
