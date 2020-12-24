bec8cb1c89f1bc25616bfeb14938827446044916
Koji Shimba <me@shimba.co>
2020-12-24 22:22:19 +0900

    Call constructor

--- app/components/sidebars/anime_sidebar_component.rb
+++ app/components/sidebars/anime_sidebar_component.rb
@@ -3,6 +3,7 @@
 module Sidebars
   class AnimeSidebarComponent < ApplicationComponent
     def initialize(anime_entity:, vod_channel_entities:, viewer: nil)
+      super
       @anime_entity = anime_entity
       @vod_channel_entities = vod_channel_entities
       @viewer = viewer
