952ef5811eff2ede140c6c3558e3a0742304627e
Koji Shimba <me@shimba.co>
2021-01-04 21:20:40 +0900

    エピソード一覧ページの修正

--- app/controllers/v4/episodes_controller.rb
+++ app/controllers/v4/episodes_controller.rb
@@ -11,10 +11,12 @@ module V4
       anime = Work.only_kept.find(params[:anime_id])
       raise ActionController::RoutingError, "Not Found" if anime.no_episodes?
 
-      @anime_entity, @page_info_entity = EpisodeListPage::AnimeRepository.new(graphql_client: graphql_client).execute(
+      result = EpisodeListPage::AnimeRepository.new(graphql_client: graphql_client).execute(
         database_id: anime.id,
         pagination: Annict::Pagination.new(before: params[:before], after: params[:after], per: 500)
       )
+      @anime_entity = result.anime_entity
+      @page_info_entity = result.page_info_entity
       load_vod_channel_entities(anime: anime, anime_entity: @anime_entity)
     end
 
