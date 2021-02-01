6776b057de14cf06d459236de6c7288bfce19b71
Koji Shimba <me@shimba.co>
2021-02-02 05:28:20 +0900

    ホスト追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
   config.hosts += [
     ENV.fetch("ANNICT_API_DOMAIN"),
     ENV.fetch("ANNICT_DOMAIN"),
-    ENV.fetch("ANNICT_JP_DOMAIN")
+    ENV.fetch("ANNICT_JP_DOMAIN"),
+    "api"
   ]
 end
