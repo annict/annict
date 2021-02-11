a772dd07a5d6a0b339a5d6e98f4ac733bc39fe31
Koji Shimba <me@shimba.co>
2021-02-11 10:26:39 +0900

    リゾルバ内の一部クエリをキャッシュする

--- app/graphql/canary/resolvers/episodes.rb
+++ app/graphql/canary/resolvers/episodes.rb
@@ -6,31 +6,49 @@ module Canary
       def resolve(viewer_checked_in_current_status: nil, order_by: nil)
         order = Canary::OrderProperty.build(order_by)
         viewer = context[:viewer]
+        anime = object
 
-        @episodes = object.episodes.only_kept
+        episode_ids = Rails.cache.fetch(cache_key(viewer, anime, viewer_checked_in_current_status), expires_in: 24.hours) do
+          episodes = anime.episodes.only_kept
 
-        if viewer_checked_in_current_status
-          library_entry = viewer.library_entries.with_not_deleted_work.find_by(work: object)
+          if viewer_checked_in_current_status
+            library_entry = viewer.library_entries.with_not_deleted_work.find_by(work: anime)
 
-          if library_entry
-            @episodes = @episodes.where(id: library_entry.watched_episode_ids)
+            if library_entry
+              episodes = episodes.where(id: library_entry.watched_episode_ids)
+            end
+          elsif viewer_checked_in_current_status == false
+            library_entry = viewer.library_entries.with_not_deleted_work.find_by(work: anime)
+
+            if library_entry
+              episodes = episodes.where(id: episodes.pluck(:id) - library_entry.watched_episode_ids)
+            end
           end
-        elsif viewer_checked_in_current_status == false
-          library_entry = viewer.library_entries.with_not_deleted_work.find_by(work: object)
 
-          if library_entry
-            @episodes = @episodes.where(id: @episodes.pluck(:id) - library_entry.watched_episode_ids)
-          end
+          episodes.pluck(:id)
         end
 
-        @episodes = case order.field
+        episodes = Episode.where(id: episode_ids)
+
+        case order.field
         when :created_at, :sort_number
-          @episodes.order(order.field => order.direction)
+          episodes.order(order.field => order.direction)
         else
-          @episodes
+          episodes
         end
+      end
 
-        @episodes
+      private
+
+      def cache_key(viewer, anime, viewer_checked_in_current_status)
+        [
+          self.class.name,
+          viewer.id,
+          anime.id,
+          anime.updated_at.rfc3339,
+          viewer_checked_in_current_status.inspect,
+          '1' # キャッシュクリア用の任意の文字列
+        ].freeze
       end
     end
   end
--- app/graphql/canary/resolvers/slots_on_program.rb
+++ app/graphql/canary/resolvers/slots_on_program.rb
@@ -4,21 +4,38 @@ module Canary
   module Resolvers
     class SlotsOnProgram < Canary::Resolvers::Base
       def resolve(viewer_untracked: nil, order_by: nil)
-        program = object
         viewer = context[:viewer]
+        program = object
         order = Canary::OrderProperty.build(order_by)
 
-        slots = program.slots.only_kept
+        slot_ids = Rails.cache.fetch(cache_key(viewer, program, viewer_untracked), expires_in: 24.hours) do
+          slots = program.slots.only_kept
 
-        if viewer_untracked
-          library_entry = viewer.library_entries.find_by(work: program.work)
+          if viewer_untracked
+            library_entry = viewer.library_entries.find_by(work_id: program.work_id)
 
-          if library_entry
-            slots = slots.where.not(episode_id: library_entry.watched_episode_ids)
+            if library_entry
+              slots = slots.where.not(episode_id: library_entry.watched_episode_ids)
+            end
           end
+
+          slots.pluck(:id)
         end
 
-        slots.order(order.field => order.direction)
+        Slot.where(id: slot_ids).order(order.field => order.direction)
+      end
+
+      private
+
+      def cache_key(viewer, program, viewer_untracked)
+        [
+          self.class.name,
+          viewer.id,
+          program.id,
+          program.updated_at.rfc3339,
+          viewer_untracked.inspect,
+          '2' # キャッシュクリア用の任意の文字列
+        ].freeze
       end
     end
   end
