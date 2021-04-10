157f43af2441e09dcaf934ed29470b259963874b
Koji Shimba <me@shimba.co>
2021-04-10 19:47:26 +0900

    使われていない処理を除去

--- app/models/library_entry.rb
+++ app/models/library_entry.rb
@@ -60,52 +60,6 @@ class LibraryEntry < ApplicationRecord
     with_not_deleted_work.with_status(status_kind).count
   end
 
-  def self.refresh_next_episode(user)
-    library_entries = user.library_entries.includes(:work).with_status(:watching)
-    next_episode_data = library_entries.fetch_next_episode_data
-
-    library_entries.find_each do |ls|
-      next_episode = next_episode_data.detect { |ne| ne[:work_id] == ls.work_id }
-      next if ls.next_episode_id == next_episode[:next_episode_id]
-      ls.update_column(:next_episode_id, next_episode[:next_episode_id])
-    end
-  end
-
-  def self.fetch_next_episode_data
-    work_ids = pluck(:work_id)
-
-    return [] if work_ids.empty?
-
-    watched_episode_ids = pluck(:watched_episode_ids).flatten
-
-    episode_condition = watched_episode_ids.empty? ? "" : "id NOT IN (#{watched_episode_ids.join(',')}) AND"
-    sql = <<~SQL
-      WITH ranked_episodes AS (
-        SELECT
-          id,
-          work_id,
-          dense_rank() OVER (
-            PARTITION BY work_id ORDER BY sort_number ASC
-          ) AS episode_rank
-        FROM episodes
-        WHERE
-          #{episode_condition}
-          work_id IN (#{work_ids.join(',')}) AND
-          deleted_at IS NULL
-      )
-      SELECT work_id, id FROM ranked_episodes WHERE episode_rank = 1;
-    SQL
-    next_episodes = Episode.find_by_sql(sql)
-
-    work_ids.map do |work_id|
-      next_episode = next_episodes.select { |e| e.work_id == work_id }.first
-      {
-        work_id: work_id,
-        next_episode_id: next_episode&.id
-      }
-    end
-  end
-
   def fetch_next_episode
     episode_ids = work.episodes.only_kept.pluck(:id)
     next_episode_ids = episode_ids - watched_episode_ids
