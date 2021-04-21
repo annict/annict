1a36b7c8986ab650bcc129346f8cb23d90fad26a
Koji Shimba <me@shimba.co>
2021-04-22 01:19:06 +0900

    みんなの記録に自分の記録を表示しないようにする

--- app/controllers/my/episode_records_controller.rb
+++ app/controllers/my/episode_records_controller.rb
@@ -7,36 +7,39 @@ module My
     before_action :authenticate_user!, only: %i(create)
 
     def index
-      render unless user_signed_in?
-
       episode = Episode.only_kept.find(params[:episode_id])
-      is_tracked = current_user.episode_records.only_kept.where(episode_id: episode.id).exists?
-      likes = current_user.likes.select(:recipient_id, :recipient_type)
-
-      @my_records = current_user.
-        records.
-        only_kept.
-        eager_load(:episode_record).
-        where(episode_records: { episode_id: episode.id }).
-        order(created_at: :desc)
-      @my_records.each do |record|
-        record.is_spoiler = false
-        record.is_liked = record.liked?(likes)
-      end
-
       records = episode.records.only_kept.eager_load(:episode_record, user: %i(gumroad_subscriber profile setting)).
         merge(EpisodeRecord.with_body.order_by_rating_state(:desc).order(created_at: :desc))
+      @my_records = @following_records = []
 
-      @following_records = records.merge(current_user.followings)
-      @following_records.each do |record|
-        record.is_spoiler = !is_tracked
-        record.is_liked = record.liked?(likes)
-      end
+      if user_signed_in?
+        is_tracked = current_user.episode_records.only_kept.where(episode_id: episode.id).exists?
+        likes = current_user.likes.select(:recipient_id, :recipient_type)
 
-      @all_records = records.page(params[:page]).per(30)
-      @all_records.each do |record|
-        record.is_spoiler = !is_tracked
-        record.is_liked = record.liked?(likes)
+        @my_records = current_user.
+          records.
+          only_kept.
+          eager_load(:episode_record).
+          where(episode_records: { episode_id: episode.id }).
+          order(created_at: :desc)
+        @my_records.each do |record|
+          record.is_spoiler = false
+          record.is_liked = record.liked?(likes)
+        end
+
+        @following_records = records.merge(current_user.followings)
+        @following_records.each do |record|
+          record.is_spoiler = !is_tracked
+          record.is_liked = record.liked?(likes)
+        end
+
+        @all_records = records.where.not(user: current_user).page(params[:page]).per(30)
+        @all_records.each do |record|
+          record.is_spoiler = !is_tracked
+          record.is_liked = record.liked?(likes)
+        end
+      else
+        @all_records = records.page(params[:page]).per(30)
       end
     end
 
