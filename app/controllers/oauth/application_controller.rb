5b33957ecdddda9a587f32d058dada7a5fcd70ba
Koji Shimba <me@shimba.co>
2021-04-28 08:58:46 +0900

    Oauth::ApplicationController 修正

--- app/controllers/oauth/application_controller.rb
+++ app/controllers/oauth/application_controller.rb
@@ -4,30 +4,18 @@ module Oauth
   class ApplicationController < ActionController::Base
     include Doorkeeper::Helpers::Controller
 
-    include ControllerCommon
-    include Localable
-    include Analyzable
-    include LogrageSetting
-    include Gonable
-    include FlashMessage
-    include ViewSelector
-    include RavenContext
     include PageCategorizable
-    include V4::UserDataFetchable
+    include RavenLoadable
+    include Loggable
+    include Localizable
+    include KeywordSearchable
 
-    helper_method :gon, :locale_ja?, :locale_en?, :local_url, :page_category
+    layout "default"
 
-    layout "application"
+    helper_method :locale_ja?, :locale_en?, :local_url, :page_category
 
-    around_action :switch_locale
-    before_action :redirect_if_unexpected_subdomain
+    before_action :set_raven_context
     before_action :set_search_params
-    before_action :store_data_into_gon
-
-    private
-
-    def set_search_params
-      @search = SearchService.new(params[:q])
-    end
+    around_action :set_locale
   end
 end
