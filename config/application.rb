cfa16855e1b0ed63c757dfbade50c35d4a549ec3
Koji Shimba <me@shimba.co>
2021-05-03 18:25:15 +0900

    db/schema.rb を生成

--- config/application.rb
+++ config/application.rb
@@ -61,8 +61,6 @@ module Annict
 
     config.active_job.queue_adapter = :delayed_job
 
-    config.active_record.schema_format = :sql
-
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       # Redirect: annict.herokuapp.com -> annict.com
       r301(/.*/, "https://#{ENV.fetch("ANNICT_HOST")}$&", if: proc { |rack_env|
