f8dc0fa4bdfea2d9d73fb438a8807eac85328f9c
Koji Shimba <me@shimba.co>
2021-05-07 18:58:31 +0900

    ActionDispatch へのルーティング関係のパッチを除去
    Rails 6.1から標準でroutesファイルを分割できるようになったため。
    https://github.com/rails/rails/pull/37892

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-module ActionDispatch
-  module Routing
-    class Mapper
-      def draw(routes_name)
-        instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
-      end
-    end
-  end
-end
-
 Rails.application.routes.draw do
   draw :api
   draw :chat
