15a86257a96169234425323f300445caca42bc63
Koji Shimba <me@shimba.co>
2021-05-16 06:18:20 +0900

    spec修正

--- spec/requests/v3/favorite_people/index_spec.rb
+++ spec/requests/v3/favorite_people/index_spec.rb
@@ -3,10 +3,6 @@
 describe "GET /@:username/favorite_people", type: :request do
   let!(:user) { create(:registered_user) }
 
-  before do
-    host! ENV.fetch("ANNICT_JP_HOST")
-  end
-
   context "お気に入りがいないとき" do
     it "アクセスできること" do
       get "/@#{user.username}/favorite_people"
