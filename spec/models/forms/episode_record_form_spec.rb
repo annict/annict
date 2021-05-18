16b8c8c95772f4a55442456e2ed7fe20f1649b6b
Koji Shimba <me@shimba.co>
2021-05-19 06:52:20 +0900

    Fix EpisodeRecordCreator

---
+++ spec/models/forms/episode_record_form_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+describe Forms::EpisodeRecordForm do
+  let(:episode) { create :episode }
+
+  context "バリデーションエラーになったとき" do
+    it "エラー内容を返すこと" do
+      form = Forms::EpisodeRecordForm.new(
+        comment: "a" * (1_048_596 + 1), # 文字数制限 (1,048,596文字) 以上の感想を書く
+        episode: episode,
+        rating: "good",
+        share_to_twitter: false
+      )
+
+      expect(form.valid?).to eq false
+      expect(form.errors.count).to eq 1
+      expect(form.errors.full_messages.first).to eq "感想は1048596文字以内で入力してください"
+    end
+  end
+end
