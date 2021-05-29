901f8fce6985bd1aac1d30055a06a62c15971909
Koji Shimba <me@shimba.co>
2021-05-29 15:50:47 +0900

    タイムラインの見た目を調整

--- app/controllers/v6/home_controller.rb
+++ app/controllers/v6/home_controller.rb
@@ -7,14 +7,6 @@ module V6
     def show
       set_page_category PageCategory::HOME
 
-      @new_anime_list = Rails.cache.fetch("user-home-new-anime-list", expires_in: 3.hours) {
-        Anime.order(created_at: :desc).preload(:anime_image).limit(5)
-      }
-
-      @forum_posts = Rails.cache.fetch("user-home-forum-posts", expires_in: 1.hour) {
-        ForumPost.joins(:forum_category).merge(ForumCategory.with_slug(:site_news)).order(created_at: :desc).limit(5)
-      }
-
       @activity_groups = ActivityGroup
         .preload(user: :profile)
         .where(user_id: current_user.following_user_ids)
