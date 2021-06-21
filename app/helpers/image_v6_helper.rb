322a2b718d6787e795f5bdc23455b0b9cf16a825
Koji Shimba <me@shimba.co>
2021-06-21 23:50:47 +0900

    Fix #ann_image_url

--- app/helpers/image_v6_helper.rb
+++ app/helpers/image_v6_helper.rb
@@ -2,26 +2,21 @@
 
 module ImageV6Helper
   def ann_image_url(record, field, width:, blur: 0, format: "jpg")
-    height = height(record, field, width)
     path = record ? record.uploaded_file_path(field) : "no-image.jpg"
+    height = case [record.class, field]
+      when [AnimeImage, :image] then ((4 * width) / 3).ceil
+      when [Profile, :image] then width
+      when [Trailer, :image] then ((9 * width) / 16).ceil
+      end
+    fit = width == height ? "crop" : "fill"
 
     ix_image_url(path, {
       blur: blur,
       fill: "solid",
-      fit: "fill",
+      fit: fit,
       fm: format,
       height: height,
       w: width
     })
-  end
-
-  private
-
-  def height(record, field, width)
-    case [record.class, field]
-    when [AnimeImage, :image] then ((4 * width) / 3).ceil
-    when [Profile, :image] then width
-    when [Trailer, :image] then ((9 * width) / 16).ceil
-    end
   end
 end
