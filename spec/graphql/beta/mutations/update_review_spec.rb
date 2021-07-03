8ed47d4c04b7dde34a24f1205a350f9f1b143487
Koji Shimba <me@shimba.co>
2021-07-03 09:39:45 +0900

    Fix updateReview

---
+++ spec/graphql/beta/mutations/update_review_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+describe Beta::Mutations::UpdateReview do
+  let(:user) { create :registered_user }
+  let!(:record) { create(:record, user: user) }
+  let!(:anime_record) { create(:anime_record, user: user, record: record) }
+  let(:token) { create(:oauth_access_token) }
+  let(:context) { {viewer: user, doorkeeper_token: token, writable: true} }
+  let(:anime_record_id) { Canary::AnnictSchema.id_from_object(anime_record, anime_record.class) }
+
+  context "正常系" do
+    let(:query) do
+      <<~GRAPHQL
+        mutation($reviewId: ID!) {
+          updateReview(input: {
+            reviewId: $reviewId,
+            ratingOverallState: GREAT,
+            ratingAnimationState: GREAT,
+            ratingMusicState: GREAT,
+            ratingStoryState: GREAT,
+            ratingCharacterState: GREAT,
+            body: "またーり"
+          }) {
+            review {
+              annictId
+              body
+              ratingOverallState
+              ratingAnimationState
+              ratingMusicState
+              ratingStoryState
+              ratingCharacterState
+            }
+          }
+        }
+      GRAPHQL
+    end
+    let(:variables) { {reviewId: anime_record_id} }
+
+    it "更新されたRecordデータが返ること" do
+      expect(Record.count).to eq 1
+      expect(AnimeRecord.count).to eq 1
+      expect(anime_record.body).to eq "おもしろかった"
+      expect(anime_record.rating_overall_state).to be_nil
+      expect(anime_record.rating_animation_state).to be_nil
+      expect(anime_record.rating_music_state).to be_nil
+      expect(anime_record.rating_story_state).to be_nil
+      expect(anime_record.rating_character_state).to be_nil
+
+      record_cache_expired_at = user.record_cache_expired_at
+      result = Beta::AnnictSchema.execute(query, variables: variables, context: context)
+
+      expect(Record.count).to eq 1
+      expect(AnimeRecord.count).to eq 1
+
+      expect(result["errors"]).to be_nil
+      expect(result.dig("data", "updateReview", "review", "body")).to eq "またーり"
+      expect(result.dig("data", "updateReview", "review", "ratingOverallState")).to eq "GREAT"
+      expect(result.dig("data", "updateReview", "review", "ratingAnimationState")).to eq "GREAT"
+      expect(result.dig("data", "updateReview", "review", "ratingMusicState")).to eq "GREAT"
+      expect(result.dig("data", "updateReview", "review", "ratingStoryState")).to eq "GREAT"
+      expect(result.dig("data", "updateReview", "review", "ratingCharacterState")).to eq "GREAT"
+      expect(user.record_cache_expired_at).to_not eq record_cache_expired_at
+    end
+  end
+end
