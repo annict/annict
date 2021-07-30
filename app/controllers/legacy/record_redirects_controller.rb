bd22b2a6f9d837275dccda5d4f1d14426e261303
Koji Shimba <me@shimba.co>
2021-07-31 06:22:07 +0900

    昔使用していたリダイレクトパスのルーティングを復活

---
+++ app/controllers/legacy/record_redirects_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Legacy
+  class RecordRedirectsController < ApplicationController
+    def show
+      url = case params[:provider]
+      when "tw"
+        EpisodeRecord.only_kept.find_by!(twitter_url_hash: params[:url_hash]).share_url_with_query(:twitter)
+      when "fb"
+        EpisodeRecord.only_kept.find_by!(facebook_url_hash: params[:url_hash]).share_url_with_query(:facebook)
+      else
+        root_path
+      end
+
+      redirect_to url, status: 301
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,7 @@ Rails.application.routes.draw do
   match "/channels",                                            via: :get,    as: :channel_list,                               to: "channels#index"
   match "/characters/:character_id",                            via: :get,    as: :character,                                  to: "characters#show"
   match "/characters/:character_id/fans",                       via: :get,    as: :character_fan_list,                         to: "character_fans#index"
+  match "/checkins/redirect/:provider/:url_hash",               via: :get,    as: :legacy_record_redirect_1,                   to: "legacy/record_redirects#show", provider: /fb|tw/, url_hash: /[0-9a-zA-Z_-]{10}/
   match "/db",                                                  via: :get,    as: :db_root,                                    to: "db/home#show"
   match "/db/activities",                                       via: :get,    as: :db_activity_list,                           to: "db/activities#index"
   match "/db/casts/:id",                                        via: :delete, as: :db_cast_detail,                             to: "db/casts#destroy"
@@ -222,6 +223,7 @@ Rails.application.routes.draw do
   match "/people/:person_id",                                   via: :get,    as: :person,                                     to: "people#show"
   match "/people/:person_id/fans",                              via: :get,    as: :person_fan_list,                            to: "person_fans#index"
   match "/privacy",                                             via: :get,    as: :privacy,                                    to: "pages#privacy"
+  match "/r/:provider/:url_hash",                               via: :get,    as: :legacy_record_redirect_2,                   to: "legacy/record_redirects#show", provider: /fb|tw/, url_hash: /[0-9a-zA-Z_-]{10}/
   match "/registrations/new",                                   via: :get,    as: :new_registration,                           to: "registrations#new"
   match "/search",                                              via: :get,    as: :search,                                     to: "searches#show"
   match "/settings/account",                                    via: :get,    as: :settings_account,                           to: "settings/accounts#show"
