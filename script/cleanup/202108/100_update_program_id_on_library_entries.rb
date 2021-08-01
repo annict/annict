6e6d09b70707e054b75d2f0a1b6521bb4fd1eab2
Koji Shimba <me@shimba.co>
2021-08-02 06:49:26 +0900

    クリーンアップスクリプトの修正

--- script/cleanup/202108/100_update_program_id_on_library_entries.rb
+++ script/cleanup/202108/100_update_program_id_on_library_entries.rb
@@ -7,12 +7,14 @@ users.find_each do |user|
   p "user: #{user.id}"
 
   ActiveRecord::Base.transaction do
-    ChannelAnime.where(user: user).find_each do |cw|
-      program = Program.find_by(channel_id: cw.channel_id, work_id: cw.work_id)
-
+    ChannelAnime.where(user: user).find_each do |ca|
+      program = Program.where(work_id: ca.work_id, channel_id: ca.channel_id).order(started_at: :desc).first
       next unless program
 
-      library_entry = user.library_entries.where(work_id: cw.work_id).first_or_create!
+      library_entry = user.library_entries.where(work_id: ca.work_id).first
+      next unless library_entry
+      next if library_entry.program_id
+
       library_entry.update!(program_id: program.id)
     end
   end
