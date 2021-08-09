a66b98b17266053187b8e68171ae1f6bbfe4e44a
Koji Shimba <me@shimba.co>
2021-08-10 06:20:58 +0900

    フレーム内を読み込んでいる間ぐるぐるさせる

--- app/components/offcanvases/tracking_offcanvas_component.rb
+++ app/components/offcanvases/tracking_offcanvas_component.rb
@@ -13,12 +13,22 @@ module Offcanvases
             h.tag :button, class: "btn-close text-reset", data_bs_dismiss: "offcanvas", type: "button"
           end
 
-          h.tag :turbo_frame,
+          h.tag :turbo_frame, {
+            class: "h-100",
             data_controller: "reloadable",
             data_reloadable_event_name_value: "tracking-offcanvas",
             id: "c-tracking-offcanvas-frame",
             src: "",
             tabindex: "-1"
+          } do
+            h.tag :div, class: "align-items-center d-flex h-100 justify-content-center" do
+              h.tag :div, class: "spinner-border" do
+                h.tag :span, class: "visually-hidden" do
+                  h.text "Loading..."
+                end
+              end
+            end
+          end
         end
       end
     end
