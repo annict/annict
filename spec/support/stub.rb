600a9e939947baebddf6779a0db419d52d9fdef7
Koji Shimba <me@shimba.co>
2021-08-11 15:01:32 +0900

    Fix for testing

--- spec/support/stub.rb
+++ spec/support/stub.rb
@@ -3,9 +3,10 @@
 RSpec.configure do |config|
   config.before :suite do
     ImageHelper.module_eval do
-      def v4_ann_image_url(*)
+      def ann_image_url(*)
         "#{ENV.fetch("ANNICT_URL")}/dummy_image"
       end
+      alias_method :v4_ann_image_url, :ann_image_url
     end
   end
 end
