2848ff8139338aecd19a67ecad8e3c70f0e3a3a9
Koji Shimba <me@shimba.co>
2021-08-12 06:43:08 +0900

    ページネーションのスタイルを修正

--- app/components/button_groups/pagination_button_group_component.rb
+++ app/components/button_groups/pagination_button_group_component.rb
@@ -2,24 +2,78 @@
 
 module ButtonGroups
   class PaginationButtonGroupComponent < ApplicationV6Component
-    def initialize(view_context, collection:, controller: nil, action: nil)
+    PAGE_WINDOW = 10
+    ACCEPTED_PARAM_KEYS = %i[q]
+
+    def initialize(view_context, collection:, without_count: false, controller: nil, action: nil)
       super view_context
       @collection = collection
+      @without_count = without_count
       @controller = controller
       @action = action
     end
 
     def render
       build_html do |h|
-        h.tag :div, class: "btn-group" do
-          h.tag :a, class: prev_button_class_name, href: prev_page_path do
-            h.tag :i, class: "far fa-angle-left me-2"
-            h.text t("noun.prev_page")
+        h.tag :div, class: "btn-group c-pagination-button-group" do
+          unless @without_count
+            h.tag :a, class: prev_button_class_name, href: page_url_for(page: 1) do
+              h.tag :i, class: "far fa-angle-double-left"
+            end
           end
 
-          h.tag :a, class: next_button_class_name, href: next_page_path do
-            h.text t("noun.next_page")
-            h.tag :i, class: "far fa-angle-right ms-2"
+          h.tag :a, class: prev_button_class_name, href: page_url_for(page: current_page - 1) do
+            h.tag :i, class: "far fa-angle-left"
+          end
+
+          unless @without_count
+            h.tag :div, class: "btn-group" do
+              h.tag :button, class: "#{prev_button_class_name} dropdown-toggle", id: "prevDrop", type: "button", data_bs_toggle: "dropdown" do
+                h.text "..."
+              end
+
+              h.tag :ul, class: "dropdown-menu" do
+                prev_pages.each do |page|
+                  h.tag :li do
+                    h.tag :a, class: "dropdown-item", href: page_url_for(page: page) do
+                      h.text page
+                    end
+                  end
+                end
+              end
+            end
+          end
+
+          h.tag :a, class: "active btn btn-secondary disabled", href: "#" do
+            h.text current_page
+          end
+
+          unless @without_count
+            h.tag :div, class: "btn-group" do
+              h.tag :button, class: "#{next_button_class_name} dropdown-toggle", id: "nextDrop", type: "button", data_bs_toggle: "dropdown" do
+                h.text "..."
+              end
+
+              h.tag :ul, class: "dropdown-menu" do
+                next_pages.each do |page|
+                  h.tag :li do
+                    h.tag :a, class: "dropdown-item", href: page_url_for(page: page) do
+                      h.text page
+                    end
+                  end
+                end
+              end
+            end
+          end
+
+          h.tag :a, class: next_button_class_name, href: page_url_for(page: current_page + 1) do
+            h.tag :i, class: "far fa-angle-right"
+          end
+
+          unless @without_count
+            h.tag :a, class: next_button_class_name, href: page_url_for(page: total_page) do
+              h.tag :i, class: "far fa-angle-double-right"
+            end
           end
         end
       end
@@ -36,31 +90,35 @@ module ButtonGroups
     end
 
     def disabled_button_class_name
-      %w[btn-outline-secondary disabled]
+      %w[btn-outline-secondary disabled opacity-25]
     end
 
     def prev_button_class_name
-      class_name = base_button_class_name
+      @prev_button_class_name ||= begin
+        class_name = base_button_class_name
 
-      class_name += if @collection.first_page?
-        disabled_button_class_name
-      else
-        enabled_button_class_name
+        class_name += if @collection.first_page?
+          disabled_button_class_name
+        else
+          enabled_button_class_name
+        end
+
+        class_name.join(" ")
       end
-
-      class_name.join(" ")
     end
 
     def next_button_class_name
-      class_name = base_button_class_name
+      @next_button_class_name ||= begin
+        class_name = base_button_class_name
 
-      class_name += if @collection.last_page?
-        disabled_button_class_name
-      else
-        enabled_button_class_name
+        class_name += if @collection.last_page?
+          disabled_button_class_name
+        else
+          enabled_button_class_name
+        end
+
+        class_name.join(" ")
       end
-
-      class_name.join(" ")
     end
 
     def controller
@@ -71,12 +129,26 @@ module ButtonGroups
       @action.presence || view_context.params[:action]
     end
 
-    def prev_page_path
-      @prev_page_path ||= view_context.prev_page_path(@collection, params: {controller: controller, action: action})
+    def page_url_for(page:)
+      params = view_context.params.to_unsafe_h.slice(*ACCEPTED_PARAM_KEYS)
+      params[:page] = page
+      url_for(controller: controller, action: action, only_path: true, params: params)
     end
 
-    def next_page_path
-      @next_page_path ||= view_context.next_page_path(@collection, params: {controller: controller, action: action})
+    def current_page
+      @collection.current_page
+    end
+
+    def total_page
+      @collection.total_pages
+    end
+
+    def next_pages
+      @next_pages ||= (1..PAGE_WINDOW / 2).to_a.map { |n| current_page + n }.select { |n| n <= total_page }
+    end
+
+    def prev_pages
+      @prev_pages ||= (1..PAGE_WINDOW / 2).to_a.reverse.map { |n| current_page - n }.select { |n| n.positive? }
     end
   end
 end
