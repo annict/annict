af7e16bab98ca3ce1d97c2f305a6464054efdaf6
Koji Shimba <me@shimba.co>
2021-08-12 07:41:29 +0900

    非推奨メッセージを追加

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -44,6 +44,9 @@ class ApplicationRecord < ActiveRecord::Base
     return super if method_name.blank?
     return super unless method_name.to_s.start_with?("local_")
 
+    unless Rails.env.production?
+      ActiveSupport::Deprecation.warn("local_* methods using method_missing are deprecated. (#{self.class.name}##{method_name})")
+    end
     _local_property(method_name.to_s.sub("local_", ""), *arguments)
   end
 
