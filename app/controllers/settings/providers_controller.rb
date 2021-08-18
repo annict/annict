88e49af76232f4e0b3ed3a0265bbd876e3ec4969
Koji Shimba <me@shimba.co>
2021-08-19 06:12:28 +0900

    連係解除処理を修正

--- app/controllers/settings/providers_controller.rb
+++ app/controllers/settings/providers_controller.rb
@@ -5,12 +5,12 @@ module Settings
     before_action :authenticate_user!
 
     def destroy
-      provider = current_user.providers.find(params[:id])
+      provider = current_user.providers.find(params[:provider_id])
 
       ActiveRecord::Base.transaction do
         case provider.name
         when "twitter"
-          provider.user.setting.update_column(:share_record_to_twitter, false)
+          provider.user.setting.update!(share_record_to_twitter: false)
         end
 
         provider.destroy
