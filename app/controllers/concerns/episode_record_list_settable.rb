3048e0aac30a4a1492092e04a3dda25779f6d498
Koji Shimba <me@shimba.co>
2021-08-20 05:55:37 +0900

    Fix episode page

--- app/controllers/concerns/episode_record_list_settable.rb
+++ app/controllers/concerns/episode_record_list_settable.rb
@@ -4,15 +4,19 @@ module EpisodeRecordListSettable
   extend ActiveSupport::Concern
 
   def set_episode_record_list(episode)
-    records = episode.records
-      .preload(:work, user: %i[gumroad_subscriber profile setting])
+    records = episode
+      .records
+      .preload(:work, :episode)
+      .eager_load(user: [:setting, :profile, :gumroad_subscriber])
       .only_kept
     @my_records = @following_records = Record.none
 
     if user_signed_in?
       @my_records = current_user
         .records
+        .preload(:work, :episode)
         .only_kept
+        .where(episode: episode)
         .order(created_at: :desc)
       @following_records = records
         .merge(current_user.followings)
