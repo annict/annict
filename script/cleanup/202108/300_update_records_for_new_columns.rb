0fccc0bab46b28583374e729ad7c6e3084822149
Koji Shimba <me@shimba.co>
2021-08-23 05:41:50 +0900

    watched_at に保存する値を修正

--- script/cleanup/202108/300_update_records_for_new_columns.rb
+++ script/cleanup/202108/300_update_records_for_new_columns.rb
@@ -53,7 +53,7 @@ target_work_records.preload(:record).find_each(order: :desc) do |wr|
     character_rating: new_rating(wr.rating_character_state),
     music_rating: new_rating(wr.rating_music_state),
     story_rating: new_rating(wr.rating_story_state),
-    watched_at: wr.created_at,
+    watched_at: wr.record.created_at,
     modified_at: wr.modified_at
   )
 end
@@ -77,7 +77,7 @@ target_episode_records.preload(:record).find_each(order: :desc) do |er|
     advanced_rating: er.rating,
     twitter_url_hash: er.twitter_url_hash,
     facebook_url_hash: er.facebook_url_hash,
-    watched_at: er.created_at,
-    modified_at: er.modify_body? ? er.updated_at : nil
+    watched_at: er.record.created_at,
+    modified_at: er.modify_body? ? er.record.updated_at : nil
   )
 end
