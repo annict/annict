2ad1cf5986f3366d2a4c30a8b25c8a616d6b2584
Koji Shimba <me@shimba.co>
2021-09-16 06:13:49 +0900

    Use Time#use_zone

--- app/models/concerns/forms/recordable.rb
+++ app/models/concerns/forms/recordable.rb
@@ -36,8 +36,9 @@ module Forms::Recordable
     def watched_at=(value)
       return if value.nil?
 
-      Time.zone = user.time_zone
-      @watched_at = value.is_a?(Time) ? value.in_time_zone : Time.zone.parse(value)
+      Time.use_zone(user.time_zone) do
+        @watched_at = value.is_a?(Time) ? value.in_time_zone : Time.zone.parse(value)
+      end
     end
 
     # @overload
@@ -52,20 +53,20 @@ module Forms::Recordable
     private
 
     def watched_at_can_only_set_supporter
-      Time.zone = user.time_zone
-
-      if watched_at.present? && !user.supporter? && watched_at != record&.watched_at&.in_time_zone
-        i18n_path = "activemodel.errors.forms.recordable.watched_at_can_only_set_supporter"
-        errors.add(:watched_at, I18n.t(i18n_path))
+      Time.use_zone(user.time_zone) do
+        if watched_at.present? && !user.supporter? && watched_at != record&.watched_at&.in_time_zone
+          i18n_path = "activemodel.errors.forms.recordable.watched_at_can_only_set_supporter"
+          errors.add(:watched_at, I18n.t(i18n_path))
+        end
       end
     end
 
     def watched_at_cannot_be_in_the_future
-      Time.zone = user.time_zone
-
-      if watched_at.present? && watched_at > Time.zone.now
-        i18n_path = "activemodel.errors.forms.recordable.watched_at_cannot_be_in_the_future"
-        errors.add(:watched_at, I18n.t(i18n_path))
+      Time.use_zone(user.time_zone) do
+        if watched_at.present? && watched_at > Time.zone.now
+          i18n_path = "activemodel.errors.forms.recordable.watched_at_cannot_be_in_the_future"
+          errors.add(:watched_at, I18n.t(i18n_path))
+        end
       end
     end
   end
