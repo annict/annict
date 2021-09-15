8884639e917b303f2b2ae98e4a4da8a1a93c1888
Koji Shimba <me@shimba.co>
2021-09-15 21:39:33 +0900

    Fix updateRecord spec

--- spec/factories/episode_record.rb
+++ spec/factories/episode_record.rb
@@ -2,12 +2,5 @@
 
 FactoryBot.define do
   factory :episode_record do
-    association :user, :with_profile
-    body { "おもしろかった" }
-    twitter_url_hash { |n| "xxxxx#{n}" }
-    work
-    episode
-    record
-    rating { 3.0 }
   end
 end
--- spec/graphql/beta/mutations/update_record_spec.rb
+++ spec/graphql/beta/mutations/update_record_spec.rb
@@ -4,8 +4,8 @@ describe Beta::Mutations::UpdateRecord do
   let!(:user) { create :registered_user }
   let!(:work) { create :work }
   let!(:episode) { create :episode, work: work }
-  let!(:record) { create :record, user: user, work: work }
-  let!(:episode_record) { create(:episode_record, user: user, record: record, work: work, episode: episode, rating: nil) }
+  let!(:episode_record) { create(:episode_record) }
+  let!(:record) { create :record, :for_episode, user: user, work: work, recordable: episode_record }
   let!(:token) { create(:oauth_access_token) }
   let!(:context) { {viewer: user, doorkeeper_token: token, writable: true} }
   let!(:episode_record_id) { Canary::AnnictSchema.id_from_object(episode_record, episode_record.class) }
@@ -33,8 +33,8 @@ describe Beta::Mutations::UpdateRecord do
     it "更新されたRecordデータが返ること" do
       expect(Record.count).to eq 1
       expect(EpisodeRecord.count).to eq 1
-      expect(episode_record.body).to eq "おもしろかった"
-      expect(episode_record.rating_state).to be_nil
+      expect(record.body).to eq "おもしろかった"
+      expect(record.rating).to eq "good"
 
       record_cache_expired_at = user.record_cache_expired_at
       result = Beta::AnnictSchema.execute(query, variables: variables, context: context)
