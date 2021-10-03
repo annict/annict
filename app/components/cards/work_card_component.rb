92c7f411ec47067435fe6fc855ab47c1208dbf0e
Koji Shimba <me@shimba.co>
2021-10-03 10:56:01 +0900

    作品タイトルを2行表示する

--- app/components/cards/work_card_component.rb
+++ app/components/cards/work_card_component.rb
@@ -15,28 +15,34 @@ module Cards
 
     def render
       build_html do |h|
-        h.tag :div, class: "border-0 card h-100" do
-          h.tag :a, href: view_context.work_path(@work), class: "text-reset" do
-            h.html Pictures::WorkPictureComponent.new(
-              view_context,
-              work: @work,
-              width: @width,
-              alt: @work.local_title
-            ).render
-
-            h.tag :div, class: "fw-bold h5 mb-0 mt-2 text-center text-truncate", title: @work.local_title do
-              h.text @work.local_title
+        h.tag :div, class: "align-items-end border-0 c-work-card card flex-column gap-2 h-100 vstack" do
+          h.tag :div, class: "text-center w-100" do
+            h.tag :a, href: view_context.work_path(@work) do
+              h.html Pictures::WorkPictureComponent.new(
+                view_context,
+                work: @work,
+                width: @width,
+                alt: @work.local_title
+              ).render
             end
+          end
 
-            if @caption.present?
-              h.tag :div, class: "mb-0 small text-center text-muted text-truncate", title: @caption do
-                h.text @caption
+          h.tag :div, class: "w-100" do
+            h.tag :a, href: view_context.work_path(@work), class: "text-body" do
+              h.tag :div, class: "c-work-card__work-title fw-bold h5 mb-0", title: @work.local_title do
+                h.text @work.local_title
+              end
+
+              if @caption.present?
+                h.tag :div, class: "small text-muted text-truncate", title: @caption do
+                  h.text @caption
+                end
               end
             end
           end
 
           if @show_note
-            h.tag :div, class: "mt-1 text-center" do
+            h.tag :div, class: "text-center" do
               if @note.present?
                 h.tag :a, class: "d-inline-block px-2 u-fake-link", data_bs_toggle: "popover", data_bs_trigger: "focus", data_bs_placement: "top", data_bs_content: @note, tabindex: "0" do
                   h.tag :i, class: "far fa-comment-dots"
@@ -50,7 +56,7 @@ module Cards
           end
 
           if @show_button && (!current_user || !@user || current_user.id == @user.id)
-            h.tag :div, class: "mt-2 text-center" do
+            h.tag :div, class: "mt-auto text-center w-100" do
               h.html ButtonGroups::WorkButtonGroupComponent.new(view_context, work: @work).render
             end
           end
