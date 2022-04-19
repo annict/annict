570525cbbd1897706405454916740f910518aedb
Koji Shimba <me@shimba.co>
2022-04-20 02:27:42 +0900

    Run `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,13 @@
 
 require_relative "boot"
 
-%w[
-  rails
-  active_model/railtie
-  active_job/railtie
-  active_record/railtie
-  action_controller/railtie
-  action_mailer/railtie
-  action_view/railtie
-].each do |railtie|
-  require railtie
-end
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
