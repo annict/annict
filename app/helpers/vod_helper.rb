7a08ed89e5c808b066901e45282dee655c677134
Koji Shimba <me@shimba.co>
2022-07-01 21:33:12 +0900

    HTTPS

--- app/helpers/vod_helper.rb
+++ app/helpers/vod_helper.rb
@@ -6,11 +6,11 @@ module VodHelper
     when Channel::AMAZON_VIDEO_ID
       "https://www.amazon.co.jp/dp/#{code}"
     when Channel::BANDAI_CHANNEL_ID
-      "http://www.b-ch.com/ttl/index.php?ttl_c=#{code}"
+      "https://www.b-ch.com/ttl/index.php?ttl_c=#{code}"
     when Channel::D_ANIME_STORE_ID
       "https://animestore.docomo.ne.jp/animestore/ci_pc?workId=#{code}"
     when Channel::NICONICO_CHANNEL_ID
-      "http://ch.nicovideo.jp/#{code}"
+      "https://ch.nicovideo.jp/#{code}"
     when Channel::NETFLIX_ID
       "https://www.netflix.com/title/#{code}"
     when Channel::ABEMA_VIDEO_ID
