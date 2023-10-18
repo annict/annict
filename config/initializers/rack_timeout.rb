3983cfde2146772fe2d322be0a696abbdb2a241e
Koji Shimba <me@shimba.co>
2023-10-18 22:21:43 +0900

    Setup rack-timeout

---
+++ config/initializers/rack_timeout.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before(
+  Rack::Runtime,
+  Rack::Timeout,
+  service_timeout: ENV.fetch("RACK_TIMEOUT_SERVICE_TIMEOUT", 15).to_i,
+  term_on_timeout: ENV.fetch("RACK_TIMEOUT_TERM_ON_TIMEOUT", 1).to_i
+)
