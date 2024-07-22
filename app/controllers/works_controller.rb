15d1dd9a4168e07be1b8504919cf13d688325d51
Koji Shimba <me@shimba.co>
2024-07-23 01:34:36 +0900

    作品ページでミュートしている人の記録が表示されないように修正

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,12 +11,15 @@ class WorksController < ApplicationV6Controller
 
     @trailers = @work.trailers.only_kept.order(:sort_number).first(5)
     @episodes = @work.episodes.only_kept.order(:sort_number).first(29)
-    @records = @work
+    records = @work
       .records
       .with_work_record
       .only_kept
       .merge(WorkRecord.with_body.order_by_rating(:desc).order(created_at: :desc))
       .preload(:work, :work_record, :episode_record, user: %i[gumroad_subscriber profile])
-      .first(11)
+    if user_signed_in?
+      records = records.where.not(user_id: current_user.mute_users.pluck(:muted_user_id))
+    end
+    @records = records.first(11)
   end
 end
