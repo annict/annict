f2d0f6d161d437b82835b3cda4a00e67b24be1e9
Koji Shimba <me@shimba.co>
2024-07-23 00:07:09 +0900

    Rack::Attackの設定を更新

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,10 @@
 # typed: false
 # frozen_string_literal: true
 
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
 def remote_ip(req)
-  @remote_ip ||= (req.env["action_dispatch.remote_ip"] || req.ip).to_s
+  @remote_ip ||= (req.env["HTTP_CF_CONNECTING_IP"] || req.env["action_dispatch.remote_ip"] || req.ip).to_s
 end
 
 Rack::Attack.throttle("requests by ip", limit: 10, period: 2.seconds) do |req|
