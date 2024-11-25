02d1923331ba460838dfe78c6b0a1b89ac69e84d
Okunichiyou <seifuzi@icloud.com>
2024-11-25 22:43:20 +0900

    sort_seasonメソッドのテスト追加(#4011)
    season_yearとseason_nameを含めたソート結果をテストするようにスイートを修正
    season_yearがnilの場合はstarted_onの値に関わらず順位が後ろでソートされることを確認

--- spec/models/series_work_spec.rb
+++ spec/models/series_work_spec.rb
@@ -1,32 +1,52 @@
 # typed: false
 # frozen_string_literal: true
 
+def create_work(season_year: 2010, season_name: 'spring', started_on: '2010-04-11')
+  create(:work,
+         season_year:,
+         season_name:,
+         started_on: Date.parse(started_on))
+end
+
 describe SeriesWork, type: :model do
   describe '.sort_season' do
-    context 'when same season_year and different started_on' do
+    let(:series) { create(:series) }
+
+    context 'differ in season_year, season_name, and started_on' do
+      let(:work_earier_started_on) { create_work(started_on: '2010-04-01') }
+      let(:work_later_started_on) { create_work(started_on: '2010-04-21') }
+      let(:work_earier_season_year) { create_work(season_year: 2009) }
+      let(:work_earier_season_name) { create_work(season_name: 'winter', started_on: '2010-01-21') }
+
       before do
-        series = create(:series)
-
-        common_attributes = { season_year: 2010, season_name: 'spring' }
-        @work_started_on_first = create(:work,
-                                        **common_attributes,
-                                        started_on: Date.parse('2010-01-01'))
-        @work_started_on_last = create(:work,
-                                       **common_attributes,
-                                       started_on: Date.parse('2010-12-01'))
-        @work_started_on_second = create(:work,
-                                         **common_attributes,
-                                         started_on: Date.parse('2010-06-01'))
-
-        create(:series_work, series:, work: @work_started_on_first)
-        create(:series_work, series:, work: @work_started_on_last)
-        create(:series_work, series:, work: @work_started_on_second)
+        create(:series_work, series:, work: work_later_started_on)
+        create(:series_work, series:, work: work_earier_started_on)
+        create(:series_work, series:, work: work_earier_season_year)
+        create(:series_work, series:, work: work_earier_season_name)
       end
 
-      it 'sorts SeriesWork by started_on column in ascending order', :aggregate_failures do
-        expect(SeriesWork.sort_season.first.work).to eq(@work_started_on_first)
-        expect(SeriesWork.sort_season.second.work).to eq(@work_started_on_second)
-        expect(SeriesWork.sort_season.last.work).to eq(@work_started_on_last)
+      it 'sorts SeriesWork by season_year, then season_name, and then started_on in ascending order',
+         :aggregate_failures do
+        expect(SeriesWork.sort_season[0].work).to eq(work_earier_season_year)
+        expect(SeriesWork.sort_season[1].work).to eq(work_earier_season_name)
+        expect(SeriesWork.sort_season[2].work).to eq(work_earier_started_on)
+        expect(SeriesWork.sort_season[3].work).to eq(work_later_started_on)
+      end
+    end
+
+    context 'when one work has a nil season_year but an earlier started_on date' do
+      let(:work_with_season_year_nil) { create_work(season_year: nil, started_on: '2009-04-01') }
+      let(:work_with_season_year_exists) { create_work(season_year: 2010, started_on: '2010-04-01') }
+
+      before do
+        create(:series_work, series:, work: work_with_season_year_nil)
+        create(:series_work, series:, work: work_with_season_year_exists)
+      end
+
+      it 'places the work with a valid season_year before the work with a nil season_year, regardless of started_on date',
+         :aggregate_failures do
+        expect(SeriesWork.sort_season[0].work).to eq(work_with_season_year_exists)
+        expect(SeriesWork.sort_season[1].work).to eq(work_with_season_year_nil)
       end
     end
   end
