5554a5719a3a671c9e5e78ace9a5979de9a7115e
Koji Shimba <me@shimba.co>
2025-02-04 01:30:22 +0900

    NULLを許容しているフィードを `null: true` にする

--- app/graphql/beta/types/objects/organization_type.rb
+++ app/graphql/beta/types/objects/organization_type.rb
@@ -13,11 +13,11 @@ module Beta
         field :name, String, null: false
         field :name_kana, String, null: false
         field :name_en, String, null: false
-        field :url, String, null: false
+        field :url, String, null: true
         field :url_en, String, null: false
-        field :wikipedia_url, String, null: false
+        field :wikipedia_url, String, null: true
         field :wikipedia_url_en, String, null: false
-        field :twitter_username, String, null: false
+        field :twitter_username, String, null: true
         field :twitter_username_en, String, null: false
         field :favorite_organizations_count, Integer, null: false
         field :staffs_count, Integer, null: false
--- app/graphql/beta/types/objects/person_type.rb
+++ app/graphql/beta/types/objects/person_type.rb
@@ -13,18 +13,18 @@ module Beta
         field :name, String, null: false
         field :name_kana, String, null: false
         field :name_en, String, null: false
-        field :nickname, String, null: false
+        field :nickname, String, null: true
         field :nickname_en, String, null: false
-        field :gender_text, String, null: false
-        field :url, String, null: false
+        field :gender_text, String, null: true
+        field :url, String, null: true
         field :url_en, String, null: false
-        field :wikipedia_url, String, null: false
+        field :wikipedia_url, String, null: true
         field :wikipedia_url_en, String, null: false
-        field :twitter_username, String, null: false
+        field :twitter_username, String, null: true
         field :twitter_username_en, String, null: false
-        field :birthday, String, null: false
-        field :blood_type, String, null: false
-        field :height, String, null: false
+        field :birthday, String, null: true
+        field :blood_type, String, null: true
+        field :height, String, null: true
         field :favorite_people_count, Integer, null: false
         field :casts_count, Integer, null: false
         field :staffs_count, Integer, null: false
