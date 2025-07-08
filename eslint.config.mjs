9c52c78302fc97a55ff4faa686e42fa615d4696f
Shimba, Koji <me@shimba.co>
2025-07-09 02:03:07 +0900

    ESLintの修正 (#4221)

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  globalIgnores(["app/assets/builds/**/*", "tailwind.config.js"]),
+]);
