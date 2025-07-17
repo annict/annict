6f4565269609250b6a6fd7c20c300f0847d94a72
Koji Shimba <me@shimba.co>
2025-07-18 03:29:51 +0900

    Fix

--- spec/requests/api/graphql_spec.rb
+++ spec/requests/api/graphql_spec.rb
@@ -1,12 +1,14 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /graphql", type: :request do
-  let(:work) { create(:work) }
-  let(:access_token) { create(:oauth_access_token) }
-  let(:id) { Beta::AnnictSchema.id_from_object(work, work.class) }
-  let(:query) do
-    <<~GRAPHQL
+RSpec.describe "POST /graphql", type: :request do
+  it "認証済みユーザーがクエリを実行できること" do
+    work = create(:work)
+    user = create(:user)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application, owner: user)
+    id = Beta::AnnictSchema.id_from_object(work, work.class)
+    query = <<~GRAPHQL
       query($workId: ID!) {
         node(id: $workId) {
           id
@@ -17,10 +19,8 @@ describe "POST /graphql", type: :request do
         }
       }
     GRAPHQL
-  end
-  let(:headers) { {"Authorization" => "bearer #{access_token.token}"} }
+    headers = {"Authorization" => "bearer #{access_token.token}"}
 
-  it "リクエストできること" do
     post "/graphql", params: {variables: {workId: id}, query: query}, headers: headers
 
     expect(response.status).to eq(200)
@@ -33,5 +33,80 @@ describe "POST /graphql", type: :request do
         }
       }
     }.deep_stringify_keys)
+  end
+
+  it "認証なしでクエリを実行した場合401エラーが返ること" do
+    work = create(:work)
+    id = Beta::AnnictSchema.id_from_object(work, work.class)
+    query = <<~GRAPHQL
+      query($workId: ID!) {
+        node(id: $workId) {
+          id
+          ... on Work {
+            annictId
+            title
+          }
+        }
+      }
+    GRAPHQL
+
+    post "/graphql", params: {variables: {workId: id}, query: query}
+
+    expect(response.status).to eq(401)
+  end
+
+  it "不正なクエリ形式の場合エラーが返ること" do
+    user = create(:user)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application, owner: user)
+    headers = {"Authorization" => "bearer #{access_token.token}"}
+    invalid_query = "invalid query"
+
+    post "/graphql", params: {query: invalid_query}, headers: headers
+
+    expect(response.status).to eq(200)
+    expect(json).to have_key("errors")
+  end
+
+  it "ミューテーションを実行できること" do
+    user = create(:user)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application, owner: user, scopes: "read write")
+    work = create(:work)
+    episode = create(:episode, work: work)
+    mutation = <<~GRAPHQL
+      mutation {
+        createRecord(input: {episodeId: "#{Beta::AnnictSchema.id_from_object(episode, episode.class)}", ratingState: GREAT}) {
+          record {
+            id
+            ratingState
+          }
+        }
+      }
+    GRAPHQL
+    headers = {"Authorization" => "bearer #{access_token.token}"}
+
+    post "/graphql", params: {query: mutation}, headers: headers
+
+    expect(response.status).to eq(200)
+    expect(json).to have_key("data").or(have_key("errors"))
+  end
+
+  it "存在しないノードIDを指定した場合例外が発生すること" do
+    user = create(:user)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application, owner: user)
+    headers = {"Authorization" => "bearer #{access_token.token}"}
+    query = <<~GRAPHQL
+      query {
+        node(id: "V29yay05OTk5OTk5") {
+          id
+        }
+      }
+    GRAPHQL
+
+    expect {
+      post "/graphql", params: {query: query}, headers: headers
+    }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
