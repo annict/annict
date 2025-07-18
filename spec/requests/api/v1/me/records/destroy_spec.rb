641fb0a0cc88007388572ebf430296c1df76eee4
Koji Shimba <me@shimba.co>
2025-07-18 14:05:26 +0900

    Fix

--- spec/requests/api/v1/me/records/destroy_spec.rb
+++ spec/requests/api/v1/me/records/destroy_spec.rb
@@ -1,24 +1,55 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /v1/me/records/:id" do
-  let(:user) { create(:user, :with_profile, :with_setting) }
-  let(:application) { create(:oauth_application, owner: user) }
-  let(:access_token) { create(:oauth_access_token, application: application) }
-  let(:work) { create(:work, :with_current_season) }
-  let(:episode) { create(:episode, work: work) }
-  let!(:record) { create(:episode_record, work: work, episode: episode, user: user) }
+RSpec.describe "DELETE /v1/me/records/:id", type: :request do
+  it "正常に記録を削除できること" do
+    user = create(:user, :with_profile, :with_setting)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application)
+    work = create(:work, :with_current_season)
+    episode = create(:episode, work: work)
+    record = create(:episode_record, work: work, episode: episode, user: user)
 
-  it "responses 204" do
-    delete api("/v1/me/records/#{record.id}", access_token: access_token.token)
-    expect(response.status).to eq(204)
-  end
-
-  it "deletes a record" do
     expect(access_token.owner.episode_records.count).to eq(1)
 
     delete api("/v1/me/records/#{record.id}", access_token: access_token.token)
 
+    expect(response.status).to eq(204)
     expect(access_token.owner.episode_records.count).to eq(0)
+  end
+
+  it "認証トークンがない場合、エラーが返されること" do
+    user = create(:user, :with_profile, :with_setting)
+    work = create(:work, :with_current_season)
+    episode = create(:episode, work: work)
+    record = create(:episode_record, work: work, episode: episode, user: user)
+
+    delete api("/v1/me/records/#{record.id}")
+
+    expect(response.status).to eq(401)
+  end
+
+  it "存在しない記録IDの場合、エラーが返されること" do
+    user = create(:user, :with_profile, :with_setting)
+    application = create(:oauth_application, owner: user)
+    access_token = create(:oauth_access_token, application: application)
+
+    delete api("/v1/me/records/invalid_id", access_token: access_token.token)
+
+    expect(response.status).to eq(400)
+  end
+
+  it "他のユーザーの記録を削除しようとした場合、エラーが返されること" do
+    user1 = create(:user, :with_profile, :with_setting)
+    user2 = create(:user, :with_profile, :with_setting)
+    application = create(:oauth_application, owner: user2)
+    access_token = create(:oauth_access_token, application: application)
+    work = create(:work, :with_current_season)
+    episode = create(:episode, work: work)
+    record = create(:episode_record, work: work, episode: episode, user: user1)
+
+    delete api("/v1/me/records/#{record.id}", access_token: access_token.token)
+
+    expect(response.status).to eq(404)
   end
 end
