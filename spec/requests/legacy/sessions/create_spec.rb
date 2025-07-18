539ae65a40ba3e22ca778bb1f5d6a7ab419e9d73
Koji Shimba <me@shimba.co>
2025-07-18 15:44:59 +0900

    Fix

--- spec/requests/legacy/sessions/create_spec.rb
+++ spec/requests/legacy/sessions/create_spec.rb
@@ -1,25 +1,64 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /legacy/sign_in", type: :request do
-  context "送信データが正しいとき" do
-    let(:user) { create :registered_user }
-    let(:password) { "password" }
+RSpec.describe "POST /legacy/sign_in", type: :request do
+  it "確認済みのユーザーがログインできること" do
+    user = create(:registered_user)
+    password = "password"
+    user.update!(password: password)
 
-    before do
-      user.update(password: password)
-    end
-
-    it "ログインできること" do
-      post "/legacy/sign_in", params: {
-        user: {
-          email_username: user.username,
-          password: password
-        }
+    post "/legacy/sign_in", params: {
+      user: {
+        email_username: user.username,
+        password: password
       }
+    }
 
-      expect(response).to redirect_to("/")
-      expect(request.flash[:notice]).to include("ログインしました")
-    end
+    expect(response).to redirect_to("/")
+    expect(flash[:notice]).to include("ログインしました")
+  end
+
+  it "2020年4月7日以降に登録した未確認ユーザーがログインできないこと" do
+    user = create(:registered_user, created_at: Time.zone.parse("2020-04-07 00:00:01"))
+    password = "password"
+    user.update!(password: password, confirmed_at: nil)
+
+    post "/legacy/sign_in", params: {
+      user: {
+        email_username: user.email,
+        password: password
+      }
+    }
+
+    expect(response).to redirect_to("/")
+    expect(flash[:alert]).to include("メールアドレスの確認が完了していないため、ログインできませんでした")
+  end
+
+  it "存在しないユーザーでログインできないこと" do
+    post "/legacy/sign_in", params: {
+      user: {
+        email_username: "nonexistent@example.com",
+        password: "password"
+      }
+    }
+
+    expect(response).to have_http_status(:ok)
+    expect(flash[:alert]).to include("ユーザが見つかりません")
+  end
+
+  it "パスワードが間違っているときログインできないこと" do
+    user = create(:registered_user)
+    password = "password"
+    user.update!(password: password)
+
+    post "/legacy/sign_in", params: {
+      user: {
+        email_username: user.username,
+        password: "wrong_password"
+      }
+    }
+
+    expect(response).to have_http_status(:ok)
+    expect(flash[:alert]).to include("ログインできません")
   end
 end
