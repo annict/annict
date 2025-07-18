fb4d4df98f5fa4a69eb4e59dc44cbf7e6f8eef50
Koji Shimba <me@shimba.co>
2025-07-18 15:28:42 +0900

    Fix

--- spec/requests/legacy/sessions/new_spec.rb
+++ spec/requests/legacy/sessions/new_spec.rb
@@ -1,9 +1,28 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /legacy/sign_in", type: :request do
-  it "パスワードでログインページが表示されること" do
+RSpec.describe "GET /legacy/sign_in", type: :request do
+  it "ログインしていないとき、パスワードでログインページが表示されること" do
     get "/legacy/sign_in"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("おかえりなさい！")
+  end
+
+  it "ログインしているとき、トップページにリダイレクトされること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
+
+    get "/legacy/sign_in"
+
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to("/")
+  end
+
+  it "backパラメータがあるとき、リダイレクト先として保存されること" do
+    back_url = "/some/path"
+
+    get "/legacy/sign_in", params: {back: back_url}
 
     expect(response.status).to eq(200)
     expect(response.body).to include("おかえりなさい！")
