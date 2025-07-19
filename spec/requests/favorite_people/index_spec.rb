d88197e1fdad9013a3fbdbe72d707f1d83296e88
Koji Shimba <me@shimba.co>
2025-07-19 16:27:19 +0900

    Fix

--- spec/requests/favorite_people/index_spec.rb
+++ spec/requests/favorite_people/index_spec.rb
@@ -1,15 +1,22 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /@:username/favorite_people", type: :request do
-  let!(:user) { create(:registered_user) }
+RSpec.describe "GET /@:username/favorite_people", type: :request do
+  it "お気に入りがいないとき、アクセスできること" do
+    user = create(:registered_user)
+    get "/@#{user.username}/favorite_people"
 
-  context "お気に入りがいないとき" do
-    it "アクセスできること" do
-      get "/@#{user.username}/favorite_people"
+    expect(response.status).to eq(200)
+    expect(response.body).to include("人物はいません")
+  end
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include("人物はいません")
-    end
+  it "お気に入り人物がいるとき、正常にアクセスできること" do
+    user = create(:registered_user)
+    person = create(:person)
+    create(:person_favorite, user: user, person: person)
+
+    get "/@#{user.username}/favorite_people"
+
+    expect(response.status).to eq(200)
   end
 end
