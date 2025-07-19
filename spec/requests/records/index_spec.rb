e89a2e369913425bdb4e4dc77ccc2a5ad9257a4c
Koji Shimba <me@shimba.co>
2025-07-19 16:56:03 +0900

    Fix

--- spec/requests/records/index_spec.rb
+++ spec/requests/records/index_spec.rb
@@ -1,83 +1,69 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /@:username/records", type: :request do
-  context "when user signs in" do
-    let!(:user) { create(:registered_user) }
+RSpec.describe "GET /@:username/records", type: :request do
+  it "ユーザーがログインしていて記録が存在しない場合、記録がないメッセージを表示すること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/@#{user.username}/records"
 
-    context "when records do not exist" do
-      it "displays no exists message" do
-        get "/@#{user.username}/records"
-
-        expect(response.status).to eq(200)
-        expect(response.body).to include("記録はありません")
-      end
-    end
-
-    context "when records exist" do
-      let!(:record_1) { create(:record, user: user) }
-      let!(:episode_record) { create(:episode_record, record: record_1, user: user, body: "楽しかった") }
-      let!(:record_2) { create(:record, user: user) }
-      let!(:work_record) { create(:work_record, user: user, record: record_2, body: "最高") }
-
-      it "displays records" do
-        get "/@#{user.username}/records"
-
-        expect(response.status).to eq(200)
-        expect(response.body).to include(user.profile.name)
-        expect(response.body).to include("楽しかった")
-        expect(response.body).to include("最高")
-      end
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("記録はありません")
   end
 
-  context "when user does not sign in" do
-    let!(:user) { create(:registered_user) }
+  it "ユーザーがログインしていて記録が存在する場合、記録を表示すること" do
+    user = create(:registered_user)
+    record_1 = create(:record, user:)
+    create(:episode_record, record: record_1, user:, body: "楽しかった")
+    record_2 = create(:record, user:)
+    create(:work_record, user:, record: record_2, body: "最高")
+    login_as(user, scope: :user)
 
-    context "when records do not exist" do
-      it "displays no exists message" do
-        get "/@#{user.username}/records"
+    get "/@#{user.username}/records"
 
-        expect(response.status).to eq(200)
-        expect(response.body).to include("記録はありません")
-      end
-    end
-
-    context "when records exist" do
-      let!(:record_1) { create(:record, user: user) }
-      let!(:episode_record) { create(:episode_record, record: record_1, user: user, body: "楽しかった") }
-      let!(:record_2) { create(:record, user: user) }
-      let!(:work_record) { create(:work_record, user: user, record: record_2, body: "最高") }
-
-      it "displays records" do
-        get "/@#{user.username}/records"
-
-        expect(response.status).to eq(200)
-        expect(response.body).to include(user.profile.name)
-        expect(response.body).to include("楽しかった")
-        expect(response.body).to include("最高")
-      end
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include(user.profile.name)
+    expect(response.body).to include("楽しかった")
+    expect(response.body).to include("最高")
   end
 
-  context "when the month parameter is attached" do
-    let!(:user) { create(:registered_user) }
-    let!(:record_1) { create(:record, user: user, watched_at: Time.zone.parse("2020-04-01")) }
-    let!(:record_1_work_record) { create(:work_record, user: user, record: record_1, body: "最高") }
-    let!(:record_2) { create(:record, user: user, watched_at: Time.zone.parse("2020-05-01")) }
-    let!(:record_2_work_record) { create(:work_record, user: user, record: record_2, body: "すごく良かった") }
+  it "ユーザーがログインしていない場合で記録が存在しない場合、記録がないメッセージを表示すること" do
+    user = create(:registered_user)
 
-    it "displays records" do
-      get "/@#{user.username}/records?year=2020&month=5"
+    get "/@#{user.username}/records"
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include(user.profile.name)
-      expect(response.body).to include("すごく良かった")
-      expect(response.body).not_to include("最高")
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("記録はありません")
+  end
+
+  it "ユーザーがログインしていない場合で記録が存在する場合、記録を表示すること" do
+    user = create(:registered_user)
+    record_1 = create(:record, user:)
+    create(:episode_record, record: record_1, user:, body: "楽しかった")
+    record_2 = create(:record, user:)
+    create(:work_record, user:, record: record_2, body: "最高")
+
+    get "/@#{user.username}/records"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include(user.profile.name)
+    expect(response.body).to include("楽しかった")
+    expect(response.body).to include("最高")
+  end
+
+  it "月パラメーターが指定された場合、指定された月の記録のみを表示すること" do
+    user = create(:registered_user)
+    record_1 = create(:record, user:, watched_at: Time.zone.parse("2020-04-01"))
+    create(:work_record, user:, record: record_1, body: "最高")
+    record_2 = create(:record, user:, watched_at: Time.zone.parse("2020-05-01"))
+    create(:work_record, user:, record: record_2, body: "すごく良かった")
+
+    get "/@#{user.username}/records?year=2020&month=5"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include(user.profile.name)
+    expect(response.body).to include("すごく良かった")
+    expect(response.body).not_to include("最高")
   end
 end
