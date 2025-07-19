6cd0e12ccebcabf7c577218a8499dc6a8736fb9e
Koji Shimba <me@shimba.co>
2025-07-20 04:48:46 +0900

    Fix

--- spec/requests/db/casts/update_spec.rb
+++ spec/requests/db/casts/update_spec.rb
@@ -1,90 +1,73 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "PATCH /db/casts/:id", type: :request do
-  context "user does not sign in" do
-    let!(:character) { create(:character) }
-    let!(:person) { create(:person) }
-    let!(:cast) { create(:cast) }
-    let!(:old_cast) { cast.attributes }
-    let!(:cast_params) do
-      {
-        character_id: character.id,
-        person_id: person.id
-      }
-    end
+RSpec.describe "PATCH /db/casts/:id", type: :request do
+  it "ログインしていないとき、キャストを更新できずログインページにリダイレクトすること" do
+    character = FactoryBot.create(:character)
+    person = FactoryBot.create(:person)
+    cast = FactoryBot.create(:cast)
+    old_cast = cast.attributes
+    cast_params = {
+      character_id: character.id,
+      person_id: person.id
+    }
 
-    it "user can not access this page" do
-      patch "/db/casts/#{cast.id}", params: {cast: cast_params}
-      cast.reload
+    patch "/db/casts/#{cast.id}", params: {cast: cast_params}
+    cast.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
 
-      expect(cast.character_id).to eq(old_cast["character_id"])
-      expect(cast.person_id).to eq(old_cast["person_id"])
-    end
+    expect(cast.character_id).to eq(old_cast["character_id"])
+    expect(cast.person_id).to eq(old_cast["person_id"])
   end
 
-  context "user who is not editor signs in" do
-    let!(:character) { create(:character) }
-    let!(:person) { create(:person) }
-    let!(:user) { create(:registered_user) }
-    let!(:cast) { create(:cast) }
-    let!(:old_cast) { cast.attributes }
-    let!(:cast_params) do
-      {
-        character_id: character.id,
-        person_id: person.id
-      }
-    end
+  it "編集者権限のないユーザーがログインしているとき、キャストを更新できずアクセス拒否されること" do
+    character = FactoryBot.create(:character)
+    person = FactoryBot.create(:person)
+    user = FactoryBot.create(:registered_user)
+    cast = FactoryBot.create(:cast)
+    old_cast = cast.attributes
+    cast_params = {
+      character_id: character.id,
+      person_id: person.id
+    }
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      patch "/db/casts/#{cast.id}", params: {cast: cast_params}
-      cast.reload
+    patch "/db/casts/#{cast.id}", params: {cast: cast_params}
+    cast.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(cast.character_id).to eq(old_cast["character_id"])
-      expect(cast.person_id).to eq(old_cast["person_id"])
-    end
+    expect(cast.character_id).to eq(old_cast["character_id"])
+    expect(cast.person_id).to eq(old_cast["person_id"])
   end
 
-  context "user who is editor signs in" do
-    let!(:character) { create(:character) }
-    let!(:person) { create(:person) }
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:cast) { create(:cast) }
-    let!(:old_cast) { cast.attributes }
-    let!(:cast_params) do
-      {
-        character_id: character.id,
-        person_id: person.id
-      }
-    end
-    let!(:attr_names) { cast_params.keys }
+  it "編集者権限のあるユーザーがログインしているとき、キャストを正常に更新できること" do
+    character = FactoryBot.create(:character)
+    person = FactoryBot.create(:person)
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    cast = FactoryBot.create(:cast)
+    old_cast = cast.attributes
+    cast_params = {
+      character_id: character.id,
+      person_id: person.id
+    }
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can update cast" do
-      expect(cast.character_id).to eq(old_cast["character_id"])
-      expect(cast.person_id).to eq(old_cast["person_id"])
+    expect(cast.character_id).to eq(old_cast["character_id"])
+    expect(cast.person_id).to eq(old_cast["person_id"])
 
-      patch "/db/casts/#{cast.id}", params: {cast: cast_params}
-      cast.reload
+    patch "/db/casts/#{cast.id}", params: {cast: cast_params}
+    cast.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("更新しました")
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("更新しました")
 
-      expect(cast.character_id).to eq(character.id)
-      expect(cast.person_id).to eq(person.id)
-    end
+    expect(cast.character_id).to eq(character.id)
+    expect(cast.person_id).to eq(person.id)
   end
 end
