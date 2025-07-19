80a4476757aecbd8968547bdd7825fc50bdcd8d1
Koji Shimba <me@shimba.co>
2025-07-19 18:53:03 +0900

    Fix

--- spec/requests/api/internal/unstars/create_spec.rb
+++ spec/requests/api/internal/unstars/create_spec.rb
@@ -1,55 +1,67 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /api/internal/unstars" do
-  let!(:user) { create(:registered_user) }
+RSpec.describe "POST /api/internal/unstars", type: :request do
+  it "キャラクターのスターを外した時、201ステータスを返すこと" do
+    user = create(:registered_user)
+    character_favorite = create(:character_favorite, user:)
+    data = {starrable_type: "Character", starrable_id: character_favorite.character_id}
 
-  before do
     login_as(user, scope: :user)
+    post api("/api/internal/unstars", data)
+
+    expect(response.status).to eq(201)
   end
 
-  context "when user unstars a character" do
-    let!(:character_favorite) { create(:character_favorite, user: user) }
-    let!(:data) { {starrable_type: "Character", starrable_id: character_favorite.character_id} }
+  it "キャラクターのスターを外した時、レコードが削除されること" do
+    user = create(:registered_user)
+    character_favorite = create(:character_favorite, user:)
+    data = {starrable_type: "Character", starrable_id: character_favorite.character_id}
 
-    it "responses 200" do
-      post api("/api/internal/unstars", data)
-      expect(response.status).to eq(201)
-    end
-
-    it "removes a record" do
-      expect { post api("/api/internal/unstars", data) }
-        .to change { user.favorite_characters.count }.from(1).to(0)
-    end
+    login_as(user, scope: :user)
+    expect { post api("/api/internal/unstars", data) }
+      .to change { user.favorite_characters.count }.from(1).to(0)
   end
 
-  context "when user unstars a organization" do
-    let!(:organization_favorite) { create(:organization_favorite, user: user) }
-    let!(:data) { {starrable_type: "Organization", starrable_id: organization_favorite.organization_id} }
+  it "組織のスターを外した時、201ステータスを返すこと" do
+    user = create(:registered_user)
+    organization_favorite = create(:organization_favorite, user:)
+    data = {starrable_type: "Organization", starrable_id: organization_favorite.organization_id}
 
-    it "responses 200" do
-      post api("/api/internal/unstars", data)
-      expect(response.status).to eq(201)
-    end
+    login_as(user, scope: :user)
+    post api("/api/internal/unstars", data)
 
-    it "removes a record" do
-      expect { post api("/api/internal/unstars", data) }
-        .to change { user.favorite_organizations.count }.from(1).to(0)
-    end
+    expect(response.status).to eq(201)
   end
 
-  context "when user unstars person" do
-    let!(:person_favorite) { create(:person_favorite, user: user) }
-    let!(:data) { {starrable_type: "Person", starrable_id: person_favorite.person_id} }
+  it "組織のスターを外した時、レコードが削除されること" do
+    user = create(:registered_user)
+    organization_favorite = create(:organization_favorite, user:)
+    data = {starrable_type: "Organization", starrable_id: organization_favorite.organization_id}
 
-    it "responses 200" do
-      post api("/api/internal/unstars", data)
-      expect(response.status).to eq(201)
-    end
+    login_as(user, scope: :user)
+    expect { post api("/api/internal/unstars", data) }
+      .to change { user.favorite_organizations.count }.from(1).to(0)
+  end
 
-    it "removes a record" do
-      expect { post api("/api/internal/unstars", data) }
-        .to change { user.favorite_people.count }.from(1).to(0)
-    end
+  it "人物のスターを外した時、201ステータスを返すこと" do
+    user = create(:registered_user)
+    person_favorite = create(:person_favorite, user:)
+    data = {starrable_type: "Person", starrable_id: person_favorite.person_id}
+
+    login_as(user, scope: :user)
+    post api("/api/internal/unstars", data)
+
+    expect(response.status).to eq(201)
+  end
+
+  it "人物のスターを外した時、レコードが削除されること" do
+    user = create(:registered_user)
+    person_favorite = create(:person_favorite, user:)
+    data = {starrable_type: "Person", starrable_id: person_favorite.person_id}
+
+    login_as(user, scope: :user)
+    expect { post api("/api/internal/unstars", data) }
+      .to change { user.favorite_people.count }.from(1).to(0)
   end
 end
