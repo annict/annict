89c207b23845fc4cf38e6afc79b48b8ca6f6c467
Koji Shimba <me@shimba.co>
2025-07-20 04:33:02 +0900

    Fix

--- spec/requests/db/home/show_spec.rb
+++ spec/requests/db/home/show_spec.rb
@@ -1,28 +1,21 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /db", type: :request do
-  context "user does not sign in" do
-    it "responses the home page" do
-      get "/db"
+RSpec.describe "GET /db", type: :request do
+  it "ユーザーがログインしていないとき、ホームページが表示されること" do
+    get "/db"
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include("Annict DBにようこそ！")
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("Annict DBにようこそ！")
   end
 
-  context "user signs in" do
-    let!(:user) { create(:registered_user) }
+  it "ユーザーがログインしているとき、ホームページが表示されること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db"
 
-    it "responses the home page" do
-      get "/db"
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include("Annict DBにようこそ！")
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("Annict DBにようこそ！")
   end
 end
