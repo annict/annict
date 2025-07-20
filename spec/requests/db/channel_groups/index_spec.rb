51547bc770ad79ea9ba9217659cff78b5a98bed1
Koji Shimba <me@shimba.co>
2025-07-20 11:05:37 +0900

    Fix

--- spec/requests/db/channel_groups/index_spec.rb
+++ spec/requests/db/channel_groups/index_spec.rb
@@ -1,31 +1,24 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /db/channel_groups", type: :request do
-  context "user does not sign in" do
-    let!(:channel_group) { ChannelGroup.first }
+RSpec.describe "GET /db/channel_groups", type: :request do
+  it "ログインしていないとき、チャンネルグループ一覧を表示すること" do
+    channel_group = ChannelGroup.first
 
-    it "responses channel_group list" do
-      get "/db/channel_groups"
+    get "/db/channel_groups"
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include(channel_group.name)
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include(channel_group.name)
   end
 
-  context "user signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:channel_group) { ChannelGroup.first }
+  it "ログインしているとき、チャンネルグループ一覧を表示すること" do
+    user = create(:registered_user)
+    channel_group = ChannelGroup.first
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db/channel_groups"
 
-    it "responses channel_group list" do
-      get "/db/channel_groups"
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include(channel_group.name)
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include(channel_group.name)
   end
 end
