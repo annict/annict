ef769ddf11e2853a9052a46fa84e085f53eaae6a
Koji Shimba <me@shimba.co>
2025-07-20 10:53:40 +0900

    Fix

--- spec/requests/db/cast_publishings/destroy_spec.rb
+++ spec/requests/db/cast_publishings/destroy_spec.rb
@@ -1,58 +1,43 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/casts/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:cast) { create(:cast, :published) }
+RSpec.describe "DELETE /db/casts/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    cast = create(:cast, :published)
 
-    it "user can not access this page" do
-      delete "/db/casts/#{cast.id}/publishing"
-      cast.reload
+    delete "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(cast.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(cast.published?).to eq(true)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:cast) { create(:cast, :published) }
+  it "編集者でないユーザーがログインしているとき、アクセスできないこと" do
+    user = create(:registered_user)
+    cast = create(:cast, :published)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    delete "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
-    it "user can not access" do
-      delete "/db/casts/#{cast.id}/publishing"
-      cast.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(cast.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(cast.published?).to eq(true)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:cast) { create(:cast, :published) }
+  it "編集者がログインしているとき、キャストを非公開にできること" do
+    user = create(:registered_user, :with_editor_role)
+    cast = create(:cast, :published)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(cast.published?).to eq(true)
 
-    it "user can unpublish cast" do
-      expect(cast.published?).to eq(true)
+    delete "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
-      delete "/db/casts/#{cast.id}/publishing"
-      cast.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("非公開にしました")
-
-      expect(cast.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("非公開にしました")
+    expect(cast.published?).to eq(false)
   end
 end
