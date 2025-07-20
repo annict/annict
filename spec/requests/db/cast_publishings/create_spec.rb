2af9dec0e22fb70f82eacae973fbc7ad37e5d332
Koji Shimba <me@shimba.co>
2025-07-20 11:03:17 +0900

    Fix

--- spec/requests/db/cast_publishings/create_spec.rb
+++ spec/requests/db/cast_publishings/create_spec.rb
@@ -1,58 +1,64 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /db/casts/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:cast) { create(:cast, :unpublished) }
+RSpec.describe "POST /db/casts/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    cast = FactoryBot.create(:cast, :unpublished)
 
-    it "user can not access this page" do
-      post "/db/casts/#{cast.id}/publishing"
-      cast.reload
+    post "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(cast.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(cast.published?).to eq(false)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:cast) { create(:cast, :unpublished) }
+  it "エディター権限を持たないユーザーがログインしているとき、アクセスできないこと" do
+    user = FactoryBot.create(:registered_user)
+    cast = FactoryBot.create(:cast, :unpublished)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    post "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
-    it "user can not access" do
-      post "/db/casts/#{cast.id}/publishing"
-      cast.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(cast.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(cast.published?).to eq(false)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:cast) { create(:cast, :unpublished) }
+  it "エディター権限を持つユーザーがログインしているとき、キャストを公開できること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    cast = FactoryBot.create(:cast, :unpublished)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(cast.published?).to eq(false)
 
-    it "user can publish cast" do
-      expect(cast.published?).to eq(false)
+    post "/db/casts/#{cast.id}/publishing"
+    cast.reload
 
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("公開しました")
+    expect(cast.published?).to eq(true)
+  end
+
+  it "すでに公開済みのキャストの場合、404エラーになること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    cast = FactoryBot.create(:cast, :published)
+    login_as(user, scope: :user)
+
+    expect(cast.published?).to eq(true)
+
+    expect do
       post "/db/casts/#{cast.id}/publishing"
-      cast.reload
+    end.to raise_error(ActiveRecord::RecordNotFound)
+  end
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("公開しました")
+  it "存在しないキャストIDが指定されたとき、404エラーになること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
 
-      expect(cast.published?).to eq(true)
-    end
+    expect do
+      post "/db/casts/non-existent-id/publishing"
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
