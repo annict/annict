e828556802849d64a450dee4796c168bfb004d8f
Koji Shimba <me@shimba.co>
2025-07-21 18:34:11 +0900

    Fix

--- spec/requests/db/work_publishings/create_spec.rb
+++ spec/requests/db/work_publishings/create_spec.rb
@@ -1,58 +1,67 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /db/works/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:work) { create(:work, :unpublished) }
+RSpec.describe "POST /db/works/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    work = create(:work, :unpublished)
 
-    it "user can not access this page" do
-      post "/db/works/#{work.id}/publishing"
-      work.reload
+    post "/db/works/#{work.id}/publishing"
+    work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(work.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(work.published?).to eq(false)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:work) { create(:work, :unpublished) }
+  it "エディター権限を持たないユーザーの場合、アクセスできないこと" do
+    user = create(:registered_user)
+    work = create(:work, :unpublished)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    post "/db/works/#{work.id}/publishing"
+    work.reload
 
-    it "user can not access" do
-      post "/db/works/#{work.id}/publishing"
-      work.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(work.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(work.published?).to eq(false)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:work) { create(:work, :unpublished) }
+  it "エディター権限を持つユーザーの場合、作品を公開できること" do
+    user = create(:registered_user, :with_editor_role)
+    work = create(:work, :unpublished)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(work.published?).to eq(false)
 
-    it "user can publish work" do
-      expect(work.published?).to eq(false)
+    post "/db/works/#{work.id}/publishing"
+    work.reload
 
-      post "/db/works/#{work.id}/publishing"
-      work.reload
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("公開しました")
+    expect(work.published?).to eq(true)
+  end
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("公開しました")
+  it "すでに公開済みの作品の場合、404エラーになること" do
+    user = create(:registered_user, :with_editor_role)
+    work = create(:work, :published)
+    login_as(user, scope: :user)
 
-      expect(work.published?).to eq(true)
-    end
+    expect { post "/db/works/#{work.id}/publishing" }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "削除済みの作品の場合、404エラーになること" do
+    user = create(:registered_user, :with_editor_role)
+    work = create(:work, :unpublished)
+    work.destroy!
+    login_as(user, scope: :user)
+
+    expect { post "/db/works/#{work.id}/publishing" }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "存在しない作品IDの場合、404エラーになること" do
+    user = create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
+
+    expect { post "/db/works/invalid-id/publishing" }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
