d99e78b479ee0e3e864c21a9f8158eabf597ed3f
Koji Shimba <me@shimba.co>
2025-07-21 17:33:04 +0900

    Fix

--- spec/requests/db/series_works/destroy_spec.rb
+++ spec/requests/db/series_works/destroy_spec.rb
@@ -1,82 +1,68 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/series_works/:id", type: :request do
-  context "user does not sign in" do
-    let!(:series_work) { create(:series_work, :not_deleted) }
+RSpec.describe "DELETE /db/series_works/:id", type: :request do
+  it "ログインしていないとき、アクセスできないこと" do
+    series_work = FactoryBot.create(:series_work, :not_deleted)
 
-    it "user can not access this page" do
-      expect(SeriesWork.count).to eq(1)
+    expect(SeriesWork.count).to eq(1)
 
-      delete "/db/series_works/#{series_work.id}"
-      series_work.reload
+    delete "/db/series_works/#{series_work.id}"
+    series_work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
 
-      expect(SeriesWork.count).to eq(1)
-    end
+    expect(SeriesWork.count).to eq(1)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:series_work) { create(:series_work, :not_deleted) }
+  it "一般ユーザーでログインしているとき、アクセスできないこと" do
+    user = FactoryBot.create(:registered_user)
+    series_work = FactoryBot.create(:series_work, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      expect(SeriesWork.count).to eq(1)
+    expect(SeriesWork.count).to eq(1)
 
-      delete "/db/series_works/#{series_work.id}"
-      series_work.reload
+    delete "/db/series_works/#{series_work.id}"
+    series_work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(SeriesWork.count).to eq(1)
-    end
+    expect(SeriesWork.count).to eq(1)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:series_work) { create(:series_work, :not_deleted) }
+  it "エディターでログインしているとき、アクセスできないこと" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    series_work = FactoryBot.create(:series_work, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      expect(SeriesWork.count).to eq(1)
+    expect(SeriesWork.count).to eq(1)
 
-      delete "/db/series_works/#{series_work.id}"
-      series_work.reload
+    delete "/db/series_works/#{series_work.id}"
+    series_work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(SeriesWork.count).to eq(1)
-    end
+    expect(SeriesWork.count).to eq(1)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:series_work) { create(:series_work, :not_deleted) }
+  it "管理者でログインしているとき、シリーズと作品の関連をソフトデリートできること" do
+    user = FactoryBot.create(:registered_user, :with_admin_role)
+    series_work = FactoryBot.create(:series_work, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can delete series work softly" do
-      expect(SeriesWork.count).to eq(1)
+    expect(SeriesWork.count).to eq(1)
 
-      delete "/db/series_works/#{series_work.id}"
+    delete "/db/series_works/#{series_work.id}"
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("削除しました")
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("削除しました")
 
-      expect(SeriesWork.count).to eq(0)
-    end
+    expect(SeriesWork.count).to eq(0)
   end
 end
