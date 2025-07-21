348c94895449a6c32c2eac30009460458770c9dd
Koji Shimba <me@shimba.co>
2025-07-21 18:01:05 +0900

    Fix

--- spec/requests/db/staffs/update_spec.rb
+++ spec/requests/db/staffs/update_spec.rb
@@ -1,80 +1,63 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "PATCH /db/staffs/:id", type: :request do
-  context "user does not sign in" do
-    let!(:person) { create(:person) }
-    let!(:staff) { create(:staff) }
-    let!(:old_staff) { staff.attributes }
-    let!(:staff_params) do
-      {
-        resource_id: person.id
-      }
-    end
+RSpec.describe "PATCH /db/staffs/:id", type: :request do
+  it "ログインしていないとき、このページにアクセスできないこと" do
+    person = FactoryBot.create(:person)
+    staff = FactoryBot.create(:staff)
+    old_staff = staff.attributes
+    staff_params = {
+      resource_id: person.id
+    }
 
-    it "user can not access this page" do
-      patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
-      staff.reload
+    patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
 
-      expect(staff.resource_id).to eq(old_staff["resource_id"])
-    end
+    expect(staff.resource_id).to eq(old_staff["resource_id"])
   end
 
-  context "user who is not editor signs in" do
-    let!(:person) { create(:person) }
-    let!(:user) { create(:registered_user) }
-    let!(:staff) { create(:staff) }
-    let!(:old_staff) { staff.attributes }
-    let!(:staff_params) do
-      {
-        resource_id: person.id
-      }
-    end
+  it "編集者権限を持たないユーザーでログインしているとき、アクセスできないこと" do
+    person = FactoryBot.create(:person)
+    user = FactoryBot.create(:registered_user)
+    staff = FactoryBot.create(:staff)
+    old_staff = staff.attributes
+    staff_params = {
+      resource_id: person.id
+    }
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
-      staff.reload
+    patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(staff.resource_id).to eq(old_staff["resource_id"])
-    end
+    expect(staff.resource_id).to eq(old_staff["resource_id"])
   end
 
-  context "user who is editor signs in" do
-    let!(:person) { create(:person) }
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:staff) { create(:staff) }
-    let!(:old_staff) { staff.attributes }
-    let!(:staff_params) do
-      {
-        resource_id: person.id
-      }
-    end
-    let!(:attr_names) { staff_params.keys }
+  it "編集者権限を持つユーザーでログインしているとき、スタッフを更新できること" do
+    person = FactoryBot.create(:person)
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    staff = FactoryBot.create(:staff)
+    old_staff = staff.attributes
+    staff_params = {
+      resource_id: person.id
+    }
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can update staff" do
-      expect(staff.resource_id).to eq(old_staff["resource_id"])
+    expect(staff.resource_id).to eq(old_staff["resource_id"])
 
-      patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
-      staff.reload
+    patch "/db/staffs/#{staff.id}", params: {staff: staff_params}
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("更新しました")
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("更新しました")
 
-      expect(staff.resource_id).to eq(person.id)
-    end
+    expect(staff.resource_id).to eq(person.id)
   end
 end
