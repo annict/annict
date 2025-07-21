1e6bd28fdf31c09178f99b8d16bd5dad0d76404b
Koji Shimba <me@shimba.co>
2025-07-21 15:54:52 +0900

    Fix

--- spec/requests/db/characters/destroy_spec.rb
+++ spec/requests/db/characters/destroy_spec.rb
@@ -1,82 +1,80 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/characters/:id", type: :request do
-  context "user does not sign in" do
-    let!(:character) { create(:character, :not_deleted) }
+RSpec.describe "DELETE /db/characters/:id", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトし、キャラクターは削除されないこと" do
+    character = create(:character, :not_deleted)
 
-    it "user can not access this page" do
-      expect(Character.count).to eq(1)
+    expect(Character.count).to eq(1)
 
-      delete "/db/characters/#{character.id}"
-      character.reload
+    delete "/db/characters/#{character.id}"
+    character.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(Character.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(Character.count).to eq(1)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:character) { create(:character, :not_deleted) }
+  it "編集者権限のないユーザーがログインしているとき、アクセスできず、キャラクターは削除されないこと" do
+    user = create(:registered_user)
+    character = create(:character, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Character.count).to eq(1)
 
-    it "user can not access" do
-      expect(Character.count).to eq(1)
+    delete "/db/characters/#{character.id}"
+    character.reload
 
-      delete "/db/characters/#{character.id}"
-      character.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Character.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(Character.count).to eq(1)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:character) { create(:character, :not_deleted) }
+  it "編集者権限のあるユーザーがログインしているとき、アクセスできず、キャラクターは削除されないこと" do
+    user = create(:registered_user, :with_editor_role)
+    character = create(:character, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Character.count).to eq(1)
 
-    it "user can not access" do
-      expect(Character.count).to eq(1)
+    delete "/db/characters/#{character.id}"
+    character.reload
 
-      delete "/db/characters/#{character.id}"
-      character.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Character.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(Character.count).to eq(1)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:character) { create(:character, :not_deleted) }
+  it "管理者権限のあるユーザーがログインしているとき、キャラクターをソフトデリートできること" do
+    user = create(:registered_user, :with_admin_role)
+    character = create(:character, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Character.count).to eq(1)
 
-    it "user can delete character softly" do
-      expect(Character.count).to eq(1)
+    delete "/db/characters/#{character.id}"
 
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("削除しました")
+    expect(Character.count).to eq(0)
+  end
+
+  it "管理者権限のあるユーザーがログインしているとき、存在しないキャラクターIDでアクセスすると404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+    login_as(user, scope: :user)
+
+    expect {
+      delete "/db/characters/99999999"
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "管理者権限のあるユーザーがログインしているとき、既に削除されたキャラクターIDでアクセスすると404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+    character = create(:character, deleted_at: Time.current)
+    login_as(user, scope: :user)
+
+    expect {
       delete "/db/characters/#{character.id}"
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("削除しました")
-
-      expect(Character.count).to eq(0)
-    end
+    }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
