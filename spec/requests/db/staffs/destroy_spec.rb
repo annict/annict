59638f819521193d86a3d71bacbc757a9a943251
Koji Shimba <me@shimba.co>
2025-07-21 18:01:48 +0900

    Fix

--- spec/requests/db/staffs/destroy_spec.rb
+++ spec/requests/db/staffs/destroy_spec.rb
@@ -1,82 +1,85 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/staffs/:id", type: :request do
-  context "user does not sign in" do
-    let!(:staff) { create(:staff, :not_deleted) }
+RSpec.describe "DELETE /db/staffs/:id", type: :request do
+  it "未ログインユーザーは削除できず、ログインページにリダイレクトされること" do
+    staff = create(:staff, :not_deleted)
 
-    it "user can not access this page" do
-      expect(Staff.count).to eq(1)
+    expect(Staff.count).to eq(1)
 
-      delete "/db/staffs/#{staff.id}"
-      staff.reload
+    delete "/db/staffs/#{staff.id}"
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(Staff.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(Staff.count).to eq(1)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:staff) { create(:staff, :not_deleted) }
+  it "一般ユーザーは削除できず、アクセス拒否されること" do
+    user = create(:registered_user)
+    staff = create(:staff, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      expect(Staff.count).to eq(1)
+    expect(Staff.count).to eq(1)
 
-      delete "/db/staffs/#{staff.id}"
-      staff.reload
+    delete "/db/staffs/#{staff.id}"
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Staff.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(Staff.count).to eq(1)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:staff) { create(:staff, :not_deleted) }
+  it "editor権限ユーザーは削除できず、アクセス拒否されること" do
+    user = create(:registered_user, :with_editor_role)
+    staff = create(:staff, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      expect(Staff.count).to eq(1)
+    expect(Staff.count).to eq(1)
 
-      delete "/db/staffs/#{staff.id}"
-      staff.reload
+    delete "/db/staffs/#{staff.id}"
+    staff.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Staff.count).to eq(1)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(Staff.count).to eq(1)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:staff) { create(:staff, :not_deleted) }
+  it "admin権限ユーザーはスタッフをソフトデリートできること" do
+    user = create(:registered_user, :with_admin_role)
+    staff = create(:staff, :not_deleted)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can delete staff softly" do
-      expect(Staff.count).to eq(1)
+    expect(Staff.count).to eq(1)
 
+    delete "/db/staffs/#{staff.id}"
+
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("削除しました")
+    expect(Staff.count).to eq(0)
+  end
+
+  it "存在しないスタッフIDを指定した場合、404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+
+    login_as(user, scope: :user)
+
+    expect do
+      delete "/db/staffs/999999"
+    end.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "既に削除済みのスタッフを指定した場合、404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+    staff = create(:staff, :deleted)
+
+    login_as(user, scope: :user)
+
+    expect do
       delete "/db/staffs/#{staff.id}"
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("削除しました")
-
-      expect(Staff.count).to eq(0)
-    end
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
