854915e859c77a510705d15aaba6ca9c91851ef7
Koji Shimba <me@shimba.co>
2025-07-21 18:55:58 +0900

    Fix

--- spec/requests/db/slots/new_spec.rb
+++ spec/requests/db/slots/new_spec.rb
@@ -1,47 +1,85 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /db/works/:work_id/slots/new", type: :request do
-  context "user does not sign in" do
-    let!(:work) { create(:work) }
+RSpec.describe "GET /db/works/:work_id/slots/new", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    work = FactoryBot.create(:work)
 
-    it "user can not access this page" do
-      get "/db/works/#{work.id}/slots/new"
+    get "/db/works/#{work.id}/slots/new"
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
   end
 
-  context "user who is not editor signs in" do
-    let!(:work) { create(:work) }
-    let!(:user) { create(:registered_user) }
+  it "エディター権限を持たないユーザーがログインしているとき、アクセスできないこと" do
+    work = FactoryBot.create(:work)
+    user = FactoryBot.create(:registered_user)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db/works/#{work.id}/slots/new"
 
-    it "can not access" do
-      get "/db/works/#{work.id}/slots/new"
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
   end
 
-  context "user who is editor signs in" do
-    let!(:work) { create(:work) }
-    let!(:user) { create(:registered_user, :with_editor_role) }
+  it "エディター権限を持つユーザーがログインしているとき、ページが表示されること" do
+    work = FactoryBot.create(:work)
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db/works/#{work.id}/slots/new"
 
-    it "responses page" do
+    expect(response.status).to eq(200)
+    expect(response.body).to include("放送予定登録")
+  end
+
+  it "管理者権限を持つユーザーがログインしているとき、ページが表示されること" do
+    work = FactoryBot.create(:work)
+    user = FactoryBot.create(:registered_user, :with_admin_role)
+    login_as(user, scope: :user)
+
+    get "/db/works/#{work.id}/slots/new"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("放送予定登録")
+  end
+
+  it "存在しない作品IDを指定したとき、404エラーになること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
+
+    expect {
+      get "/db/works/999999/slots/new"
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "削除された作品を指定したとき、404エラーになること" do
+    work = FactoryBot.create(:work)
+    work.destroy_in_batches
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
+
+    expect {
       get "/db/works/#{work.id}/slots/new"
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include("放送予定登録")
+  it "program_idsパラメータが指定されたとき、フォームにデフォルト値が設定されること" do
+    work = FactoryBot.create(:work)
+    # Channelは事前にデータベースに存在している前提のテスト
+    # Channel.firstを使用する
+    if Channel.count == 0
+      channel_group = FactoryBot.create(:channel_group)
+      Channel.create!(name: "テストチャンネル", channel_group:)
     end
+    program1 = FactoryBot.create(:program, work:, started_at: Time.current)
+    program2 = FactoryBot.create(:program, work:, started_at: 1.day.from_now)
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
+
+    get "/db/works/#{work.id}/slots/new", params: {program_ids: [program1.id, program2.id]}
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("放送予定登録")
   end
 end
