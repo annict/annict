44dccff2f1b7290e85e47a3fdee62df38c6bcf05
Koji Shimba <me@shimba.co>
2025-07-21 17:34:38 +0900

    Fix

--- spec/requests/db/series_works/update_spec.rb
+++ spec/requests/db/series_works/update_spec.rb
@@ -1,93 +1,84 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "PATCH /db/series_works/:id", type: :request do
-  context "user does not sign in" do
-    let!(:series_work) { create(:series_work) }
-    let!(:old_series_work) { series_work.attributes }
-    let!(:series_work_params) do
-      {
-        work_id: series_work.work_id,
-        summary: "2期",
-        summary_en: "Season 2"
-      }
+RSpec.describe "PATCH /db/series_works/:id", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    series_work = FactoryBot.create(:series_work)
+    old_series_work = series_work.attributes
+    series_work_params = {
+      work_id: series_work.work_id,
+      summary: "2期",
+      summary_en: "Season 2"
+    }
+
+    patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
+    series_work.reload
+
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(series_work.summary).to eq(old_series_work["summary"])
+  end
+
+  it "編集者権限を持たないユーザーでログインしているとき、アクセスが拒否されること" do
+    user = FactoryBot.create(:registered_user)
+    series_work = FactoryBot.create(:series_work)
+    old_series_work = series_work.attributes
+    series_work_params = {
+      work_id: series_work.work_id,
+      summary: "2期",
+      summary_en: "Season 2"
+    }
+
+    login_as(user, scope: :user)
+    patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
+    series_work.reload
+
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(series_work.summary).to eq(old_series_work["summary"])
+  end
+
+  it "編集者権限を持つユーザーでログインしているとき、シリーズワークを更新できること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    series_work = FactoryBot.create(:series_work)
+    old_series_work = series_work.attributes
+    series_work_params = {
+      work_id: series_work.work_id,
+      summary: "2期",
+      summary_en: "Season 2"
+    }
+    attr_names = %i[work_id summary summary_en]
+
+    # 更新前の値を確認
+    attr_names.each do |attr_name|
+      expect(series_work.send(attr_name)).to eq(old_series_work[attr_name.to_s])
     end
 
-    it "user can not access this page" do
-      patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
-      series_work.reload
+    login_as(user, scope: :user)
+    patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
+    series_work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("更新しました")
 
-      expect(series_work.summary).to eq(old_series_work["summary"])
+    # 更新後の値を確認
+    attr_names.each do |attr_name|
+      expect(series_work.send(attr_name)).to eq(series_work_params[attr_name])
     end
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:series_work) { create(:series_work) }
-    let!(:old_series_work) { series_work.attributes }
-    let!(:series_work_params) do
-      {
-        work_id: series_work.work_id,
-        summary: "2期",
-        summary_en: "Season 2"
-      }
-    end
+  it "編集者権限を持つユーザーでログインしているとき、無効なパラメータでバリデーションエラーが発生すること" do
+    user = FactoryBot.create(:registered_user, :with_editor_role)
+    series_work = FactoryBot.create(:series_work)
+    invalid_params = {
+      work_id: nil,
+      summary: "2期",
+      summary_en: "Season 2"
+    }
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
+    patch "/db/series_works/#{series_work.id}", params: {series_work: invalid_params}
 
-    it "user can not access" do
-      patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
-      series_work.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(series_work.summary).to eq(old_series_work["summary"])
-    end
-  end
-
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:series_work) { create(:series_work) }
-    let!(:old_series_work) { series_work.attributes }
-    let!(:series_work_params) do
-      {
-        work_id: series_work.work_id,
-        summary: "2期",
-        summary_en: "Season 2"
-      }
-    end
-    let!(:attr_names) do
-      %i[
-        work_id
-        summary
-        summary_en
-      ]
-    end
-
-    before do
-      login_as(user, scope: :user)
-    end
-
-    it "user can update series work" do
-      attr_names.each do |attr_name|
-        expect(series_work.send(attr_name)).to eq(old_series_work[attr_name.to_s])
-      end
-
-      patch "/db/series_works/#{series_work.id}", params: {series_work: series_work_params}
-      series_work.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("更新しました")
-
-      attr_names.each do |attr_name|
-        expect(series_work.send(attr_name)).to eq(series_work_params[attr_name])
-      end
-    end
+    expect(response.status).to eq(422)
   end
 end
