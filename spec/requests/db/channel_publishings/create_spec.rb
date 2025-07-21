462c271d1e8a419082d3d800cbb7cf1b67f8b78a
Koji Shimba <me@shimba.co>
2025-07-21 15:48:50 +0900

    Fix

--- spec/requests/db/channel_publishings/create_spec.rb
+++ spec/requests/db/channel_publishings/create_spec.rb
@@ -1,77 +1,56 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /db/channels/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:channel) { Channel.first.tap { |c| c.unpublish } }
+RSpec.describe "POST /db/channels/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    channel = Channel.first.tap { |c| c.unpublish }
 
-    it "user can not access this page" do
-      post "/db/channels/#{channel.id}/publishing"
-      channel.reload
+    post "/db/channels/#{channel.id}/publishing"
+    channel.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(channel.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(channel.published?).to eq(false)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:channel) { Channel.first.tap { |c| c.unpublish } }
+  it "編集者権限を持たない一般ユーザーでログインしているとき、アクセスできないこと" do
+    user = create(:registered_user)
+    channel = Channel.first.tap { |c| c.unpublish }
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    post "/db/channels/#{channel.id}/publishing"
+    channel.reload
 
-    it "user can not access" do
-      post "/db/channels/#{channel.id}/publishing"
-      channel.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(channel.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(channel.published?).to eq(false)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:channel) { Channel.first.tap { |c| c.unpublish } }
+  it "編集者権限を持つユーザーでログインしているとき、アクセスできないこと" do
+    user = create(:registered_user, :with_editor_role)
+    channel = Channel.first.tap { |c| c.unpublish }
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    post "/db/channels/#{channel.id}/publishing"
+    channel.reload
 
-    it "user can not access" do
-      post "/db/channels/#{channel.id}/publishing"
-      channel.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(channel.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(channel.published?).to eq(false)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:channel) { Channel.first.tap { |c| c.unpublish } }
+  it "管理者権限を持つユーザーでログインしているとき、チャンネルを公開できること" do
+    user = create(:registered_user, :with_admin_role)
+    channel = Channel.first.tap { |c| c.unpublish }
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(channel.published?).to eq(false)
 
-    it "user can publish channel" do
-      expect(channel.published?).to eq(false)
+    post "/db/channels/#{channel.id}/publishing"
+    channel.reload
 
-      post "/db/channels/#{channel.id}/publishing"
-      channel.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("公開しました")
-
-      expect(channel.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("公開しました")
+    expect(channel.published?).to eq(true)
   end
 end
