0f99d00813015465ee34adaa3ca21a87e1bcc039
Koji Shimba <me@shimba.co>
2025-07-21 15:08:01 +0900

    Fix

--- spec/requests/db/channel_group_publishings/destroy_spec.rb
+++ spec/requests/db/channel_group_publishings/destroy_spec.rb
@@ -1,77 +1,56 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/channel_groups/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:channel_group) { ChannelGroup.first }
+RSpec.describe "DELETE /db/channel_groups/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    channel_group = ChannelGroup.first
 
-    it "user can not access this page" do
-      delete "/db/channel_groups/#{channel_group.id}/publishing"
-      channel_group.reload
+    delete "/db/channel_groups/#{channel_group.id}/publishing"
+    channel_group.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(channel_group.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(channel_group.published?).to eq(true)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:channel_group) { ChannelGroup.first }
+  it "エディターではないユーザーがログインしているとき、アクセスできないこと" do
+    user = create(:registered_user)
+    channel_group = ChannelGroup.first
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    delete "/db/channel_groups/#{channel_group.id}/publishing"
+    channel_group.reload
 
-    it "user can not access" do
-      delete "/db/channel_groups/#{channel_group.id}/publishing"
-      channel_group.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(channel_group.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(channel_group.published?).to eq(true)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:channel_group) { ChannelGroup.first }
+  it "エディターロールを持つユーザーがログインしているとき、アクセスできないこと" do
+    user = create(:registered_user, :with_editor_role)
+    channel_group = ChannelGroup.first
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    delete "/db/channel_groups/#{channel_group.id}/publishing"
+    channel_group.reload
 
-    it "user can not access" do
-      delete "/db/channel_groups/#{channel_group.id}/publishing"
-      channel_group.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(channel_group.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(channel_group.published?).to eq(true)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:channel_group) { ChannelGroup.first }
+  it "管理者ロールを持つユーザーがログインしているとき、チャンネルグループを非公開にできること" do
+    user = create(:registered_user, :with_admin_role)
+    channel_group = ChannelGroup.first
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(channel_group.published?).to eq(true)
 
-    it "user can unpublish channel_group" do
-      expect(channel_group.published?).to eq(true)
+    delete "/db/channel_groups/#{channel_group.id}/publishing"
+    channel_group.reload
 
-      delete "/db/channel_groups/#{channel_group.id}/publishing"
-      channel_group.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("非公開にしました")
-
-      expect(channel_group.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("非公開にしました")
+    expect(channel_group.published?).to eq(false)
   end
 end
