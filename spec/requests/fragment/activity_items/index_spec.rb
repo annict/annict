0d301d0007b974e45451c405f8d9d92a039ef73a
Koji Shimba <me@shimba.co>
2025-07-22 00:59:58 +0900

    Fix

--- spec/requests/fragment/activity_items/index_spec.rb
+++ spec/requests/fragment/activity_items/index_spec.rb
@@ -1,23 +1,40 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /fragment/activity_groups/:activity_group_id/items", type: :request do
-  context "ログインしているとき" do
-    let!(:user) { create(:registered_user) }
-    let!(:episode_record) { create(:episode_record, user: user, body: "楽しかった") }
-    let!(:activity_group) { create(:activity_group, user: user, itemable_type: "EpisodeRecord", single: true) }
-    let!(:activity) { create(:activity, user: user, itemable: episode_record, activity_group: activity_group) }
+RSpec.describe "GET /fragment/activity_groups/:activity_group_id/items", type: :request do
+  it "ログインしているとき、アクティビティに紐付く記録を表示すること" do
+    user = create(:registered_user)
+    episode_record = create(:episode_record, user: user, body: "楽しかった")
+    activity_group = create(:activity_group, user: user, itemable_type: "EpisodeRecord", single: true)
+    create(:activity, user: user, itemable: episode_record, activity_group: activity_group)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
+    get "/fragment/activity_groups/#{activity_group.id}/items"
 
-    it "アクティビティに紐付く記録を表示すること" do
-      get "/fragment/activity_groups/#{activity_group.id}/items"
+    expect(response.status).to eq(200)
+    expect(response.body).to include(episode_record.episode.title)
+    expect(response.body).to include("楽しかった")
+  end
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include(episode_record.episode.title)
-      expect(response.body).to include("楽しかった")
-    end
+  it "ログインしていないとき、アクティビティが紐付いていればアクセスできること" do
+    user = create(:registered_user)
+    episode_record = create(:episode_record, user: user, body: "面白かった")
+    activity_group = create(:activity_group, user: user, itemable_type: "EpisodeRecord", single: true)
+    create(:activity, user: user, itemable: episode_record, activity_group: activity_group)
+
+    get "/fragment/activity_groups/#{activity_group.id}/items"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include(episode_record.episode.title)
+    expect(response.body).to include("面白かった")
+  end
+
+  it "存在しないactivity_groupを指定したとき、RecordNotFoundエラーが発生すること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
+
+    expect {
+      get "/fragment/activity_groups/non-existent-id/items"
+    }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
