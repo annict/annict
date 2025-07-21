6fbdf1f67d7ced7d469c3be1289b90bbede13e08
Koji Shimba <me@shimba.co>
2025-07-21 17:14:43 +0900

    Fix

--- spec/requests/db/people/new_spec.rb
+++ spec/requests/db/people/new_spec.rb
@@ -1,43 +1,31 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /db/people/new", type: :request do
-  context "user does not sign in" do
-    it "user can not access this page" do
-      get "/db/people/new"
+RSpec.describe "GET /db/people/new", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    get "/db/people/new"
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
+  it "編集者権限がないユーザーとしてログインしているとき、アクセスできないこと" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db/people/new"
 
-    it "can not access" do
-      get "/db/people/new"
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
+  it "編集者権限があるユーザーとしてログインしているとき、人物登録ページが表示されること" do
+    user = create(:registered_user, :with_editor_role)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/db/people/new"
 
-    it "responses page" do
-      get "/db/people/new"
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include("人物登録")
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("人物登録")
   end
 end
