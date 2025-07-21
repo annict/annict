b3d7a9120cd64ea6dc1807484ef0ce7a695f5ab1
Koji Shimba <me@shimba.co>
2025-07-21 17:34:08 +0900

    Fix

--- spec/requests/db/program_publishings/destroy_spec.rb
+++ spec/requests/db/program_publishings/destroy_spec.rb
@@ -1,58 +1,49 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/programs/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:program) { create(:program, :published) }
+RSpec.describe "DELETE /db/programs/:id/publishing", type: :request do
+  it "ログインしていないとき、アクセスできず番組の公開状態が変更されないこと" do
+    channel_group = create(:channel_group)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :published, channel:)
 
-    it "user can not access this page" do
-      delete "/db/programs/#{program.id}/publishing"
-      program.reload
+    delete "/db/programs/#{program.id}/publishing"
+    program.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(program.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(program.published?).to eq(true)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:program) { create(:program, :published) }
+  it "編集者ではないユーザーがログインしているとき、アクセスできず番組の公開状態が変更されないこと" do
+    user = create(:registered_user)
+    channel_group = create(:channel_group)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :published, channel:)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    delete "/db/programs/#{program.id}/publishing"
+    program.reload
 
-    it "user can not access" do
-      delete "/db/programs/#{program.id}/publishing"
-      program.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(program.published?).to eq(true)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(program.published?).to eq(true)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:program) { create(:program, :published) }
+  it "編集者がログインしているとき、番組を非公開にできること" do
+    user = create(:registered_user, :with_editor_role)
+    channel_group = create(:channel_group)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :published, channel:)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(program.published?).to eq(true)
 
-    it "user can unpublish program" do
-      expect(program.published?).to eq(true)
+    delete "/db/programs/#{program.id}/publishing"
+    program.reload
 
-      delete "/db/programs/#{program.id}/publishing"
-      program.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("非公開にしました")
-
-      expect(program.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("非公開にしました")
+    expect(program.published?).to eq(false)
   end
 end
