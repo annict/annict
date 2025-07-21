df0fcaf0f18c9e3d3ffa500e437b9ad381bb7e32
Koji Shimba <me@shimba.co>
2025-07-21 18:24:08 +0900

    Fix

--- spec/requests/db/works/destroy_spec.rb
+++ spec/requests/db/works/destroy_spec.rb
@@ -1,82 +1,83 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "DELETE /db/works/:id", type: :request do
-  context "user does not sign in" do
-    let!(:work) { create(:work, :not_deleted) }
+RSpec.describe "DELETE /db/works/:id", type: :request do
+  it "ユーザーがサインインしていないとき、ログインページにリダイレクトすること" do
+    work = create(:work, :not_deleted)
 
-    it "user can not access this page" do
-      expect(Work.count).to eq(1)
+    expect(Work.count).to eq(1)
 
-      delete "/db/works/#{work.id}"
-      work.reload
+    delete "/db/works/#{work.id}"
+    work.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
 
-      expect(Work.count).to eq(1)
-    end
+    expect(Work.count).to eq(1)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:work) { create(:work, :not_deleted) }
+  it "エディター権限を持たないユーザーがサインインしているとき、アクセスが拒否されること" do
+    user = create(:registered_user)
+    work = create(:work, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Work.count).to eq(1)
 
-    it "user can not access" do
-      expect(Work.count).to eq(1)
+    delete "/db/works/#{work.id}"
+    work.reload
 
-      delete "/db/works/#{work.id}"
-      work.reload
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Work.count).to eq(1)
-    end
+    expect(Work.count).to eq(1)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:work) { create(:work, :not_deleted) }
+  it "エディター権限を持つユーザーがサインインしているとき、アクセスが拒否されること" do
+    user = create(:registered_user, :with_editor_role)
+    work = create(:work, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Work.count).to eq(1)
 
-    it "user can not access" do
-      expect(Work.count).to eq(1)
+    delete "/db/works/#{work.id}"
+    work.reload
 
-      delete "/db/works/#{work.id}"
-      work.reload
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(Work.count).to eq(1)
-    end
+    expect(Work.count).to eq(1)
   end
 
-  context "user who is admin signs in" do
-    let!(:user) { create(:registered_user, :with_admin_role) }
-    let!(:work) { create(:work, :not_deleted) }
+  it "管理者権限を持つユーザーがサインインしているとき、作品を論理削除できること" do
+    user = create(:registered_user, :with_admin_role)
+    work = create(:work, :not_deleted)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    expect(Work.count).to eq(1)
 
-    it "user can delete work softly" do
-      expect(Work.count).to eq(1)
+    delete "/db/works/#{work.id}"
 
-      delete "/db/works/#{work.id}"
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("削除しました")
 
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("削除しました")
+    expect(Work.count).to eq(0)
+  end
 
-      expect(Work.count).to eq(0)
-    end
+  it "存在しない作品IDを指定したとき、404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+    login_as(user, scope: :user)
+
+    expect { delete "/db/works/non-existent-id" }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "既に削除済みの作品を削除しようとしたとき、404エラーになること" do
+    user = create(:registered_user, :with_admin_role)
+    work = create(:work, :not_deleted)
+    login_as(user, scope: :user)
+
+    # 先に削除する
+    work.destroy!
+
+    expect { delete "/db/works/#{work.id}" }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
