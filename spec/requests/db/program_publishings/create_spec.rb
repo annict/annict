41509690fed17de846818908bcd6df09223504ce
Koji Shimba <me@shimba.co>
2025-07-21 17:30:13 +0900

    Fix

--- spec/requests/db/program_publishings/create_spec.rb
+++ spec/requests/db/program_publishings/create_spec.rb
@@ -1,58 +1,74 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "POST /db/programs/:id/publishing", type: :request do
-  context "user does not sign in" do
-    let!(:program) { create(:program, :unpublished) }
+RSpec.describe "POST /db/programs/:id/publishing", type: :request do
+  it "ログインしていないとき、ログインページにリダイレクトされること" do
+    channel_group = ChannelGroup.create!(name: "地上波", sort_number: 1)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :unpublished, channel:)
 
-    it "user can not access this page" do
-      post "/db/programs/#{program.id}/publishing"
-      program.reload
+    post "/db/programs/#{program.id}/publishing"
+    program.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("ログインしてください")
-
-      expect(program.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("ログインしてください")
+    expect(program.published?).to eq(false)
   end
 
-  context "user who is not editor signs in" do
-    let!(:user) { create(:registered_user) }
-    let!(:program) { create(:program, :unpublished) }
+  it "編集者でないユーザーがログインしているとき、アクセスが拒否されること" do
+    user = create(:registered_user)
+    channel_group = ChannelGroup.create!(name: "地上波", sort_number: 1)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :unpublished, channel:)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can not access" do
-      post "/db/programs/#{program.id}/publishing"
-      program.reload
+    post "/db/programs/#{program.id}/publishing"
+    program.reload
 
-      expect(response.status).to eq(302)
-      expect(flash[:alert]).to eq("アクセスできません")
-
-      expect(program.published?).to eq(false)
-    end
+    expect(response.status).to eq(302)
+    expect(flash[:alert]).to eq("アクセスできません")
+    expect(program.published?).to eq(false)
   end
 
-  context "user who is editor signs in" do
-    let!(:user) { create(:registered_user, :with_editor_role) }
-    let!(:program) { create(:program, :unpublished) }
+  it "編集者がログインしているとき、プログラムを公開できること" do
+    user = create(:registered_user, :with_editor_role)
+    channel_group = ChannelGroup.create!(name: "地上波", sort_number: 1)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :unpublished, channel:)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    login_as(user, scope: :user)
 
-    it "user can publish program" do
-      expect(program.published?).to eq(false)
+    expect(program.published?).to eq(false)
 
+    post "/db/programs/#{program.id}/publishing"
+    program.reload
+
+    expect(response.status).to eq(302)
+    expect(flash[:notice]).to eq("公開しました")
+    expect(program.published?).to eq(true)
+  end
+
+  it "編集者がログインしているとき、存在しないプログラムIDを指定すると404エラーが発生すること" do
+    user = create(:registered_user, :with_editor_role)
+
+    login_as(user, scope: :user)
+
+    expect do
+      post "/db/programs/nonexistent-id/publishing"
+    end.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "編集者がログインしているとき、既に公開済みのプログラムを指定すると404エラーが発生すること" do
+    user = create(:registered_user, :with_editor_role)
+    channel_group = ChannelGroup.create!(name: "地上波", sort_number: 1)
+    channel = Channel.create!(channel_group:, name: "テレビ東京", sort_number: 1)
+    program = create(:program, :published, channel:)
+
+    login_as(user, scope: :user)
+
+    expect do
       post "/db/programs/#{program.id}/publishing"
-      program.reload
-
-      expect(response.status).to eq(302)
-      expect(flash[:notice]).to eq("公開しました")
-
-      expect(program.published?).to eq(true)
-    end
+    end.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
