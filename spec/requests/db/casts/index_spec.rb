9241239b996d4701291340a7b877c90f99038fd0
Koji Shimba <me@shimba.co>
2025-07-22 19:24:45 +0900

    Fix

--- spec/requests/db/casts/index_spec.rb
+++ spec/requests/db/casts/index_spec.rb
@@ -3,7 +3,7 @@
 
 RSpec.describe "GET /db/works/:work_id/casts", type: :request do
   it "ユーザーがログインしていないとき、キャスト一覧が表示されること" do
-    cast = create(:cast)
+    cast = FactoryBot.create(:cast)
 
     get "/db/works/#{cast.work_id}/casts"
 
@@ -13,8 +13,8 @@ RSpec.describe "GET /db/works/:work_id/casts", type: :request do
   end
 
   it "ユーザーがログインしているとき、キャスト一覧が表示されること" do
-    user = create(:registered_user)
-    cast = create(:cast)
+    user = FactoryBot.create(:registered_user)
+    cast = FactoryBot.create(:cast)
     login_as(user, scope: :user)
 
     get "/db/works/#{cast.work_id}/casts"
@@ -25,8 +25,8 @@ RSpec.describe "GET /db/works/:work_id/casts", type: :request do
   end
 
   it "削除済みのキャストは表示されないこと" do
-    cast = create(:cast)
-    deleted_cast = create(:cast, work: cast.work, deleted_at: Time.current)
+    cast = FactoryBot.create(:cast)
+    deleted_cast = FactoryBot.create(:cast, work: cast.work, deleted_at: Time.current)
 
     get "/db/works/#{cast.work_id}/casts"
 
@@ -36,10 +36,10 @@ RSpec.describe "GET /db/works/:work_id/casts", type: :request do
   end
 
   it "キャストがソート番号順に表示されること" do
-    work = create(:work)
-    cast1 = create(:cast, work: work, sort_number: 2)
-    cast2 = create(:cast, work: work, sort_number: 1)
-    cast3 = create(:cast, work: work, sort_number: 3)
+    work = FactoryBot.create(:work)
+    cast1 = FactoryBot.create(:cast, work:, sort_number: 2)
+    cast2 = FactoryBot.create(:cast, work:, sort_number: 1)
+    cast3 = FactoryBot.create(:cast, work:, sort_number: 3)
 
     get "/db/works/#{work.id}/casts"
 
@@ -53,7 +53,7 @@ RSpec.describe "GET /db/works/:work_id/casts", type: :request do
   end
 
   it "削除済みの作品にアクセスしたとき、RecordNotFoundエラーが発生すること" do
-    work = create(:work, deleted_at: Time.current)
+    work = FactoryBot.create(:work, deleted_at: Time.current)
 
     expect {
       get "/db/works/#{work.id}/casts"
@@ -64,5 +64,48 @@ RSpec.describe "GET /db/works/:work_id/casts", type: :request do
     expect {
       get "/db/works/999999/casts"
     }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "キャストが存在しない作品にアクセスしたとき、空のリストが表示されること" do
+    work = FactoryBot.create(:work)
+
+    get "/db/works/#{work.id}/casts"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("キャスト")
+  end
+
+  it "複数の削除済みキャストと通常のキャストが混在するとき、削除済みキャストは表示されないこと" do
+    work = FactoryBot.create(:work)
+    cast1 = FactoryBot.create(:cast, work:, sort_number: 1)
+    deleted_cast1 = FactoryBot.create(:cast, work:, sort_number: 2, deleted_at: Time.current)
+    cast2 = FactoryBot.create(:cast, work:, sort_number: 3)
+    deleted_cast2 = FactoryBot.create(:cast, work:, sort_number: 4, deleted_at: Time.current)
+    cast3 = FactoryBot.create(:cast, work:, sort_number: 5)
+
+    get "/db/works/#{work.id}/casts"
+
+    expect(response.status).to eq(200)
+    # 通常のキャストは表示される
+    expect(response.body).to include(cast1.character.name)
+    expect(response.body).to include(cast2.character.name)
+    expect(response.body).to include(cast3.character.name)
+    # 削除済みキャストは表示されない
+    expect(response.body).not_to include(deleted_cast1.character.name)
+    expect(response.body).not_to include(deleted_cast2.character.name)
+  end
+
+  it "キャラクター名やパーソン名にHTMLエスケープが必要な文字が含まれる場合、適切にエスケープされること" do
+    work = FactoryBot.create(:work)
+    character = FactoryBot.create(:character, name: '<script>alert("XSS")</script>')
+    person = FactoryBot.create(:person, name: '"テスト" & テスト')
+    FactoryBot.create(:cast, work:, character:, person:)
+
+    get "/db/works/#{work.id}/casts"
+
+    expect(response.status).to eq(200)
+    # HTMLエスケープされた文字列が含まれていることを確認
+    expect(response.body).to include("&lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;")
+    expect(response.body).to include("&quot;テスト&quot; &amp; テスト")
   end
 end
