cb736b0f71fd5cc2338f42bb571be5f4fd4a76c7
Koji Shimba <me@shimba.co>
2025-07-22 13:29:09 +0900

    Fix

--- spec/requests/sign_up/new_spec.rb
+++ spec/requests/sign_up/new_spec.rb
@@ -1,11 +1,32 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /sign_up", type: :request do
-  it "サインアップページが表示されること" do
+RSpec.describe "GET /sign_up", type: :request do
+  it "ログインしていないとき、サインアップページが正常に表示されること" do
     get "/sign_up"
 
-    expect(response.status).to eq(200)
+    expect(response).to have_http_status(:ok)
     expect(response.body).to include("アカウント作成")
+  end
+
+  it "既にログインしているユーザーの場合、トップページにリダイレクトされること" do
+    user = FactoryBot.create(:user)
+    login_as(user, scope: :user)
+
+    get "/sign_up"
+
+    expect(response).to redirect_to(root_path)
+  end
+
+  it "レスポンスヘッダーにContent-Typeが正しく設定されること" do
+    get "/sign_up"
+
+    expect(response.content_type).to start_with("text/html")
+  end
+
+  it "main_simpleレイアウトが使用されること" do
+    get "/sign_up"
+
+    expect(response.body).not_to include("class=\"main-layout\"")
   end
 end
