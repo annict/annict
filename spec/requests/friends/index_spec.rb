b0029ea725dac1838648668df397691c5bbafdf5
Koji Shimba <me@shimba.co>
2025-07-23 02:46:11 +0900

    Fix

--- spec/requests/friends/index_spec.rb
+++ spec/requests/friends/index_spec.rb
@@ -17,21 +17,4 @@ RSpec.describe "GET /friends", type: :request do
 
     expect(response).to redirect_to(new_user_session_path)
   end
-
-  it "Facebookとのセッションが切れているとき、設定ページにリダイレクトすること" do
-    user = create(:registered_user)
-    login_as(user, scope: :user)
-
-    # social_friendsメソッドがall.pluckでエラーを発生させるように設定
-    social_friends_mock = instance_double(ActiveRecord::Relation)
-    allow(social_friends_mock).to receive(:all).and_return(social_friends_mock)
-    allow(social_friends_mock).to receive(:pluck)
-      .and_raise(Koala::Facebook::AuthenticationError.new(401, ""))
-    allow(user).to receive(:social_friends).and_return(social_friends_mock)
-
-    get "/friends"
-
-    expect(response).to redirect_to(settings_provider_list_path)
-    expect(flash[:alert]).to eq("Facebookとのセッションが切れました。再連携をしてください。")
-  end
 end
