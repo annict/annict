1c53cf795289e6a409474da25bd24e40b597144b
Koji Shimba <me@shimba.co>
2025-07-22 13:29:32 +0900

    Fix

--- spec/requests/sign_in/new_spec.rb
+++ spec/requests/sign_in/new_spec.rb
@@ -1,28 +1,55 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /sign_in", type: :request do
-  context "ログインしているとき" do
-    let!(:user) { create(:registered_user) }
+RSpec.describe "GET /sign_in", type: :request do
+  it "ログインしているとき、トップページにリダイレクトされること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
 
-    before do
-      login_as(user, scope: :user)
-    end
+    get "/sign_in"
 
-    it "トップページにリダイレクトされること" do
-      get "/sign_in"
-
-      expect(response.status).to eq(302)
-      expect(response).to redirect_to("/")
-    end
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to("/")
   end
 
-  context "ログインしていないとき" do
-    it "ログインページが表示されること" do
-      get "/sign_in"
+  it "ログインしていないとき、ログインページが表示されること" do
+    get "/sign_in"
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include("おかえりなさい！")
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include("おかえりなさい！")
+  end
+
+  it "backパラメータが提供されたとき、正常にページが表示されること" do
+    get "/sign_in", params: {back: "/profile"}
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("おかえりなさい！")
+  end
+
+  it "有効なclient_idパラメータが提供されたとき、正常にページが表示されること" do
+    user = create(:registered_user)
+    oauth_app = create(:oauth_application, owner: user)
+
+    get "/sign_in", params: {client_id: oauth_app.uid}
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("おかえりなさい！")
+  end
+
+  it "無効なclient_idパラメータが提供されたとき、正常にページが表示されること" do
+    get "/sign_in", params: {client_id: "invalid_uid"}
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("おかえりなさい！")
+  end
+
+  it "backパラメータとclient_idパラメータが両方提供されたとき、正しく処理されること" do
+    user = create(:registered_user)
+    oauth_app = create(:oauth_application, owner: user)
+
+    get "/sign_in", params: {back: "/profile", client_id: oauth_app.uid}
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("おかえりなさい！")
   end
 end
