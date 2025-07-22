f255128afd3b6f28276b3b82ce0281d009cf97ee
Koji Shimba <me@shimba.co>
2025-07-22 11:40:37 +0900

    Fix

--- spec/requests/settings/apps/index_spec.rb
+++ spec/requests/settings/apps/index_spec.rb
@@ -1,19 +1,35 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /settings/apps", type: :request do
-  let!(:user) { create(:registered_user) }
-  let!(:oauth_application) { create(:oauth_application) }
-  let!(:oauth_access_token) { create(:oauth_access_token, application: oauth_application, owner: user) }
-
-  before do
+RSpec.describe "GET /settings/apps", type: :request do
+  it "ログインしているとき、接続済みアプリとトークンが表示されること" do
+    user = create(:registered_user)
+    oauth_application = create(:oauth_application)
+    create(:oauth_access_token, application: oauth_application, owner: user)
     login_as(user, scope: :user)
-  end
 
-  it "ページが表示されること" do
     get "/settings/apps"
 
     expect(response.status).to eq(200)
     expect(response.body).to include(oauth_application.name)
+  end
+
+  it "ログインしているとき、個人トークンが表示されること" do
+    user = create(:registered_user)
+    personal_token = create(:oauth_access_token, application: nil, owner: user, description: "Test Token")
+    login_as(user, scope: :user)
+
+    get "/settings/apps"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("個人用アクセストークン")
+    expect(response.body).to include(personal_token.description)
+  end
+
+  it "ログインしていないとき、ログインページにリダイレクトすること" do
+    get "/settings/apps"
+
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to(new_user_session_path)
   end
 end
