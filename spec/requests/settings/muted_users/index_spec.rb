b7f388d8453431633c12af974ed2a3254a136526
Koji Shimba <me@shimba.co>
2025-07-22 12:12:02 +0900

    Fix

--- spec/requests/settings/muted_users/index_spec.rb
+++ spec/requests/settings/muted_users/index_spec.rb
@@ -1,20 +1,55 @@
 # typed: false
 # frozen_string_literal: true
 
-describe "GET /settings/muted_users", type: :request do
-  let!(:user_1) { create(:registered_user) }
-  let!(:user_2) { create(:registered_user) }
+RSpec.describe "GET /settings/muted_users", type: :request do
+  it "未ログイン時、ログインページにリダイレクトすること" do
+    get "/settings/muted_users"
 
-  before do
-    user_1.mute(user_2)
-
-    login_as(user_1, scope: :user)
+    expect(response).to redirect_to(new_user_session_path)
+    expect(flash[:alert]).to eq("ログインしてください")
   end
 
-  it "ページが表示されること" do
+  it "ミュートしたユーザーがいない場合、空の状態が表示されること" do
+    user = create(:registered_user)
+    login_as(user, scope: :user)
+
+    get "/settings/muted_users"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("ミュートした人はいません")
+  end
+
+  it "ミュートしたユーザーが1人いる場合、そのユーザーが表示されること" do
+    user_1 = create(:registered_user)
+    user_2 = create(:registered_user)
+    user_1.mute(user_2)
+    login_as(user_1, scope: :user)
+
     get "/settings/muted_users"
 
     expect(response.status).to eq(200)
     expect(response.body).to include(user_2.profile.name)
+    expect(response.body).to include("ミュートを解除")
+  end
+
+  it "複数のミュートしたユーザーがいる場合、新しい順に表示されること" do
+    user_1 = create(:registered_user)
+    user_2 = create(:registered_user)
+    user_3 = create(:registered_user)
+
+    user_1.mute(user_2)
+    user_1.mute(user_3)
+    login_as(user_1, scope: :user)
+
+    get "/settings/muted_users"
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include(user_2.profile.name)
+    expect(response.body).to include(user_3.profile.name)
+
+    # user_3が後にミュートされたので先に表示される（新しい順）
+    user_2_position = response.body.index(user_2.profile.name)
+    user_3_position = response.body.index(user_3.profile.name)
+    expect(user_3_position).to be < user_2_position
   end
 end
